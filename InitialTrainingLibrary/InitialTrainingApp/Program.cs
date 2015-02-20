7fd343096f6508fef83808d92067e32fe8590bd0
Ghost rider <warriorr@poczta.fm>
2015-02-20 21:51:07 +0100

    showdown commit.

--- InitialTrainingLibrary/InitialTrainingApp/Program.cs
+++ InitialTrainingLibrary/InitialTrainingApp/Program.cs
@@ -10,6 +10,28 @@ namespace InitialTrainingApp
     {
         static void Main(string[] args)
         {
+            Console.ReadKey();
+            int e = 4;
+            var test = 3435;
+
+            Class1 dsfqfq = new Class1();
+            dsfqfq.test = 4325;
+            var dsa = 321;
+            var class1 = new Class1();
+            var class23 = new Class1();
+
+            var dsafjia = new Class1();
+
+            dsafjia.test = 4325234;
+
+
+            var dsaf = new Class1();
+
+            var dsahgfuasd = new Class1();
+
+            var fdwf = dsahgfuasd.ToString();
+
+            var beata = new Class1 {test = 8};
         }
     }
 }
--- InitialTrainingLibrary/InitialUnitTest/UnitTest.cs
+++ InitialTrainingLibrary/InitialUnitTest/UnitTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace InitialUnitTest
 {
@@ -9,6 +8,7 @@ namespace InitialUnitTest
         [TestMethod]
         public void TestMethod()
         {
+
         }
     }
 }
