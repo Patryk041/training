88ed2d51691ff3b5993bab194032f66d627ab72f
Ghost rider <warriorr@poczta.fm>
2015-02-21 00:47:09 +0100

    frogie test

---
+++ InitialTrainingLibrary/InitialUnitTest/frogie/FrogieTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.frogie;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InitialUnitTest.frogie
+{
+    [TestClass]
+    public class FrogieTest
+    {
+        [TestMethod]
+        public void TestDate()
+        {
+            FrogieDateValidator validator = new FrogieDateValidator();
+            
+            Assert.IsFalse(validator.DateValidate(2014, 13, 31));
+            Assert.IsFalse(validator.DateValidate(2014, 12, 32));
+            Assert.IsTrue(validator.DateValidate(2014, 12, 31));
+            Assert.IsFalse(validator.DateValidate(2014, 04, 31));
+
+        }
+    }
+}
