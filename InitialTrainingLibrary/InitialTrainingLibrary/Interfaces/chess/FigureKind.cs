a8ced6a5be90bbf565e705861318f2a954a64e9f
Ghost rider <warriorr@poczta.fm>
2015-02-21 13:11:08 +0100

    interfejsy

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Interfaces/chess/FigureKind.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialTrainingLibrary.Interfaces.chess
+{
+    public enum FigureKind
+    {
+        Rook,
+        Horse,
+        Bishop, 
+        King,
+        Queen,
+        Pawn
+    }
+}
