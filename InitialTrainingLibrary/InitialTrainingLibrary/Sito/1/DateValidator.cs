7e4474c119a6899d94ce5c38cb5990bb2de53711
Artur Stelmach <arturstelmach92@gmail.com>
2015-02-22 20:32:09 +0100

    test commit

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Sito/1/DateValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.Interfaces;
+
+namespace InitialTrainingLibrary.Sito._1
+{
+    public class DateValidator:IDateValidator
+    {
+        public bool DateValidate(int year, int month, int day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
