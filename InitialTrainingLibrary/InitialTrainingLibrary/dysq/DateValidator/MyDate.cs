62ecf866ce615bbf8d027064cb1133c3defab87c
Mateusz Byra <mat.byra@gmail.com>
2015-02-22 15:55:37 +0100

    1.DateValidator by dysq

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/dysq/DateValidator/MyDate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.dysq.DateValidator.Days;
+using InitialTrainingLibrary.Interfaces;
+
+namespace InitialTrainingLibrary.dysq.DateValidator
+{
+    public class MyDate : IDateValidator
+    {
+
+        public bool DateValidate(int year, int month, int day)
+        {
+        
+            if (IsMonthValid(month))
+            {
+                if (IsDayValidOf31(day) && Months.GetMonths31Days().Any(_month => _month == month))
+                    return true;
+
+                if (IsDayValidOf30(day) && Months.GetMonths30Days().Any(_month => _month == month))
+                    return true;
+                
+                if (IsDayValidOf28(day))
+                    return Months.Is28DaysMonth(month);   
+            }
+
+            return false;
+        }
+
+
+        public bool IsMonthValid(int month)
+        {
+            return Months.IsMonthCorrect(month);
+        }
+
+        public bool IsDayValidOf31(int day)
+        {
+            return Days31ofMonths.IsCorrect(day);
+        }
+
+        public bool IsDayValidOf30(int day)
+        {
+            return Days30ofMonths.IsCorrect(day);
+        }
+
+        public bool IsDayValidOf28(int day)
+        {
+            return Days28ofMonth.IsCorrect(day);
+        }
+    }
+}
---
+++ InitialTrainingLibrary/InitialUnitTest/dysq/dysqTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.dysq.DateValidator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InitialUnitTest.dysq
+{
+    [TestClass]
+    public class dysqTest
+    {
+        [TestMethod]
+        public void TestMyDate()
+        {
+            MyDate validator = new MyDate();
+
+            Assert.IsFalse(validator.DateValidate(2014, 13, 31));
+            Assert.IsFalse(validator.DateValidate(2014, 12, 32));
+            Assert.IsTrue(validator.DateValidate(2014, 12, 30));
+            Assert.IsFalse(validator.DateValidate(2014, 09, 32));
+            Assert.IsTrue(validator.DateValidate(2014, 06, 10));
+            Assert.IsTrue(validator.DateValidate(2014, 12, 31));
+            Assert.IsFalse(validator.DateValidate(2014, 02, 29));
+            Assert.IsTrue(validator.DateValidate(2014, 02, 28));
+            Assert.IsFalse(validator.DateValidate(2014, 04, 31));
+        }
+
+    }
+}
