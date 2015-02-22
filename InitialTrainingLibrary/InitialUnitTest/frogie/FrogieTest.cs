8780c2c33fe2bec663fee5902e2531853a84ed12
warrior888 <warriorr@poczta.fm>
2015-02-22 12:57:10 +0100

    Algorytmika

--- InitialTrainingLibrary/InitialUnitTest/frogie/FrogieTest.cs
+++ InitialTrainingLibrary/InitialUnitTest/frogie/FrogieTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using InitialTrainingLibrary.frogie;
 using InitialTrainingLibrary.Interfaces;
 using InitialTrainingLibrary.RS.Simple;
+using InitialTrainingLibrary.syf.algorythmics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace InitialUnitTest.frogie
@@ -48,6 +49,51 @@ namespace InitialUnitTest.frogie
                 Assert.IsFalse(valid.DateValidate(2014, 04, 31));
                 Assert.IsFalse(valid.DateValidate(2015, 02, 29));
             }
+        }
+
+        [TestMethod]
+        public void testPalindrom()
+        {
+            Assert.IsTrue(Algorythmics.IsPalindrom("radar"));
+            Assert.IsTrue(Algorythmics.IsPalindrom("abnrnba"));
+            Assert.IsTrue(Algorythmics.IsPalindrom("abnrrnba"));
+            Assert.IsTrue(Algorythmics.IsPalindrom("123321"));
+            Assert.IsFalse(Algorythmics.IsPalindrom("123jkl321"));
+            Assert.IsTrue(Algorythmics.IsPalindrom2("radar"));
+            Assert.IsTrue(Algorythmics.IsPalindrom2("abnrnba"));
+            Assert.IsTrue(Algorythmics.IsPalindrom2("abnrrnba"));
+            Assert.IsTrue(Algorythmics.IsPalindrom2("123321"));
+            Assert.IsFalse(Algorythmics.IsPalindrom2("123jkl321"));
+        
+        }
+
+        [TestMethod]
+        public void testAnagram()
+        {
+            Assert.IsTrue(Algorythmics.IsAnagram("asdf0","fd0sa"));
+            Assert.IsTrue(Algorythmics.IsAnagram("beata","atbae"));
+            Assert.IsFalse(Algorythmics.IsAnagram("beas","atbae"));
+            Assert.IsFalse(Algorythmics.IsAnagram("beaaa","atbae"));
+
+        }
+
+        [TestMethod]
+        public void testRewers()
+        {
+            Assert.IsTrue(Algorythmics.Rewers("adhkl") == "lkhda");
+            Assert.IsTrue(Algorythmics.Rewers("beata") == "ataeb");
+            Assert.IsFalse(Algorythmics.Rewers("bartłomij") == "bartłomiej");
+            
+        }
+
+        [TestMethod]
+        public void testPalindrom2()
+        {
+            Assert.IsTrue(Algorythmics.IsPalindrom11("radar"));
+            Assert.IsTrue(Algorythmics.IsPalindrom11("abnrnba"));
+            Assert.IsTrue(Algorythmics.IsPalindrom11("abnrrnba"));
+            Assert.IsTrue(Algorythmics.IsPalindrom11("123321"));
+            Assert.IsFalse(Algorythmics.IsPalindrom11("123jkl321"));
         }
     }
 }
