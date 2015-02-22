507d00b309b81267a004fccda874087d3f350199
warrior888 <warriorr@poczta.fm>
2015-02-22 07:54:39 +0100

    ram exhaust example

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/syf/proofsofconcept/dispose/ExampleTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace InitialTrainingLibrary.syf.proofsofconcept.dispose
+{
+    public class ExampleTask : Task
+    {
+        private static int listSize = 100000;
+        private static int totalCallsCount = 4000;
+        private static int callsCount = 0;
+
+        public ExampleTask()
+            : base(GenerateList)
+        {
+            
+        }
+
+        private static void GenerateList()
+        {
+            using (var list = new DisposableListExample<ExampleClass>(listSize))
+            {
+            //var list = new DisposableListExample<ExampleClass>(listSize);
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    list[i] = new ExampleClass("cnbdjgdwhugfvdhvbjbwqvihbvehvbvhdbvhsvbdsavhdkbvhdas", "fbqeyfiqebceqhbveyifbceqybvvbvqehvceb");
+                }
+
+            };
+            callsCount++;
+
+            if (callsCount < totalCallsCount)
+            {
+                GenerateList();
+            }
+        }
+    }
+}
