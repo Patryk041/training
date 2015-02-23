58cbece3ddc76f505453ad38197973542b4a4dd6
MQ <r.hipnarowicz@gmail.com>
2015-02-23 22:54:00 +0100

    added class

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/hipek/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialTrainingLibrary.hipek
+{
+    class Class1
+    {
+    }
+}
