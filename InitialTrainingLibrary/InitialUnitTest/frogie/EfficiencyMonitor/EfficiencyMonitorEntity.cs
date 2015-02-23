b432d1b48ac1bca2392c16ebc94ae82d26e39c41
Ghost rider <warriorr@poczta.fm>
2015-02-23 10:14:48 +0100

    efficiency monitor and correctness checker.

---
+++ InitialTrainingLibrary/InitialUnitTest/frogie/EfficiencyMonitor/EfficiencyMonitorEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace InitialUnitTest.frogie.EfficiencyMonitor
+{
+    class EfficiencyMonitorEntity
+    {
+        public double Time { get; set; }
+
+        public string Author { get; set; }
+
+        public int FailsCount { get; set; }
+    }
+}
