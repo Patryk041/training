8fd4860aa3d8dc8ae08e2343e88973fb2fb70ca8
Szymon Mikołajewicz <szmikol@hotmail.com>
2015-02-25 15:17:20 +0100

    Added SpecificDate and Shared

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/szymon93/[1] DateValidator/SpecificDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialTrainingLibrary.szymon93._1__DateValidator
+{
+	class SpecificDate : Shared
+	{
+		int day, month, year;
+		List<int[]> Months = new List<int[]>();
+
+		public SpecificDate(int SpecificDay, int SpecificMonth, int SpecificYear)
+		{
+			day = SpecificDay;
+			month = SpecificMonth;
+			year = SpecificYear;
+
+			Months = EnterDataAboutDaysInMonths(year);
+		}
+
+		public bool CheckDateSpecific()
+		{
+			
+
+			return false;
+		}
+	}
+}
