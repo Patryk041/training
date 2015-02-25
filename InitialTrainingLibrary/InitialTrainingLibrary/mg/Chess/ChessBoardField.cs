f3e73ad44b0f5c6a6d6a29fb8daef3a174da7a75
mg <garbacz.m@hotmail.com>
2015-02-25 23:35:09 +0100

    chess, not tested

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/mg/Chess/ChessBoardField.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.Interfaces.chess;
+
+namespace InitialTrainingLibrary.mg.Chess
+{
+    class ChessBoardField :IBoardField
+    {
+        private ICoordinates coordinates;
+        private IFigure figure;
+
+        public ChessBoardField(int x, int y)
+        {
+         coordinates = new Coordinates(x,y);
+        }
+
+        public bool IsFieldWhite()
+        {
+            return (coordinates.GetX() + coordinates.GetY())%2 == 1;
+        }
+
+        public ICoordinates GetCoordinates()
+        {
+            return coordinates;
+        }
+
+        public bool HasFigure()
+        {
+            return figure != null;
+        }
+
+        public void SetFigure(IFigure figure)
+        {
+            this.figure = figure;
+        }
+
+        public IFigure GetFigure()
+        {
+            return figure;
+        }
+    }
+}
---
+++ InitialTrainingLibrary/InitialTrainingLibrary/mg/Chess/Figures/King.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.Interfaces.chess;
+
+namespace InitialTrainingLibrary.mg.Chess.Figures
+{
+    class King : Figure
+    {
+       
+       public King(int x, int y,bool isWhite)
+        {
+        this.figureKind = FigureKind.King;
+        this.isWhite = isWhite;
+        this.coordinates = new Coordinates(x,y);
+
+        }
+        
+        
+    }
+}
