43e78a7193ddb65ff42292a62760252338a30cd1
hipek <r.hipnarowicz@gmail.com>
2015-02-26 14:43:32 +0100

    DateValidator

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/hipek/1.DateValidator/ListOfMonths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialTrainingLibrary.hipek._1.DateValidator
+{
+    public enum ListOfMonths
+    {
+        January,
+        Februrary,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
