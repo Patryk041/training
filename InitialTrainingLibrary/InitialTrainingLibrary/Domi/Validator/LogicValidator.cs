9ccbe14bd3f0742e27e06f55e5352c8a15b9c622
Dominika <kropka9313@gmail.com>
2015-02-26 17:02:51 +0100

    Validator

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Domi/Validator/LogicValidator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.koziu.DateValidator;
+
+namespace InitialTrainingLibrary.Domi.Validator
+{
+    class LogicValidator
+    {
+        private int years;
+        private int months;
+        private int days;
+        private int monthLeap;
+        private int MaxDay;
+        private int yearL;
+
+        private static int day31 = 31;
+        private static int day28 = 28;
+        private static int day30 = 30;
+
+        private Dictionary<int, int> daysCount = new Dictionary<int, int>()
+        {
+            { 1, day31 },
+            { 2, day28 },
+            { 3, day31 },
+            { 4, day30 },
+            { 5, day31 },
+            { 6, day30 },
+            { 7, day31 },
+            { 8, day31 },
+            { 9, day30 },
+            { 10, day31 },
+            { 11, day30 },
+            { 12, day31 },
+        };
+
+        public LogicValidator() { }
+        public LogicValidator(int year, int month, int day)
+        {
+            this.years = year;
+            this.months = month;
+            this.days = day; 
+        }
+
+
+        public bool CheckYear()
+        {
+            if (1 <= years && years <= 3000)
+            {
+                if ((years%4 == 0) && (years%100 != 0) && (years%400 == 0))
+                {
+                    yearL = years;
+                }
+                else years = years;
+                return true;
+            }
+            else return false;
+        }
+
+
+        public bool CheckMonth() //ilosc dni dla miesiaca i czy miesiąc poprawny 
+        {
+            if (daysCount.ContainsKey(months))
+            {
+                MaxDay = months == 2 && yearL == years ? 29 : daysCount[months];
+                return true;
+            }
+            else return false;
+        }
+
+        public bool CheckDay()
+        {
+            if (1 <= days && days <= MaxDay)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ InitialTrainingLibrary/InitialUnitTest/Domi/Validator/DomiUnitTestValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using InitialTrainingLibrary.Domi.Validator;
+using InitialTrainingLibrary.koziu.DateValidator.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InitialUnitTest.Domi.Validator
+{
+    [TestClass]
+    public class DomiUnitTestValidator
+    {
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+            DataValidator dataTest = new DataValidator();
+            Assert.IsTrue(dataTest.DateValidate(1993, 3, 13));
+            Assert.IsFalse(dataTest.DateValidate(1993, 3, 50));
+        }
+    }
+}
