0c3e90ef5640f76ab214d15de47e64b4b025e1bd
Ghost rider <warriorr@poczta.fm>
2015-02-26 12:48:48 +0100

    implement IDateValidator in Vernathic date validate class.

--- InitialTrainingLibrary/InitialTrainingLibrary/Vernathic/01_DateValidator/DateValidator.cs
+++ InitialTrainingLibrary/InitialTrainingLibrary/Vernathic/01_DateValidator/DateValidator.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InitialTrainingLibrary.Interfaces;
 
 namespace InitialTrainingLibrary.Vernathic._01_DateValidator
 {
-    class DateValidator
+    public class DateValidator : IDateValidator
     {
         //DateValidator - TODO:
         //  1. create classes responsible for validate if set year, month, day gives correct date
@@ -21,5 +22,14 @@ namespace InitialTrainingLibrary.Vernathic._01_DateValidator
             //3.a   Day must be 1-28/29/30/31 - depends of the month
             //3.b   Month - 1-12
             //3.c   Leap year
+        public bool DateValidate(int year, int month, int day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            return "Vernathic";
+        }
     }
 }
