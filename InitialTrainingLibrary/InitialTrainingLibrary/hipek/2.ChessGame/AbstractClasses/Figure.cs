9efb728aac3e1f573c841b8241437cfe9ce93be2
hipek <r.hipnarowicz@gmail.com>
2015-02-26 19:11:42 +0100

    chess classess

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/hipek/2.ChessGame/AbstractClasses/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InitialTrainingLibrary.Interfaces.chess;
+
+namespace InitialTrainingLibrary.hipek._2.ChessGame.AbstractClasses
+{
+    public abstract class Figure : IFigure
+    {
+        public bool IsFigureWhite()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICoordinates GetCoordinates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FigureKind GetFigureKind()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Move(ICoordinates newCoordinates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InitialTrainingLibrary/InitialTrainingLibrary/hipek/2.ChessGame/ChessCoordinates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.hipek._2.ChessGame.AbstractClasses;
+
+namespace InitialTrainingLibrary.hipek._2.ChessGame
+{
+    class ChessCoordinates : Coordinates
+    {
+    }
+}
