29182c9b9d119a335bb0459576d2a2d596c83b6a
Artur Stelmach <arturstelmach92@gmail.com>
2015-02-26 14:46:05 +0100

    chessboard fix

--- InitialTrainingLibrary/InitialTrainingLibrary/Sito/2/StartFigures.cs
+++ InitialTrainingLibrary/InitialTrainingLibrary/Sito/2/StartFigures.cs
@@ -14,14 +14,22 @@ namespace InitialTrainingLibrary.Sito._2
         {
             {FigureKind.Queen,((isWhite,id) => isWhite ? new Coordinates(0,3) : new Coordinates(7,3))},
             {FigureKind.King,((isWhite,id) => isWhite ? new Coordinates(0,4) : new Coordinates(7,4))},
-            {FigureKind.Pawn,((isWhite,id) => isWhite ? new Coordinates(1,0+id) : new Coordinates(6,0+id))},
-            {FigureKind.Rook,((isWhite,id) => isWhite ? new Coordinates(0,0+id) : new Coordinates(7,0+id+6))},
-            {FigureKind.Horse,((isWhite,id) => isWhite ? new Coordinates(0,0+id+1) : new Coordinates(7,0+id+5))},
-            {FigureKind.Bishop,((isWhite,id) => isWhite ? new Coordinates(0,0+id+2) : new Coordinates(7,0+id+4))},
+            {FigureKind.Pawn,((isWhite,id) => isWhite ? new Coordinates(1,id-8) : new Coordinates(6,id-8))},
+            {FigureKind.Rook,((isWhite,id) => isWhite ? new Coordinates(0,CoorById[FigureKind.Rook](id)) : new Coordinates(7,CoorById[FigureKind.Rook](id)))},
+            {FigureKind.Horse,((isWhite,id) => isWhite ? new Coordinates(0,CoorById[FigureKind.Horse](id)) : new Coordinates(7,CoorById[FigureKind.Horse](id)))},
+            {FigureKind.Bishop,((isWhite,id) => isWhite ? new Coordinates(0,CoorById[FigureKind.Bishop](id)) : new Coordinates(7,CoorById[FigureKind.Bishop](id)))}
+
+        };
+
+        private static Dictionary<FigureKind, Func<int, int>> CoorById = new Dictionary<FigureKind, Func<int, int>>()
+        {
+            {FigureKind.Rook, (id) => id == 0 ? 0 : 7},
+            {FigureKind.Horse, (id) => id == 0 ? 1 : 6},
+             {FigureKind.Bishop, (id) => id == 0 ? 2 : 5},
         };
         public static ICoordinates GetStartFigureCoordinates(FigureKind figure,bool isWhite,int figureId)
         {
-            return fugureCoordinatesByFigureKind[figure](isWhite,figureId);
+            return fugureCoordinatesByFigureKind[figure](isWhite, figureId);
         }
         
     }
