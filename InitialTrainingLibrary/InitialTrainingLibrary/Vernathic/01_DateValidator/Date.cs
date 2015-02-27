f9d7ae908861f2202e9736d9a799faa0d1a77662
Natalia Legarska <natalia.legarska@gmail.com>
2015-02-28 01:12:18 +0200

    Fix warnings (unused variables)

--- InitialTrainingLibrary/InitialTrainingLibrary/Vernathic/01_DateValidator/Date.cs
+++ InitialTrainingLibrary/InitialTrainingLibrary/Vernathic/01_DateValidator/Date.cs
@@ -9,9 +9,6 @@ namespace InitialTrainingLibrary.Vernathic._01_DateValidator
     public class Date
     {
         public int year, month, day;
-        private int p1;
-        private int p2;
-        private int p3;
 
         public Date(int year, int month, int day)
         {
@@ -19,13 +16,5 @@ namespace InitialTrainingLibrary.Vernathic._01_DateValidator
             month = this.month;
             day = this.day;
         }
-
-        //public Date(int p1, int p2, int p3)
-        //{
-        //    // TODO: Complete member initialization
-        //    this.p1 = p1;
-        //    this.p2 = p2;
-        //    this.p3 = p3;
-        //}
     }
 }
