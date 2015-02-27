61dbcc5219498e15adb6a089c6831a6ee19fdd95
Natalia Legarska <natalia.legarska@gmail.com>
2015-02-28 01:03:54 +0200

    KrzysiuRysiu unit test

---
+++ InitialTrainingLibrary/InitialUnitTest/KrzysiuRysiu/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InitialUnitTest.KrzysiuRysiu
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
