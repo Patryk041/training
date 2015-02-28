1ac88441f68e555b22726b8592ae391626277e27
warrior888 <warriorr@poczta.fm>
2015-02-28 20:25:10 +0100

    wtf

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Domi/Game/Board/ChessFigureKing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialTrainingLibrary.Domi.Game.Board
+{
+    class ChessFigureKing : Figure
+    {
+        public ChessFigureKing(int x, int y, string name) : base(x, y, name)   // lokalizacja konkretnej figury (konkretny typ, skoczek królowa itp) 
+        {
+            this.FigureX = x;
+            this.FigureY = y;
+            this.FigureName = name; 
+        }
+
+       
+    }
+}
