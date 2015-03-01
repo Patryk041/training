798edaf05bd622276623cdacc037b64bdd08f396
warrior888 <warriorr@poczta.fm>
2015-03-01 09:24:19 +0100

    file parse architecture draft.

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Common/FileParser/FileDetailParserBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.Interfaces.FileParser;
+
+namespace Toci.TraininigLibrary.Common.FileParser
+{
+    class FileDetailParserBase : IFileSection
+    {
+        public string ReadEntry()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Common/FileParser/ParallelFileParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.Interfaces.FileParser;
+
+namespace Toci.TraininigLibrary.Common.FileParser
+{
+    public class ParallelFileParser<TSaver> where TSaver : IDbSave
+    {
+        protected List<FileParser<IFileSection, IFileSection, IFileSection>> parsers = new List<FileParser<IFileSection, IFileSection, IFileSection>>(); 
+
+
+    }
+}
