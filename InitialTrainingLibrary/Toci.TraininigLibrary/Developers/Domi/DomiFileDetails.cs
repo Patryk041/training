eeae75b506da4e3ed964df9cb8c49fd76c3bf528
Dominika <kropka9313@gmail.com>
2015-03-03 00:13:52 +0100

    ClassesFileParser

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Domi/DomiFileDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.FileParser;
+
+namespace Toci.TraininigLibrary.Developers.Domi
+{
+    public class DomiFileDetails : FileDetailParserBase
+    {
+        public override FileEntityBase ReadEntry(string fileLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
