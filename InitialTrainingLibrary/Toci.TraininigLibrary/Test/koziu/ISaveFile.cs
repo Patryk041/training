3bd055974ad323acd311862286ed6348b0c29ef0
koziu <koziu92@gmail.com>
2015-03-03 10:48:01 +0100

    no message

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Test/koziu/ISaveFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.FileParser;
+
+namespace Toci.TraininigLibrary.Test.koziu
+{
+    public interface ISaveFile
+    {
+       bool saveFile(List<FileEntityBase> streamList);        
+    }
+}
