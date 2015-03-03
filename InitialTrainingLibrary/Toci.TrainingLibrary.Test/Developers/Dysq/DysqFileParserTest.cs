cf8870b169e8f989cddcf888a5636c78e9e11544
Mateusz Byra <mat.byra@gmail.com>
2015-03-03 13:30:28 +0100

    DysqTest

---
+++ InitialTrainingLibrary/Toci.TrainingLibrary.Test/Developers/Dysq/DysqFileParserTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.TraininigLibrary.Common.FileParser;
+using Toci.TraininigLibrary.Developers.Dysq.FileParser;
+
+
+namespace Toci.TrainingLibrary.Test.Developers.Dysq
+{
+    [TestClass]
+    public class DysqFileParserTest
+    {
+        [TestMethod]
+        public void TestDysqFileParser()
+        {
+        
+            DysqFileDetailParser parser = new DysqFileDetailParser();
+           
+
+          string filePath = @"E:\Szkolenia\InitialTrainingLibrary\Toci.TraininigLibrary\data\transfer_2015-03-02_987654321_dysq.txt";
+
+            List<string> lines = new List<string>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {   while(!reader.EndOfStream)
+                lines.Add(reader.ReadLine());
+
+            }
+
+           var test = parser.ReadEntry(lines[0]);
+
+            var x = 0;
+        }
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Dysq/FileParser/DysqFileParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.FileParser;
+using Toci.TraininigLibrary.Common.Interfaces.FileParser;
+
+namespace Toci.TraininigLibrary.Developers.Dysq.FileParser
+{
+    public class DysqFileParser : FileParser<DysqFileHeaderParser, DysqFileDetailParser, DysqFileFooterParser>
+    {
+        public DysqFileParser(string fileName, DysqFileDetailParser detailsParser) : base(fileName, detailsParser)
+        {
+            
+        }
+
+        
+
+        public override string GetName()
+        {
+            return "Dysq";
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
