a68610aa034e22d2ff65aee67fc4dcee145d29e5
warrior888 <warriorr@poczta.fm>
2015-03-04 19:34:53 +0100

    fix

--- InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Sito/InternetStore/SitoBin.cs
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Sito/InternetStore/SitoBin.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Toci.TraininigLibrary.Common.Base.InternetStore;
+using Toci.TraininigLibrary.Common.Interfaces.InternetStore;
 
 namespace Toci.TraininigLibrary.Developers.Sito.InternetStore
 {
     class SitoBin:BinBase
     {
+        public override Dictionary<string, IPurchase> Purchases()
+        {
+            throw new NotImplementedException();
+        }
+
         public override string DevNickName()
         {
             return "Sito";
         }
-        public 
+
+        public override IDiscount GetDiscount()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
