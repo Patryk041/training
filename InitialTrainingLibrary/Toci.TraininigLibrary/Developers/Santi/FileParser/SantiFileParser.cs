ba7c6722e31791ec9e6cf98359320aa844eae539
Szymon Mikołajewicz <szmikol@hotmail.com>
2015-03-04 12:14:52 +0100

    FileParser update

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Santi/FileParser/SantiFileParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.FileParser;
+
+namespace Toci.TraininigLibrary.Developers.Santi.FileParser
+{
+	public class SantiFileParser : FileParser<SantiFileHeaderParser,SantiFileDetailParser,SantiFileFooterParser>
+	{
+		//Fields
+
+		//Constructors
+		public SantiFileParser(string fileName, SantiFileDetailParser detailParser) : base(fileName, new SantiFileDetailParser())
+		{
+		}
+
+		//Methods
+		public override string GetName()
+		{
+			return "Santi";
+		}
+
+		public override void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
