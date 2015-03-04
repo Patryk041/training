bd6283f14d173ecbe666394525f2c1b9c0aa4187
Mateusz Byra <mat.byra@gmail.com>
2015-03-04 19:26:11 +0100

    classes

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Dysq/Bin/Products/Sandals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Developers.Dysq.Bin.DysqBase;
+
+namespace Toci.TraininigLibrary.Developers.Dysq.Bin.Products
+{
+    public class Sandals : Boots
+    {
+        protected Sandals(int productId, BootKind name, double price, bool discount) : 
+            base(productId, name, price, discount)
+        {
+        }
+    }
+}
