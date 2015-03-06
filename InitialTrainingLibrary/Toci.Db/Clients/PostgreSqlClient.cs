686dd2ff22570322b083b28cae785356a29a9898
Ghost rider <warriorr@poczta.fm>
2015-03-06 22:57:41 +0100

    added dll for postgresql connectivity.

---
+++ InitialTrainingLibrary/Toci.Db/Clients/PostgreSqlClient.cs
@@ -0,0 +1,10 @@
+﻿using Npgsql;
+using Toci.Db.Interfaces;
+
+namespace Toci.Db.Clients
+{
+    public class PostgreSqlClient : IDbClient
+    {
+        protected NpgsqlConnection Connection;
+    }
+}
