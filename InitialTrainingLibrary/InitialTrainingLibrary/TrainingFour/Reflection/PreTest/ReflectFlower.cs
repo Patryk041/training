7efa21c4ec8efbd331db0e084dde973869fa91dd
Ghost rider <warriorr@poczta.fm>
2015-03-09 15:18:55 +0100

    reflection test.

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/TrainingFour/Reflection/PreTest/ReflectFlower.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace InitialTrainingLibrary.TrainingFour.Reflection.PreTest
+{
+    public class ReflectFlower
+    {
+        public void FillFlower()
+        {
+            Flower flower = new Flower();
+
+            flower.Colour = "Red";
+            //flower.Type 
+
+            PropertyInfo flowerTypeInfo = flower.GetType().GetProperty("Type", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            flowerTypeInfo.SetValue(flower, "Rose");
+
+            PropertyInfo flowerQuantityInfo = flower.GetType().GetProperty("Quantity", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            flowerQuantityInfo.SetValue(flower, 8);
+        }
+    }
+}
--- InitialTrainingLibrary/InitialUnitTest/warrior/TrainingThree/Tests.cs
+++ InitialTrainingLibrary/InitialUnitTest/warrior/TrainingThree/Tests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InitialTrainingLibrary.syf.fileoperations.xml.entities;
+using InitialTrainingLibrary.TrainingFour.Reflection.PreTest;
 using InitialTrainingLibrary.TrainingThree.Xml.Entities;
 using InitialTrainingLibrary.TrainingThree.Xml.Logic;
 using InitialUnitTest.szymon93;
@@ -23,6 +24,14 @@ namespace InitialUnitTest.warrior.TrainingThree
 
             XmlParser<Transfers> transferParser = new XmlParser<Transfers>();
             var result = transferParser.GetXmlData(@"I:\self\trainings\Dropbox\szkolenie\v 2.0\code\InitialTrainingLibrary\Toci.TraininigLibrary\xmldata\transfer_2015-03-07_43657284832_warrior.xml");
+        }
+
+        [TestMethod]
+        public void TestReflection()
+        {
+            ReflectFlower reflectFlower = new ReflectFlower();
+
+            reflectFlower.FillFlower();
         }
     }
 }
