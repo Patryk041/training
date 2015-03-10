65c8a6650fa3d08a9eac6b186530fb1006d6b3bb
Chmura <romduz@gmail.com>
2015-03-10 19:00:03 +0100

    Training Three Task 1, Note: I have some problems with the solution, missing metadata file, and sometimes namespaces problems. PAX

---
+++ InitialTrainingLibrary/Toci.TrainingLibrary.Test/Developers/Chmura/TrainingThree/ChmuraTrainingThreeTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.TraininigLibrary.Developers.Chmura;
+using Toci.TraininigLibrary.Developers.Chmura.ChmuraInheritanceTask;
+
+namespace Toci.TrainingLibrary.Test.Developers.Chmura.TrainingThree
+{
+    [TestClass]
+    public class ChmuraTrainingThreeTest
+    {
+        [TestMethod]
+        public void ChmuraExtensions3Test()
+        {
+            string a = "radar";
+            string b = "darra";
+            string c = "wild";
+            string d = "wildHabababa";
+            string e = "kajak";
+
+            Assert.AreEqual(e.ChmuraIsPalindrom(),true);
+            Assert.AreEqual(a.ChmuraIsAnagram(b),true);
+            Assert.AreEqual(c.ChmuraIsWildcard(d),true);
+        }
+
+        [TestMethod]
+        public void ChmuraDictionaryTest()
+        {
+            ChmuraStringDictionary<ChmuraClass> chmuraDic = new ChmuraStringDictionary<ChmuraClass>();
+            chmuraDic.Add("radar", new ChmuraClass() { Name = "pierwszy" });
+            chmuraDic.Add("arrad", new ChmuraClass() { Name = "drugi" });
+            chmuraDic.Add("radarowo", new ChmuraClass() { Name = "trzeci" });
+            chmuraDic.Add("JestemInny", new ChmuraClass() { Name = "czwarty" });
+
+            List<ChmuraClass> myList = new List<ChmuraClass>();
+
+            myList = chmuraDic.GetPalindromSet();
+            myList = chmuraDic.GetAnagramSet(chmuraDic.ElementAt(0).Key);
+            myList = chmuraDic.GetWildcardSet(chmuraDic.ElementAt(0).Key);
+
+        }
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Chmura/ChmuraInheritanceTask/ChmuraStringDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.Base.Inheritance;
+
+namespace Toci.TraininigLibrary.Developers.Chmura.ChmuraInheritanceTask
+{
+    public class ChmuraStringDictionary<T> : StringDictionary<T>
+    {
+        public override List<T> GetAnagramSet(string anagramCandidate)
+        {
+            return (from d in this where d.Key.ChmuraIsAnagram(anagramCandidate) select d.Value).ToList();
+        }
+
+        public override List<T> GetPalindromSet()
+        {
+            return (from d in this where d.Key.ChmuraIsPalindrom() select d.Value).ToList();
+        }
+
+        public override List<T> GetWildcardSet(string wildcardCandidate)
+        {
+            return (from d in this where d.Key.ChmuraIsWildcard(wildcardCandidate) select d.Value).ToList();
+        }
+    }
+}
