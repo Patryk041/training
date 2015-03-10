316f4260cb82fff2c1185ceff427d141cf2fea4c
Natalia Legarska <natalia.legarska@gmail.com>
2015-03-10 22:03:18 +0200

    .

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Vernathic/Inheritance/VInheritance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.Base.Inheritance;
+
+namespace Toci.TraininigLibrary.Developers.Vernathic.Inheritance
+{
+    class VInheritance : StringDictionary<>
+    {
+
+    }
+}
