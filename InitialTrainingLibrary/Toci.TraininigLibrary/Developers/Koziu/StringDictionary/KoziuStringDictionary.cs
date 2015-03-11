e70f456fcd5ce033b39bc93c8aa92f10e73afe11
koziu <koziu92@gmail.com>
2015-03-11 16:22:53 +0100

    KoziuStringDictionary, LogicKoziuStringDictionary

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Koziu/StringDictionary/KoziuStringDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.Base.Inheritance;
+
+namespace Toci.TraininigLibrary.Developers.Koziu.StringDictionary
+{
+    public class KoziuStringDictionary<T> : StringDictionary<T>
+    {
+
+        public override List<T> GetAnagramSet(string anagramCandidate)
+        {
+            return (from entity in this where LogicKoziuStringDictionary.IsAnagram(entity.Key, anagramCandidate) select entity.Value).ToList();
+        }
+
+        public override List<T> GetPalindromSet()
+        {
+            return (from entity in this where LogicKoziuStringDictionary.IsPalindrom(entity.Key) select entity.Value).ToList();
+        }
+
+        public override List<T> GetWildcardSet(string wildcard)
+        {
+            return (from entity in this where LogicKoziuStringDictionary.IsWildcard(entity.Key, wildcard) select entity.Value).ToList();
+        }
+    }
+
+    
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Koziu/StringDictionary/LogicKoziuStringDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.TraininigLibrary.Developers.Koziu.StringDictionary
+{
+    public static class LogicKoziuStringDictionary
+    {
+        public static bool IsAnagram(string key, string anagramCandidate)
+        {
+            return (key.OrderBy(x => x).SequenceEqual(anagramCandidate.ToLower().OrderBy(x => x)));
+
+        }
+        public static bool IsPalindrom(string key)
+        {
+            return key.ToLower().ToCharArray().SequenceEqual(key.ToLower().ToCharArray().Reverse());   
+        }
+
+        public static bool IsWildcard(string key, string wildcard)
+        {
+            int size = wildcard.Length;
+            bool isWildcard = false;
+            if(size <= key.Length)
+            {
+                for(int i = 0; i < size; i++)
+                {
+                    isWildcard = (wildcard.ToLower()[i] == key.ToLower()[i]) ? true : false;
+                    if (!isWildcard) i = size;
+                }
+            }
+            return isWildcard;
+        }
+    }
+}
