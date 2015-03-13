5117a023de21e65e493104bdd99b13bb01b8eb4b
Roman Suska <r.suska@opolskie.pl>
2015-03-13 20:14:45 +0100

    Rs XML Parser

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/RS/XmlParser/RsTransferEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Toci.TraininigLibrary.Developers.RS.XmlParser
+{
+    [XmlRoot("transfer")]
+    public class RsTransferEntity
+    {
+        [XmlAttribute("bank")]
+        public string BankName { get; set; }
+
+        [XmlElement("account")]
+        public string AccountNumber { get; set; }
+
+        [XmlElement("owner")]
+        public string Owner { get; set; }
+
+        [XmlElement("sum")]
+        public string Sum { get; set; }
+
+        [XmlElement("sourceAccount")]
+        public string SourceAccountNumber { get; set; }
+
+        [XmlElement("date")]
+        public string Date { get; set; }
+    }
+
+    [XmlRoot("transfers")]
+    public class RsTransfersEntity
+    {
+        [XmlElement("transfer")]
+        public RsTransferEntity[] TransferList { get; set; }
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/RS/XmlParser/RsXmlParser.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Toci.TraininigLibrary.Common.Interfaces.FileParser;
+
+namespace Toci.TraininigLibrary.Developers.RS.XmlParser
+{
+    public class RsXmlParser<T> : IFileParser
+    {
+        public T GetXmlData(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                var xmlSerializer = new XmlSerializer(typeof (T));
+                return (T) xmlSerializer.Deserialize(sr);
+            }
+        }
+    }
+}
