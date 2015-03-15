d1c8a0d355ad11f1fa3bc2f22ff18c0e5bb03288
warrior888 <warriorr@poczta.fm>
2015-03-15 12:10:16 +0100

    asp .net mvc project.

---
+++ InitialTrainingLibrary/Toci.Web.Training.Primary/Models/WarriorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Toci.Web.Training.Primary.Models
+{
+    public class WarriorModel
+    {
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+    }
+}
