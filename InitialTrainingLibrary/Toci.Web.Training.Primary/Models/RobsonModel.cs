dd12c8d05a1553682a68c33ec8ca2a3648e8f6c6
robson081192 <robson081192@gmail.com>
2015-03-16 12:22:13 +0100

    updating private fields with reflection.

---
+++ InitialTrainingLibrary/Toci.Web.Training.Primary/Models/RobsonModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Toci.Web.Training.Primary.Models
+{
+    public class RobsonModel
+    {
+       private string string1;
+       private string string2;
+       private string string3;
+       private string string4;
+
+       public string getString1()
+       {
+           return string1;
+       }
+
+       public string getString2()
+       {
+           return string2;
+       }
+
+       public string getString3()
+       {
+           return string3;
+       }
+
+       public string getString4()
+       {
+           return string4;
+       }
+    }
+}
