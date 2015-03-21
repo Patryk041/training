68a9ac92be5915e571f82ac584b090e5e91ec434
Ghost Rider <warriorr@poczta.fm>
2015-03-21 14:31:50 +0100

    training five.

---
+++ InitialTrainingLibrary/TrainingFive/Logic/DeparturesLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Toci.TraininigLibrary.Common.Eacrm;
+using Toci.TraininigLibrary.Developers.Chmura.ChmuraDeparture;
+
+namespace TrainingFive.Logic
+{
+    public class DeparturesLogic
+    {
+        public ChmuraDepartureList<DepartureEntity> GetDepartures()
+        {
+            //tu bedzie pobranie z api
+
+            return new ChmuraDepartureList<DepartureEntity>
+            {
+                new ChmuraDepartureEntity(1, 2, 1, new DateTime(2012, 2, 2), new DateTime(2012, 4, 4), 1),
+                new ChmuraDepartureEntity(1, 2, 1, new DateTime(2012, 4, 5), new DateTime(2012, 7, 7), 1),
+            };
+        }
+    }
+}
