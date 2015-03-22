3454de9191fc06e86c8443a3dc647e02f45e64b6
Mateusz Byra <mat.byra@gmail.com>
2015-03-22 09:59:11 +0100

    commit

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Dysq/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Dysq
+{
+    public class Player : SportsMan
+    {
+        public Player(int age, string name, string surname, string SportsField) : base(age, name, surname, SportsField)
+        {
+            
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Dysq/SportsMan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Dysq.Helper;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Dysq
+{
+    public class SportsMan
+    {
+        private int _age = 18;
+        protected string Name;
+        protected string Surname;
+        protected string SportsField;
+        public bool allowed;
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public int GetAge()
+        {
+            return _age;
+        }
+
+        public SportsMan(int age, string name, string surname, string sportsField)
+        {
+            Name = name;
+            Surname = surname;
+            SportsField = sportsField;
+            _age = age;
+
+            allowed = AllowedToPlay.CheckPlayer(this);
+
+        }
+
+        
+    }
+}
