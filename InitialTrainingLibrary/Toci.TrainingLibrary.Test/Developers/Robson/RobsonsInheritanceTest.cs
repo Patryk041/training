10c031ca4a92daaa3db8fbf0199c461b129c01a8
robson081192 <robson081192@gmail.com>
2015-03-24 18:34:27 +0100

    no message

---
+++ InitialTrainingLibrary/Toci.TrainingLibrary.Test/Developers/Robson/RobsonsInheritanceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.TraininigLibrary.Developers.Robson.RobsonsDictionaryInheritance;
+
+namespace Toci.TrainingLibrary.Test.Developers.Robson
+{
+    [TestClass]
+    public class RobsonsInheritanceTest
+    {
+        [TestMethod]
+        public void TestDictionary()
+        {
+            RobsonsDictionary dict = new RobsonsDictionary();
+
+            dict.Add("kajak", new ExampleClass{Name = "Robson", Value = 1});
+            dict.Add("lubie placki", new ExampleClass { Name = "Jan", Value = 2 });
+            dict.Add("wildcard", new ExampleClass { Name = "Kowalski", Value = 3 });
+
+            List<ExampleClass> list1 = dict.GetAnagramSet("ikcalp eibul");
+            List<ExampleClass> list2 = dict.GetPalindromSet();
+            List<ExampleClass> list3 = dict.GetWildcardSet("wild");
+        }
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Robson/RobsonsDictionaryInheritance/RobsonsDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Common.Base.Inheritance;
+
+namespace Toci.TraininigLibrary.Developers.Robson.RobsonsDictionaryInheritance
+{
+    public class RobsonsDictionary : StringDictionary<ExampleClass>
+    {
+        public override List<ExampleClass> GetAnagramSet(string anagramCandidate)
+        {
+            List<ExampleClass> list = new List<ExampleClass>();
+
+            foreach(var item in this)
+            {
+                char[] firstString = (char[])(item.Key.ToArray()).Where(c => c >= 'a' && c <= 'z');
+                char[] secondString = (char[])(anagramCandidate.ToArray()).Where(c => c >= 'a' && c <= 'z');
+
+                if(firstString.OrderBy(c => c).SequenceEqual(secondString.OrderBy(c => c)))
+                {
+                    list.Add(item.Value);
+                }
+            }
+            return list;
+        }
+
+        public override List<ExampleClass> GetPalindromSet()
+        {
+            List<ExampleClass> list = new List<ExampleClass>();
+
+            foreach(var item in this)
+            {
+                if(item.Key.Reverse() == item.Key)
+                {
+                    list.Add(item.Value);
+                }
+            }
+
+            return list;
+        }
+
+        public override List<ExampleClass> GetWildcardSet(string wildcard)
+        {
+            List<ExampleClass> list = new List<ExampleClass>();
+
+            foreach (var item in this)
+            {
+                if(item.Key.StartsWith(wildcard))
+                {
+                    list.Add(item.Value);
+                }
+            }
+
+            return list;
+        }
+    }
+}
