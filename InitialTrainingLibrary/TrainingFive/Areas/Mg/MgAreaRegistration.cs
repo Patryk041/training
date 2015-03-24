a47d4c2841a263fafe15d7f8757052f444120955
mg <garbacz.m@hotmail.com>
2015-03-24 18:17:49 +0100

    added mg area, default routing area

---
+++ InitialTrainingLibrary/TrainingFive/Areas/Mg/MgAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TrainingFive.Areas.Mg
+{
+    public class MgAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Mg";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Mg_default",
+                "Mg/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
