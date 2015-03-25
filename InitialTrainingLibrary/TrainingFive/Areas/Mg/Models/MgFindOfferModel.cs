06e0f2652f066023c37bd8ab81b4a43bda02f036
mg <garbacz.m@hotmail.com>
2015-03-25 12:39:01 +0100

    azg changes, drop down lists

---
+++ InitialTrainingLibrary/TrainingFive/Areas/Mg/Models/MgFindOfferModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingFive.Areas.Mg.Models
+{
+    public class MgFindOfferModel
+    {
+        public MgOfferTypeEnum.OfferType offerType;
+        public MgOfferTypeEnum.PropertyType propertyType;
+        public MgOfferTypeEnum.City city;
+        public MgOfferTypeEnum.NumberOfRooms numberOfRooms;
+    }
+}
