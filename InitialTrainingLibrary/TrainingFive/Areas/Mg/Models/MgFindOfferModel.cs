39e41af3ed17f9cf928fc656778d165f1f7d8222
mg <garbacz.m@hotmail.com>
2015-03-25 18:16:22 +0100

    azg

--- InitialTrainingLibrary/TrainingFive/Areas/Mg/Models/MgFindOfferModel.cs
+++ InitialTrainingLibrary/TrainingFive/Areas/Mg/Models/MgFindOfferModel.cs
@@ -7,9 +7,17 @@ namespace TrainingFive.Areas.Mg.Models
 {
     public class MgFindOfferModel
     {
-        public MgOfferTypeEnum.OfferType offerType;
-        public MgOfferTypeEnum.PropertyType propertyType;
-        public MgOfferTypeEnum.City city;
-        public MgOfferTypeEnum.NumberOfRooms numberOfRooms;
+        public MgOfferTypeEnum.OfferType OfferType;
+        public MgOfferTypeEnum.PropertyType PropertyType;
+        public MgOfferTypeEnum.City City;
+        public MgOfferTypeEnum.NumberOfRooms NumberOfRooms;
+
+        public float MinArea { get; set; }
+        public float MaxArea { get; set; }
+
+        public float MinPrice { get; set; }
+        public float MaxPrice{ get; set; }
+
+        public int OfferId{ get; set; }
     }
 }
