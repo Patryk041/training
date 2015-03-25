bbbfb6f0813b9b0d1b46aea1e6f5f3dd9db94919
mg <garbacz.m@hotmail.com>
2015-03-25 01:21:16 +0100

    AZG bootstrap site v 1.0

--- InitialTrainingLibrary/TrainingFive/Areas/Mg/Controllers/HomeController.cs
+++ InitialTrainingLibrary/TrainingFive/Areas/Mg/Controllers/HomeController.cs
@@ -10,9 +10,12 @@ namespace TrainingFive.Areas.Mg.Controllers
     {
         //
         // GET: /Mg/Home/
+        //http://localhost:51763/mg/home
 
         public ActionResult Index()
         {
+            ViewBag.Title = "Mg";
+            ViewBag.CurrentDate = DateTime.Now;
             return View("~/Areas/Mg/Views/Index.cshtml");
         }
 
