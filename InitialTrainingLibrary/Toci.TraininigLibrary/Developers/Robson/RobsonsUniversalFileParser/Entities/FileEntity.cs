5fcbd1bb2d457d3bad1e20eb386a37e3b5959c31
robson081192 <robson081192@gmail.com>
2015-03-27 00:10:21 +0100

    RobsonsUniversalFileParser - working :)

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Robson/RobsonsUniversalFileParser/Entities/FileEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Developers.Robson.RobsonsUniversalFileParser.Interfaces;
+
+namespace Toci.TraininigLibrary.Developers.Robson.RobsonsUniversalFileParser.Entities
+{
+    public abstract class FileEntity : IParsable
+    {
+        public abstract string GetLine();
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Robson/RobsonsUniversalFileParser/Factories/ParserFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Developers.Robson.RobsonsUniversalFileParser.FileParsers;
+using Toci.TraininigLibrary.Developers.Robson.RobsonsUniversalFileParser.Interfaces;
+
+namespace Toci.TraininigLibrary.Developers.Robson.RobsonsUniversalFileParser.Factories
+{
+    public class ParserFactory
+    {
+        public IReadable GetRightParser(string filePath)
+        {
+            string fileExtension = filePath.Substring(filePath.Length - 3, 3);
+
+            Dictionary<string, Func<IReadable>> parserDictionary = new Dictionary<string, Func<IReadable>>
+            {
+                {"txt", ()=> new TxtFileParser()},
+                {"xml", ()=> new XmlFileParser()}
+            };
+
+            return parserDictionary[fileExtension]();
+        }
+    }
+}
