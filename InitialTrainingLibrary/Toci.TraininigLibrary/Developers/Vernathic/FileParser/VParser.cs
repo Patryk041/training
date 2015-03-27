23ee3053c4181ee650f34a375b9a45ea53534385
Natalia Legarska <natalia.legarska@gmail.com>
2015-03-27 20:54:31 +0200

    FileParser's birth.

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Vernathic/FileParser/VParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.TraininigLibrary.Developers.Vernathic.FileParser
+{
+    class VParser
+    {
+        //TODO:: create algorithm
+        //TODO:: create file format
+    }
+}
