ba547d5e9725fd7678bcb729833fe5a3b52af518
denio <ell.lornzo@gmail.com>
2015-03-27 11:36:06 +0100

    DateValidator denio

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTest/TrainingOne/denio/TestDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.BeginnersTrainingLibrary.TrainingOne.denio.DateValidator;
+
+
+namespace Toci.BeginnersTest.TrainingOne.denio
+{
+    [TestClass]
+    public class TestDate
+    {
+        [TestMethod]
+        public void denioDateTest()
+        {
+            DateValidator testDate = new DateValidator();
+            Assert.IsTrue(testDate.CheckDate(2015, 2, 28));
+            Assert.IsFalse(testDate.CheckDate(2012, 2, 30));
+            Assert.IsTrue(testDate.CheckDate(1990, 12, 31));
+            Assert.IsTrue(testDate.CheckDate(1987, 2, 27));
+            Assert.IsFalse(testDate.CheckDate(1800, 13, 40));
+
+        }
+    }
+}
