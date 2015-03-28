f1cbe3743a1906554d29bd4ca40ec3cf3a9fe4f4
Roman Suska <r.suska91@gmail.com>
2015-03-28 16:25:13 +0100

    AZG Soltys menu v2

---
+++ InitialTrainingLibrary/TrainingFive/Areas/Soltys/Controllers/SubscriptionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingFive.Areas.Soltys.Controllers
+{
+    public class SubscriptionsController : Controller
+    {
+        //
+        // GET: /Soltys/Subscriptions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
