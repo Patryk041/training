6e3eeeaa70a5749242045e6c38e9cfd4c1cc4251
Roman Suska <r.suska91@gmail.com>
2015-03-30 19:03:33 +0200

    AZG Soltys Search controler and view

---
+++ InitialTrainingLibrary/TrainingFive/Areas/Soltys/Models/OfferTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingFive.Areas.Soltys.Models
+{
+    public enum OfferTypeEnum
+    {
+        Wszystkie = 1,
+        Sprzedaz = 2,
+        Wynajem = 3
+    }
+}
