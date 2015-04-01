5559101942ca38dd3156776d8db56e53005307fe
FrozenZielu <frozenzielu@gmail.com>
2015-04-01 14:33:02 +0200

    Training One : DateValidator poprawione

--- Toci.BeginnersTrainingLibrary/Toci.BeginnersTest/TrainingOne/Zielu/DateValidator/ZieluDateValidator.cs
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTest/TrainingOne/Zielu/DateValidator/ZieluDateValidator.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Zielu.DateValidator;
 
 namespace Toci.BeginnersTest.TrainingOne.Zielu.DateValidator
 {
     [TestClass]
-    public class ZieluDateValidator
+    public class ZieluDateValidation
     {
         [TestMethod]
         public void Test()
         {
             ZieluDateValidator blah = new ZieluDateValidator();
+            Assert.IsFalse(blah.CheckDate(1998,17,14));
+            Assert.IsTrue(blah.CheckDate(2004,2,29));
         }
     }
 }
