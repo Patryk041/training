b747f626306b1cbf6856cc2ca8490a07da348994
FrozenZielu <frozenzielu@gmail.com>
2015-04-01 15:21:56 +0200

    no message

--- Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Zielu/DateValidator/ZieluDayValidator.cs
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Zielu/DateValidator/ZieluDayValidator.cs
@@ -8,10 +8,11 @@ namespace Toci.BeginnersTrainingLibrary.TrainingOne.Zielu.DateValidator
 {
     public class ZieluDayValidator
     {
-        private readonly var longer = {1, 3, 5, 7, 8, 10, 12};
-        private var shorter = {4, 6, 9, 11};
+
         public bool Validate(int Day, int Month, int Year)
         {
+            int[] longer = {1, 3, 5, 7, 8, 10, 12};
+            int[] shorter = {4, 6, 9, 11};
             if (Day < 0) return false;
             var cos = new ZieluYearValidator();
             if (longer.Any(number=> Month==number)) return 32 > Day;
