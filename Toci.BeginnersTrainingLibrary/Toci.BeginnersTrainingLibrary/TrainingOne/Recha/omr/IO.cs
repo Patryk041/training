6a8317fa4565852ad67fc2b1bb5be375116c9b5f
ReszkeM <marek.reszke09@gmail.com>
2015-04-02 18:51:26 +0200

    task for toci_3mb‬

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Recha/omr/IO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Recha.omr
+{
+    public static class IO
+    {
+        public static void Save(List<string> newClass)
+        {
+            TextWriter tw = new StreamWriter("class.cs");
+
+            foreach (var line in newClass)
+            {
+                tw.WriteLine(line);
+            }
+            tw.Close();
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Recha/omr/Properties.cs
@@ -0,0 +1,16 @@
+﻿namespace Toci.BeginnersTrainingLibrary.TrainingOne.Recha.omr
+{
+    public class Properties
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool CannotBeNull { get; set; }
+
+        public Properties(string name, string type, bool cannotBeNull)
+        {
+            Name = name;
+            Type = type;
+            CannotBeNull = cannotBeNull;
+        }
+    }
+}
