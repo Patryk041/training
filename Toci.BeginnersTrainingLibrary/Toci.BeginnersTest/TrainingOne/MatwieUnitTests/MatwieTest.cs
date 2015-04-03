7683ee2571e83da6076e78fcb7d97c5acf5d8c7f
matwie7 <mat.wie.7@gmail.com>
2015-04-03 15:50:11 +0200

    dateValidator działający test

--- Toci.BeginnersTrainingLibrary/Toci.BeginnersTest/TrainingOne/MatwieUnitTests/MatwieTest.cs
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTest/TrainingOne/MatwieUnitTests/MatwieTest.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Toci.BeginnersTrainingLibrary.TrainingOne.Matwie;
 
 namespace Toci.BeginnersTest.TrainingOne.MatwieUnitTests
 {
     [TestClass]
-    class MatwieDateValidatorTest
+    public class MatwieTest
     {
         [TestMethod]
-        public void TestMethod()
+        public void TestMethod1()
         {
             MatwieDateValidator validator = new MatwieDateValidator();
             Assert.IsTrue(validator.CheckDate(1993, 7, 7));
-            Assert.IsTrue(validator.CheckDate(2015, 3, 4));
+            Assert.IsTrue(validator.CheckDate(2015, 8, 4));
             Assert.IsTrue(validator.CheckDate(2012, 2, 29));
             Assert.IsFalse(validator.CheckDate(2000000, 0, 0));
             Assert.IsFalse(validator.CheckDate(2000, 10, 0));
