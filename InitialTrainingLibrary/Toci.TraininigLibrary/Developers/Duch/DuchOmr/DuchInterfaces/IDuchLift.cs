864429b99c55fc2895b4c415cd39217adea12a25
Krzysztof Niedziela <kniedziela00@gmail.com>
2015-04-04 01:22:54 +0200

    omr concept & factory

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Duch/DuchOmr/DuchInterfaces/IDuchLift.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.TraininigLibrary.Developers.Duch.DuchOmr.DuchInterfaces
+{
+	public interface IDuchLift
+	{
+		string GetName();
+		bool Lift();
+	}
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Duch/DuchOmr/DuchLift.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Developers.Duch.DuchOmr.DuchInterfaces;
+
+namespace Toci.TraininigLibrary.Developers.Duch.DuchOmr
+{
+	class DuchLift			  : IDuchLift
+	{
+		public string GetName()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Lift()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
