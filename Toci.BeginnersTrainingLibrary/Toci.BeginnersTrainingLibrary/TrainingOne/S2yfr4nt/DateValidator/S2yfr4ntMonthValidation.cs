7ab31da456bd8ecdd502ec42e82cc988b1872d2f
szyfr4nt <Nirvana007@wp.pl>
2015-04-06 19:00:29 +0200

    s2yfr4nt

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/S2yfr4nt/DateValidator/S2yfr4ntMonthValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.S2yfr4nt.DateValidator
+{
+    class S2yfr4ntMonthValidation
+    {
+        public static bool ValidateMonth(int month)
+        {
+            return (month>0 && month<13);
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/S2yfr4nt/Text/S2yfr4ntTextOpertionsSets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Excercise;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.S2yfr4nt.Text
+{
+    class S2yfr4ntTextOpertionsSets: ITextOperationsSets
+    {
+        public string GetNick()
+        {
+            return "S2yfr4nt";
+        }
+
+        public string[] GetPalindromsSet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetAnagramsSet(string anagram)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetWildCardSet(string wildcard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
