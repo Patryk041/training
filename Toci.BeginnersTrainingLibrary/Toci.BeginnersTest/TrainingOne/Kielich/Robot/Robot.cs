4a8da83a10d90750f4cb08fa0430ad49f9c9b4dc
Bartosz Krupienko <akikyuo@gmail.com>
2015-04-06 10:23:55 +0200

    toci_3mb Omr Robot by Kielich

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTest/TrainingOne/Kielich/Robot/Robot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Kielich;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Kielich.Omr;
+
+namespace Toci.BeginnersTest
+{
+    /// <summary>
+    /// Summary description for Robot
+    /// </summary>
+    [TestClass]
+    public class TestRobot
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var asimo = new Robot();
+
+            asimo.SetConnection(true);
+            asimo.SetWork(true);
+            asimo.SetCapacity(50);
+            asimo.SetVoltage(11);
+            Assert.IsTrue(asimo.IsConnected());
+            Assert.IsTrue(asimo.IsWorking());
+            Assert.IsTrue(asimo.IsEnough());
+            Assert.IsTrue(asimo.Damage(true));
+            Assert.IsFalse(asimo.Record(false));
+            Assert.IsTrue(asimo.Jump(true));
+            Assert.IsFalse(asimo.Scan(false));
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Kielich/Omr/Power.cs
@@ -0,0 +1,12 @@
+﻿namespace Toci.BeginnersTrainingLibrary.TrainingOne.Kielich.Omr
+{
+    public abstract class Power
+    {
+        protected bool Connection;
+        protected bool Currency;
+
+        public abstract bool IsConnected();
+        public abstract bool IsEnough();
+        public abstract bool SetConnection(bool connection);
+    }
+}
