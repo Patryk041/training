d491ee04c05182b9f01ba3c5a7df7a74152cd70f
Roman Suska <r.suska91@gmail.com>
2015-04-07 17:05:24 +0200

    Soltys OMR Urząd

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/RS/Omr/Interfaces/IEmployee.cs
@@ -0,0 +1,10 @@
+﻿namespace Toci.TraininigLibrary.Developers.RS.Omr.Interfaces
+{
+    public interface IEmployee
+    {
+        string CheckIn();
+        string CheckOut();
+        string DoWork();
+        string SetSalary(int newSalary);
+    }
+}
