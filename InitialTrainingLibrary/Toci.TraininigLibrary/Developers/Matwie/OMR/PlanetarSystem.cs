2cf57ac4713d0597eda4a5911c2aba298938d858
matwie7 <mat.wie.7@gmail.com>
2015-04-07 22:34:37 +0200

    OMR

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Matwie/OMR/PlanetarSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Toci.TraininigLibrary.Developers.Matwie.OMR
+{
+    class PlanetarSystem
+    {
+        private List<AstronimicalObject> _astronimicalObjects = new List<AstronimicalObject>();
+        private readonly string _name;
+
+        public PlanetarSystem(string name)
+        {
+            _name = name;
+        }
+
+        private string Name
+        {
+            get { return _name; }
+        }
+
+        public void AddObject(AstronimicalObject asOb)
+        {
+            _astronimicalObjects.Add(asOb);            
+        }
+    }
+
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Matwie/OMR/Universe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Toci.TraininigLibrary.Developers.Matwie.OMR
+{
+    class Universe
+    {
+        private List<Galaxy> _galaxies = new List<Galaxy>();
+
+        public Universe()
+        {
+            for (int i = 0; i > 1000; i++) 
+                _galaxies.Add(new Galaxy());
+        }
+    }
+}
