178db5b802ab329beb23d1e83d40d5739fe0d19a
koziu <koziu92@gmail.com>
2015-04-07 15:33:38 +0200

    no message

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Koziu/OMR/Interfaces/IBed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.TraininigLibrary.Developers.Koziu.OMR.Interfaces
+{
+    interface IBed
+    {
+        string Name { get; set; }
+        BedKind kind {get; set; }
+        int MaxAdults { get; set; }
+        int Widht { get; set; }
+        int Height { get; set; }
+        int GetSize();  
+    }
+    enum BedKind
+    {
+        SingleBed,
+        DoubleBed,
+
+    }
+}
