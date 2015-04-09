dd2c44d6cebac86e03893eea88b8f44640527b60
Natalia Legarska <natalia.legarska@gmail.com>
2015-04-09 04:04:14 +0300

    OMR task - CoffeeMachine

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/Vernathic/OMR2/Substance.cs
@@ -0,0 +1,25 @@
+﻿namespace Toci.TraininigLibrary.Developers.Vernathic.OMR2
+{
+    public class Substance
+    {
+        public enum TypeOfSubstance
+        {
+            Coffee,
+            Water,
+            Milk,
+            Sugar
+        }
+
+        public static TypeOfSubstance MyType { get; set; }
+
+        public Substance(TypeOfSubstance typeOfSubstance)
+        {
+            MyType = typeOfSubstance;
+        }
+
+        public Substance()
+        {
+            
+        }
+    }
+}
