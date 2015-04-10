06becd9689a5366e2929a42e36e615cd6ce9769b
Szymon Mikołajewicz <szmikol@hotmail.com>
2015-04-10 13:24:09 +0200

    Transfer Files Parser

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingTwo/Santi/SantiTransferFileParserFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.BeginnersTrainingLibrary.TrainingTwo.Streams;
+using Toci.BeginnersTrainingLibrary.TrainingTwo.Santi.Extensions;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingTwo.Santi
+{
+	public static class SantiTransferFileParserFactory
+	{
+		public static TransferFileParser CreateTransferFileParserObject(string filePath)
+		{
+			TransferFileParser TransferFileParser = null;
+
+			switch(filePath.GetBankFileName())
+			{
+				case "ing.txt":
+					TransferFileParser = new SantiTransferFileParserING();
+					break;
+				case "multibank.txt":
+					TransferFileParser = new SantiTransferFileParserMultibank();
+					break;
+			}
+
+			return TransferFileParser;
+		}
+	}
+}
