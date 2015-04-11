d3845a66f8e9e3fbb3ae00f18dc4b2b017be35da
FrozenZielu <frozenzielu@gmail.com>
2015-04-11 23:08:56 +0200

    Training Two

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingTwo/Zielu/ZieluFileAggregator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Toci.BeginnersTrainingLibrary.TrainingTwo.Streams;
+using Toci.BeginnersTrainingLibrary.TrainingTwo.Zielu.Extensions;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingTwo.Zielu
+{
+    public class ZieluFileAggregator:TranfersAggregator
+    {
+        private const string FilePath = @"..\..\..\Toci.BeginnersTrainingLibrary\TrainingTwo\data";
+        public override List<TransferEntity> GetAllTransfers(List<string> filePathsList)
+        {
+            ZieluTransferFileParserFactory bankFactory = new ZieluTransferFileParserFactory();
+            var transfers = new List<TransferEntity>();
+            return filePathsList.Aggregate(transfers, (current, item) => current.Concat(bankFactory.GetTransferFileParser(item.GetFileName()).OpenFile(item)).ToList());
+        }
+
+        private List<string> GetFilePath()
+        {
+            return Directory.GetFiles(FilePath).ToList();
+        }
+
+        public List<TransferEntity> ZieluGetAllTransfers()
+        {
+            return GetAllTransfers(GetFilePath());
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingTwo/Zielu/ZieluTransferFileParserFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Toci.BeginnersTrainingLibrary.TrainingTwo.Streams;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingTwo.Zielu
+{
+    public class ZieluTransferFileParserFactory
+    {
+        private const string Ing = "int.txt";
+        private const string Multibank = "multibank.txt";
+
+        private static readonly Dictionary<string, Func<TransferFileParser>> BankDictionary = new Dictionary<string, Func<TransferFileParser>>()
+        {
+            {Ing,() => new ZieluingTransferFileParser()},
+            {Multibank, () => new ZielumultibankTransferFileParser()}
+        };
+        public TransferFileParser GetTransferFileParser(string file)
+        {
+            return BankDictionary.ContainsKey(file) ? BankDictionary[file]() : null;
+        }
+    }
+}
