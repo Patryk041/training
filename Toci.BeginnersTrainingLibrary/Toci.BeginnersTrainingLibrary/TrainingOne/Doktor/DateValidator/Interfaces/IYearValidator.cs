2a85c6217d6e18584bf02bd9c3bb84ae2b0967dc
Mateusz Porada <Mateusz.porada94@windowslive.com>
2015-04-11 20:41:26 +0200

    commit

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Doktor/DateValidator/Interfaces/IYearValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Doktor.DateValidator.Interfaces
+{
+    public interface IYearValidator
+    {
+        bool IsYearValid(int _year);
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Doktor/DateValidator/YearValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Doktor.DateValidator.Interfaces;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Doktor.DateValidator
+{
+    public class YearValidator : IYearValidator
+    {
+        public bool IsYearValid(int _year)
+        {
+            return (_year > 0 && _year < 2016);
+        }
+    }
+}
