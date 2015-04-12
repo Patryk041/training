e6411db0b529093889280b2a668093dce2fe6bc4
Aleksander Majcherczyk <alexander.adam.m@gmail.com>
2015-04-12 03:09:58 +0200

    VViosna --> Training Two Homework

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingTwo/Wiosna/WiosnaTransfers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.BeginnersTrainingLibrary.TrainingTwo.Streams;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingTwo.Wiosna
+{
+    public class WiosnaTransfers : TranfersAggregator
+    {
+        public List<string> FilesList()
+        {
+            return Directory.GetFiles(@"..\..\..\Toci.BeginnersTrainingLibrary\TrainingTwo\data").ToList();
+        }
+        public override List<TransferEntity> GetAllTransfers(List<string> filePathsList)
+        {
+            List<TransferEntity> completeListOftraTransferEntities = new List<TransferEntity>();
+            
+            foreach (var filePath in filePathsList)
+            {
+               completeListOftraTransferEntities.
+                   AddRange(WiosnaFileParserFactory.GetParsedItems(filePath).OpenFile(filePath));
+            }
+
+            return completeListOftraTransferEntities;
+        }
+    }
+}
