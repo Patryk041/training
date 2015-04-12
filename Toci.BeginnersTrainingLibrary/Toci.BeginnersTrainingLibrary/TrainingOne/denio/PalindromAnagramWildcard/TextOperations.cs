6b03b085fbd81a0fa5b50df974cb6cfce8dc64ef
denio <ell.lornzo@gmail.com>
2015-04-12 15:59:54 +0200

    Palindromy

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/denio/PalindromAnagramWildcard/TextOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.BeginnersTrainingLibrary.TrainingOne.Excercise;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.denio.PalindromAnagramWildcard
+{
+    class TextOperations : ITextOperationsSets
+    {
+        public string GetNick()
+        {
+            return "denio";
+        }
+
+        public string[] GetPalindromsSet()
+        {
+            denioFilterSets sets = new denioFilterSets();
+            //var result = sets.GetPalindroms(new[] { "radar", "fdsabhfda", "cyc", "fxafdagdsgsdf", "agga" });
+            var result = sets.GetPalindroms(new List<string>() { "radar", "fdsabhfda", "cyc", "fxafdagdsgsdf", "agga" });
+            return result;
+        }
+
+        public string[] GetAnagramsSet(string anagram)
+        {
+            Anagram test = new Anagram();
+            var results = test.tAnagram("Mother In Law", "The moral win");
+            return results;
+        }
+
+        public string[] GetWildCardSet(string wildcard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/denio/PalindromAnagramWildcard/denioFilterSets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.denio.PalindromAnagramWildcard
+{
+    public class denioFilterSets
+    {
+
+        public List<string> GetPalindrom(List<string> setp, Func<string, bool> palindromItem)
+        {
+            return setp.Where(item => palindromItem(item)).ToList();
+        }
+
+
+        public string[] GetPalindroms(List<string> palindromCandidatesList)
+        {
+
+            var wynik = GetPalindrom(palindromCandidatesList,
+                (inString) => inString.ToCharArray().SequenceEqual(inString.Reverse())).ToArray();
+            return wynik;
+        }
+    }
+}
