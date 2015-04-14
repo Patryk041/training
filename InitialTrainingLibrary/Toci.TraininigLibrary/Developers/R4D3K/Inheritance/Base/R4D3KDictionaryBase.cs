2a34d5d0bddfab5b0ba07a6b6a325b9bcf19fa59
Radek <radoslaw.szyma@gmail.com>
2015-04-14 18:56:30 +0200

    inheritance

---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/R4D3K/Inheritance/Base/R4D3KDictionaryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.TraininigLibrary.Developers.R4D3K.Inheritance.Interfaces;
+
+namespace Toci.TraininigLibrary.Developers.R4D3K.Inheritance
+{
+    public abstract class R4D3KDictionaryBase<T> : Dictionary<string, T>, IR4D3KInterface<T>
+    {
+        public abstract List<T> GetAnagramSet(string anagramCandidate);
+
+        public abstract List<T> GetPalindromSet();
+
+        public abstract List<T> GetWildcardSet(string wildcard);
+    }
+}
---
+++ InitialTrainingLibrary/Toci.TraininigLibrary/Developers/R4D3K/Inheritance/Interfaces/IR4D3KInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.TraininigLibrary.Developers.R4D3K.Inheritance.Interfaces
+{
+    public interface IR4D3KInterface<T>
+    {
+        List<T> GetAnagramSet(string anagramCandidate); //radar  raadr
+
+        List<T> GetPalindromSet(); // cyc radar aerrea
+
+        List<T> GetWildcardSet(string wildcard); // test*  :   testdsafda testewqrfqe,
+    }
+}
