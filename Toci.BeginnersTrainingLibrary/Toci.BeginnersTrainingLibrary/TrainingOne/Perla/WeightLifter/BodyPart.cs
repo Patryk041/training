6635c10fd8b9056044e8920b190d600bf08b9cdd
Mikołaj Król <ziemniaczek10@gmail.com>
2015-04-20 19:02:04 +0200

    Dodaję szablon WeightLiftera

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Perla/WeightLifter/BodyPart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Perla.WeightLifter
+{
+    public abstract class BodyPart
+    {
+        private string _typeName;
+        private int _circuit;
+
+        public string GetTypeName()
+        {
+            return _typeName;
+        }
+
+        public int GetCircuit()
+        {
+            return _circuit;
+        }
+
+        public BodyPart(string type, int circuit)
+        {
+            this._typeName = type;
+            this._circuit = circuit;
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Perla/WeightLifter/WeightLifter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Perla.WeightLifter
+{
+    public class WeightLifter : Sportsman
+    {
+        public WeightLifter(string name, int weight, int tall)
+        {
+            this._name = name;
+            this._weight = weight;
+            this._growth = tall;
+        }
+    }
+}
