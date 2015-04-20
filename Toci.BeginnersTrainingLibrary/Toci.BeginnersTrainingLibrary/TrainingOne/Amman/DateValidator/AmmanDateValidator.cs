d15b5d367c4fccd51b1decc0fc577cdcb5ff9bfd
Purely-Imaginary <theammanify@gmail.com>
2015-04-20 13:49:49 +0200

    Placeholder for task number 1

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Amman/DateValidator/AmmanDateValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Amman.DateValidator
+{
+    public class AmmanDateValidator
+    {
+        public void xyz()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
