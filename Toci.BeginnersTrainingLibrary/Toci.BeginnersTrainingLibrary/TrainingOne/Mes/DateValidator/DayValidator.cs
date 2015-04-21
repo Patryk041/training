eab93dce25cf2e010b9af6bd8b2c2c0e84a85478
learsum <learsum@gmail.com>
2015-04-21 20:02:57 +0200

    DateValidator

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Mes/DateValidator/DayValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Mes.DateValidator
+{
+    class DayValidator
+    {
+        private const int leapFebruaryDays = 29;
+        private const int nonLeapFebruaryDays = 28;
+        private const int minimumDays = 1;
+
+        private static Dictionary<int, int> monthDays = new Dictionary<int, int>()
+        {
+            {1,31},
+            {2,29},
+            {3,31},
+            {4,30},
+            {5,31},
+            {6,30},
+            {7,31},
+            {8,31},
+            {9,30},
+            {10,31},
+            {11,30},
+            {12,31}
+        };
+
+        public static bool ValidateDay(int day, int month, int year)
+        {
+            if (month == 2)
+            {
+                // luty
+                return YearValidator.IsLeapYear(year) ? day >= minimumDays && day <= leapFebruaryDays : day >= minimumDays && day <= nonLeapFebruaryDays;
+            }
+            else
+            {
+                // pozostale
+                return day >= minimumDays && day <= monthDays[month] ? true : false;
+            }
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Mes/DateValidator/MonthValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingOne.Mes.DateValidator
+{
+    public class MonthValidator
+    {
+        public static bool ValidateMonth(int month)
+        {
+            return month < 13 && month > 0 ? true : false;
+        }
+    }
+}
