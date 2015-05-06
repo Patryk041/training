d5dad0e16443aa92e1aaf94eb1d3d7c5a5693c9e
Ghost rider <warriorr@poczta.fm>
2015-05-06 13:24:51 +0200

    promissed db operability.

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingFour/DataBase/DbVirtualization/Queries/PostgreSqlDelete.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Toci.BeginnersTrainingLibrary.TrainingFour.DataBase.Interfaces;
+
+namespace Toci.BeginnersTrainingLibrary.TrainingFour.DataBase.DbVirtualization.Queries
+{
+    public class PostgreSqlDelete : SqlQuery, IDelete
+    {
+        private const string PATTERN = "delete from {0} where {1};";
+        private const string equalSign = "=";
+        private const string andSign = " and ";
+
+        public override string GetQuery(IModel model)
+        {
+
+            var whereDelete = new List<string>();
+
+            foreach (var item in model.GetDbTableRow())
+            {
+                if (item.Value.IsWhere())
+                {
+                    whereDelete.Add(item.Value.GetColumnName() + equalSign + GetSurroundedValue(item.Value.GetValue()));
+                }
+            }
+
+            var where = string.Join(andSign, whereDelete);
+
+            return string.Format(PATTERN, model.TableName, where);
+        }
+    }
+}
---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingFour/DataBase/Interfaces/IUpdate.cs
@@ -0,0 +1,7 @@
+﻿namespace Toci.BeginnersTrainingLibrary.TrainingFour.DataBase.Interfaces
+{
+    public interface IUpdate : IQuery
+    {
+         
+    }
+}
