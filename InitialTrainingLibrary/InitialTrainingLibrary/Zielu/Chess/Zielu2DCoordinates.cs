7bf468937af30c01e5ddcb60195ca518edd24b9d
FrozenZielu <frozenzielu@gmail.com>
2015-05-07 23:54:35 +0200

    ChessBoard beginning

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Zielu/Chess/Zielu2DCoordinates.cs
@@ -0,0 +1,25 @@
+﻿using InitialTrainingLibrary.Interfaces.chess;
+
+namespace InitialTrainingLibrary.Zielu.Chess
+{
+    class Zielu2DCoordinates : ICoordinates
+    {
+        protected int X;
+        protected int Y;
+
+        public Zielu2DCoordinates(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int GetX()
+        {
+            return X;
+        }
+
+        public int GetY()
+        {
+            return Y;
+        }
+    }
+}
---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Zielu/Chess/ZieluBoardField.cs
@@ -0,0 +1,42 @@
+﻿using InitialTrainingLibrary.Interfaces.chess;
+
+namespace InitialTrainingLibrary.Zielu.Chess
+{
+    class ZieluBoardField : IBoardField
+    {
+        private readonly bool _white;
+        private readonly ICoordinates _boardCoordinates;
+        private IFigure _boardFigure;
+
+        public ZieluBoardField(int x, int y)
+        {
+            _boardCoordinates = new Zielu2DCoordinates(x,y);
+            _white = (x + y)%2 == 0;
+        }
+
+        public bool IsFieldWhite()
+        {
+            return _white;
+        }
+
+        public ICoordinates GetCoordinates()
+        {
+            return _boardCoordinates;
+        }
+
+        public bool HasFigure()
+        {
+            return _boardFigure != null;
+        }
+
+        public void SetFigure(IFigure figure)
+        {
+            _boardFigure = figure;
+        }
+
+        public IFigure GetFigure()
+        {
+            return _boardFigure;
+        }
+    }
+}
