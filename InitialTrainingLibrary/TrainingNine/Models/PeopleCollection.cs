33cf1bce82614550e6b30198ea973166cd1a5a9c
Ghost Rider <warriorr@poczta.fm>
2015-05-09 11:34:53 +0200

    training nine - so far so good.

---
+++ InitialTrainingLibrary/TrainingNine/Models/PeopleCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace TrainingNine.Models
+{
+    public class PeopleCollection : List<People>//, IHttpActionResult 
+    {
+        private readonly HttpRequestMessage _request;
+   
+
+    public PeopleCollection()
+    {
+        
+        
+    }
+
+        public PeopleCollection(List<People> list)
+        {
+            this.AddRange(list);
+            //_request = request;
+        }
+        
+    }
+}
