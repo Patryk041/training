24f622b17bf7428f78d487cdfa116d50b29f8e39
Aleksander Majcherczyk <alexander.adam.m@gmail.com>
2015-05-09 20:43:06 +0200

    Chess <unfinished>

---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Wiosna/Chess/Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.Chmura.OtherChess.Board;
+using InitialTrainingLibrary.Domi.Game.Board.Figures;
+using InitialTrainingLibrary.Interfaces.chess;
+using InitialTrainingLibrary.Wiosna.Chess.Figures;
+using Rook = InitialTrainingLibrary.Wiosna.Chess.Figures.Rook;
+
+namespace InitialTrainingLibrary.Wiosna.Chess
+{
+    class Board : IBoard
+    {
+        private IBoardField[,] fields;
+
+        public Board()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    this.fields[i,j] = new BoardField(i,j);
+                }
+            }
+
+            fields[0,1].SetFigure(new Rook(0,1,true)); //figure placement on the board (white side)
+
+            fields[8,1].SetFigure(new Rook(8,1,false)); // black side
+
+
+        }
+
+        public IBoardField[,] GetFields()
+        {
+            return fields;
+        }
+
+        public string GetName()
+        {
+            return "Wiosna";
+        }
+    }
+}
---
+++ InitialTrainingLibrary/InitialTrainingLibrary/Wiosna/Chess/Figures/Rook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialTrainingLibrary.Interfaces.chess;
+
+namespace InitialTrainingLibrary.Wiosna.Chess.Figures
+{
+    class Rook : Figure
+    {
+        public Rook(int x, int y, bool isWhite) : base(x,y,isWhite)
+        {
+            coordinates =  new Coordinates(x,y);
+            this.kind = FigureKind.Rook;
+        }
+    }
+}
