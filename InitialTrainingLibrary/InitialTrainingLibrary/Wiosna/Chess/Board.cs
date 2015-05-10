70f12cc667b390e8089cfa8ac76346f4a2bbe613
Aleksander Majcherczyk <alexander.adam.m@gmail.com>
2015-05-10 02:31:49 +0200

    Chess improved

--- InitialTrainingLibrary/InitialTrainingLibrary/Wiosna/Chess/Board.cs
+++ InitialTrainingLibrary/InitialTrainingLibrary/Wiosna/Chess/Board.cs
@@ -7,6 +7,11 @@ using InitialTrainingLibrary.Chmura.OtherChess.Board;
 using InitialTrainingLibrary.Domi.Game.Board.Figures;
 using InitialTrainingLibrary.Interfaces.chess;
 using InitialTrainingLibrary.Wiosna.Chess.Figures;
+using Bishop = InitialTrainingLibrary.Wiosna.Chess.Figures.Bishop;
+using Horse = InitialTrainingLibrary.Wiosna.Chess.Figures.Horse;
+using King = InitialTrainingLibrary.Wiosna.Chess.Figures.King;
+using Pawn = InitialTrainingLibrary.Wiosna.Chess.Figures.Pawn;
+using Queen = InitialTrainingLibrary.Wiosna.Chess.Figures.Queen;
 using Rook = InitialTrainingLibrary.Wiosna.Chess.Figures.Rook;
 
 namespace InitialTrainingLibrary.Wiosna.Chess
@@ -26,9 +31,28 @@ namespace InitialTrainingLibrary.Wiosna.Chess
             }
 
             fields[0,1].SetFigure(new Rook(0,1,true)); //figure placement on the board (white side)
+            fields[0, 2].SetFigure(new Horse(0, 2, true));
+            fields[0, 3].SetFigure(new Bishop(0, 3, true));
+            fields[0, 4].SetFigure(new King(0, 4, true));
+            fields[0, 5].SetFigure(new Queen(0, 5, true));
+            fields[0, 6].SetFigure(new Bishop(0, 6, true));
+            fields[0, 7].SetFigure(new Horse(0, 7, true));
+            fields[0, 8].SetFigure(new Rook(0, 8, true));
+
+            for (int i = 0; i < 8; i++)
+            {
+                fields[1, i].SetFigure(new Pawn(i,1 , true)); // pawns placement on the both sides
+                fields[7, i].SetFigure(new Pawn(7, i, true));
+            }
 
             fields[8,1].SetFigure(new Rook(8,1,false)); // black side
-
+            fields[8, 2].SetFigure(new Horse(8, 2, true));
+            fields[8, 3].SetFigure(new Bishop(8, 3, true));
+            fields[8, 4].SetFigure(new King(8, 4, true));
+            fields[8, 5].SetFigure(new Queen(8, 5, true));
+            fields[8, 6].SetFigure(new Bishop(8, 6, true));
+            fields[8, 7].SetFigure(new Horse(8, 7, true));
+            fields[8, 8].SetFigure(new Rook(8, 8, true));
 
         }
 
