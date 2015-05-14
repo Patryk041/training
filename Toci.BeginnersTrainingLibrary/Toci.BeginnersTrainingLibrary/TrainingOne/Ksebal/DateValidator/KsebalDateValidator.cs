99c814c9c06f75d9437892ecdbaa38f3717bae34
Konrad <konradtatar@interia.pl>
2015-05-14 11:36:09 +0200

    Homework 1

---
+++ Toci.BeginnersTrainingLibrary/Toci.BeginnersTrainingLibrary/TrainingOne/Ksebal/DateValidator/KsebalDateValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace Toci.BeginnersTrainingLibrary.TrainingOne.Ksebal.DateValidator
+
+{
+    public class KsebalDateValidator : IDateValidator
+    {
+      
+
+
+        public bool CheckDate(int year, int month, int day)
+        {   
+            return ValidatorStaticClass.ValidateDate(year, month, day);
+        }
+
+        public string GetNick()
+        {
+            return "Ksebal";
+        }
+    }
+}
