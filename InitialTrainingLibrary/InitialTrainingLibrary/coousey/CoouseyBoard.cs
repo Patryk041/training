c12f5b83f1a52b347a92ee2e14ca2951a624f2c6
coousey <coousey@gmail.com>
2015-05-18 22:42:24 +0200

    chess update

--- InitialTrainingLibrary/InitialTrainingLibrary/coousey/CoouseyBoard.cs
+++ InitialTrainingLibrary/InitialTrainingLibrary/coousey/CoouseyBoard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InitialTrainingLibrary.coousey.coouseyFigures;
 using InitialTrainingLibrary.Interfaces.chess;
 using InitialTrainingLibrary.mg;
 
@@ -10,26 +11,59 @@ namespace InitialTrainingLibrary.coousey
 {
     class CoouseyBoard : IBoard
     {
-        IBoardField[,] Bord = new IBoardField[8,8];
+        IBoardField[,] Board = new IBoardField[8,8];
 
         public CoouseyBoard()
         {
-            // setting board fields
-            for (int i = 0; i < Bord.GetLength(0); i++)
+
+            int whitePawnRow = 1;
+            int blackPawnRow = 6;
+            int upperBlackRow = 4;
+            int whiteMainRow = 0;
+            int blacMainRow = 7;
+
+            int leftRookColumn = 0;
+            int rightRookColumn = 7;
+            int leftHorseColumn = 1;
+            int rightHorseColumn = 6;
+            int leftBishopColumn = 2;
+            int fightBishopColumn = 5;
+            int queenColumn = 3;
+            int kingColumn = 4;
+
+
+            for (int i = 0; i < Board.GetLength(0); i++)
             {
-                for (int j = 0; j < Bord.GetLength(1); j++)
+                bool figureIsWhite = i < upperBlackRow;
+
+                for (int j = 0; j < Board.GetLength(1); j++)
                 {
-                    bool isWhite = (i + j)%2 == 0;
-                    Bord[i,j] = new CoouseyBoardField(isWhite, new CoouseyCoordinates(i,j));
+                    // setting board fields
+                    bool fieldIsWhite = (i + j)%2 == 0;
+                    CoouseyCoordinates coordinates = new CoouseyCoordinates(i,j);
+
+                    Board[i,j] = new CoouseyBoardField(fieldIsWhite, coordinates);
+
+                    // setting fugures
+                    if (i == whitePawnRow || i == blackPawnRow)
+                    {
+                        Board[i, j].SetFigure(CoouseyFigureFactory.GetNewFigure(figureIsWhite, coordinates, FigureKind.Pawn));
+                    }
+                    else if (i == whiteMainRow || i == blacMainRow)
+                    {
+                        if(i == leftRookColumn || i == rightRookColumn)
+                            Board[i, j].SetFigure(CoouseyFigureFactory.GetNewFigure(figureIsWhite, coordinates, FigureKind.Rook));
+                        if(i == leftHorseColumn || i == rightHorseColumn)
+                            Board[i, j].SetFigure(CoouseyFigureFactory.GetNewFigure(figureIsWhite, coordinates, FigureKind.Rook));
+                    }
                 } 
             }
 
-            // setting fugures
         }
 
         public IBoardField[,] GetFields()
         {
-            return Bord;
+            return Board;
         }
 
         public string GetName()
