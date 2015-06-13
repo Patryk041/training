efdce9ecce0ebc07858a51acca26ed074fe7ed7c
Ghost Rider <warriorr@poczta.fm>
2015-06-13 11:12:31 +0200

    Initial hornets pre training structure.

---
+++ Hornets/startup/opole/Toci.Hornets.Opole.Startup/Toci.Hornets.Opole.Startup/TrainingOne/Duch/ExampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.TrainingOne.AccessModifiers;
+
+namespace Toci.Hornets.Opole.Startup.TrainingOne.Duch
+{
+    class ExampleClass
+    {
+        public void test()
+        {
+            AccessModifiersExamples fdsa = new AccessModifiersExamples();
+        }
+    }
+}
