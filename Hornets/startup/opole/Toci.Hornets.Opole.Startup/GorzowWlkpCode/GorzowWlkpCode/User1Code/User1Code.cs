ee55a398efe5377083552d8bbf62d2e1ec5a8c6a
msmigowski <m.smigowski@gmail.com>
2015-06-13 20:27:28 +0200

    rename namespace

--- Hornets/startup/opole/Toci.Hornets.Opole.Startup/GorzowWlkpCode/GorzowWlkpCode/User1Code/User1Code.cs
+++ Hornets/startup/opole/Toci.Hornets.Opole.Startup/GorzowWlkpCode/GorzowWlkpCode/User1Code/User1Code.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GorzowWlkpCode
+namespace GorzowWlkpCode.GorzowWlkpCode.User1Code
 {
     class User1Code
     {
