2fe91084e36e80ec8e0138abba923fc620a49fc8
UCantTouchThis <adam.stawarz11@gmail.com>
2015-06-27 15:06:24 +0200

    Underground1Task

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/UCantTouchThis/Undergtound1Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Hornets.UnitTests.Sieradz.UCantTouchThis
+{  
+    [TestClass]
+    public class Undergtound1Test
+    {
+
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+         
+    }
+}
