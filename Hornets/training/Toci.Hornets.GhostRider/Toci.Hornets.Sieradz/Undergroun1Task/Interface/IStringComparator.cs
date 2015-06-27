7b2534d811b0306931c7afb012c7c3c9dd485254
Krzysztof Niedziela <kniedziela00@gmail.com>
2015-06-27 14:26:40 +0200

    First Underground Sieradz Task ( Bartek nie moze wiedziec)

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/Undergroun1Task/Interface/IStringComparator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Hornets.Sieradz.Undergroun1Task.Interface
+{
+    interface IStringComparator
+    {
+        bool CompareTwoStrings(string s1, string s2);
+        //dostajemy dwa stringi, sprawdzamy czy ich dlugosc (tylko liczba liter a-z) jest taka sama
+        //Robimy oczywiscie UnitTesty ;)
+    }
+}
