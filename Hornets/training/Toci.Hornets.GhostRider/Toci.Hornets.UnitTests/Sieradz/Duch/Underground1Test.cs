99c0841f792124b90a2400788e7f98a6f2a43e3f
Krzysztof Niedziela <kniedziela00@gmail.com>
2015-06-27 14:48:51 +0200

    Unit test folder & Example Test

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/Duch/Underground1Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Hornets.UnitTests.Duch
+{
+    [TestClass]
+    public class Underground1Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
