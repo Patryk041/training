ace760f445a220704c3803c69a25b82bf8339851
pwltt <pablo1991lol@gmail.com>
2015-06-28 11:36:09 +0200

    poprawka

--- Eagles/TrainingOne/pwltt/test1.php
+++ Eagles/TrainingOne/pwltt/test1.php
@@ -4,7 +4,11 @@
 	
 	function pwltt_strlen($text)
 	{
-		return strlen($text);
+		$count = 0;
+		for($i = 0; isset($text[$i]); $i++)
+			$count++;
+		
+		return $count;
 	}
 	
 	function pwltt_substring($text,$count)
