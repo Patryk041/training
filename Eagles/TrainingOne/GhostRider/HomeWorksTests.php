49f13f922840608fdd498ace6b77347e79aa2f40
warrior888 <warriorr@poczta.fm>
2015-06-29 10:49:45 +0200

    fixes

--- Eagles/TrainingOne/GhostRider/HomeWorksTests.php
+++ Eagles/TrainingOne/GhostRider/HomeWorksTests.php
@@ -9,7 +9,7 @@
     
     $testElemets = array(
         array('dir' => 'Dziara', 'file' => 'function.php', 'strlen' => 'Dziara_strlen', 'substr' => 'Dziara_substr'),
-    	array('dir' => 'pwltt' , 'file' => 'function.php', 'strlen' => 'pwltt_strlen', 'substr' => 'pwltt_substr'),
+    	array('dir' => 'pwltt' , 'file' => 'test1.php', 'strlen' => 'pwltt_str', 'substr' => 'pwltt_substr'),
 		array('dir' => 'ObywatelGCC', 'file' => 'first.php', 'strlen' => 'ObywatelGCC_strlen', 'substr' => 'ObywatelGCC_substr'),
         array('dir' => 'Vernathic', 'file' => 'TextOperationsVernathic.php', 'strlen' => 'Vernathic_strlen', 'substr' => 'Vernathic_substr'),
         array('dir' => 'jake', 'file' => 'first.php', 'strlen' => 'jake_strlen', 'substr' => 'jake_substr'),
--- Eagles/TrainingOne/pwltt/test1.php
+++ Eagles/TrainingOne/pwltt/test1.php
@@ -13,13 +13,13 @@
 	    $wynik = '';
 		for( $i = 0; $i <=$count; $i++)
 		{
-			$wynik .= $text[i];
+			$wynik .= $text[$i];
 		}
 		return $wynik;
 	}
 	
 	
-	echo pwltt_strlen('programowanie');
+	echo pwltt_str('programowanie');
 	echo pwltt_substring('programowanie',5);
 	
 	?>
