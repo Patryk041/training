d585e36b7ba9987baaf0bf514ac34013da7ea52e
TypowyAdam <adam0309@gmail.com>
2015-06-29 12:55:24 +0200

    Adding UT1 class

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/TypowyAdam/UndergroundTasks/UndergroundTask1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.Sieradz.Undergroun1Task.Interface;
+
+namespace Toci.Hornets.Sieradz.TypowyAdam.UndergroundTasks
+{
+    class UndergroundTask1 : IStringComparator
+    {
+
+        public bool CompareTwoStrings(string s1, string s2)
+        {
+            return true;
+        }
+    }
+}
