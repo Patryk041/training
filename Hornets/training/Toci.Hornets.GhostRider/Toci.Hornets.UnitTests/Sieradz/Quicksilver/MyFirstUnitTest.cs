22b5310b9748ae2ad11b1b776ce01ee3f7b0c0a6
Natalia Radomska <natalia.a.radomska@gmail.com>
2015-07-01 15:46:58 +0200

    UnitTest Folder

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/Quicksilver/MyFirstUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Hornets.UnitTests.Sieradz.Quicksilver
+{
+    [TestClass]
+    public class MyFirstUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
