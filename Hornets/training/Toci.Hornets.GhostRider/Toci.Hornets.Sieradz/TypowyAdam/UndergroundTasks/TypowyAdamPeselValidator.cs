52fd309b266a309e82cb1207bec5c19c25c995d0
TypowyAdam <adam0309@gmail.com>
2015-07-01 10:57:05 +0200

    Baby don't hurt me(PeselValidator)

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/TypowyAdam/UndergroundTasks/TypowyAdamPeselValidator.cs
@@ -0,0 +1,17 @@
+﻿using Toci.Hornets.Sieradz.Undergroun1Task.Interface;
+
+namespace Toci.Hornets.Sieradz.TypowyAdam.UndergroundTasks
+{
+    public class TypowyAdamPeselValidator : IPeselValidator
+    {
+        public bool IsPeselValid(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ValidateDate(int year, int month, int day)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
