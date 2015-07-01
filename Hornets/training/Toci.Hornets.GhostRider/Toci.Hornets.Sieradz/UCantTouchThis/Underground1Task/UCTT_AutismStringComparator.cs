d0a3fd48c615f2a5fc38c39083dddb7fa2e42680
UCantTouchThis <adam.stawarz11@gmail.com>
2015-07-01 10:50:30 +0200

    What is love?

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/UCantTouchThis/Underground1Task/UCTT_AutismStringComparator.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/UCantTouchThis/Underground1Task/UCTT_AutismStringComparator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
 using Toci.Hornets.Sieradz.Undergroun1Task.Interface;
 
 namespace Toci.Hornets.Sieradz.UCantTouchThis.Underground1Task
@@ -7,28 +7,24 @@ namespace Toci.Hornets.Sieradz.UCantTouchThis.Underground1Task
     {
         public bool CompareTwoStrings(string string1, string string2)
         {
-            int numberOfLettersInS1 = 0, numberOfLettersInS2 = 0;
             char[] s1 = string1.ToCharArray();
             char[] s2 = string2.ToCharArray();
-            Int32 condition;
 
-            for (int i = 0; i < s1.Length; ++i)
+            return CountLetters(s1) == CountLetters(s2);
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int CountLetters(char[] array)
+        {
+            int numberOfLetters = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                    // Sonic my name
-                condition = (~((s1[i] - 'A') >> 31) & ((s1[i] - 'Z') >> 31)) |
-                            (~((s1[i] - 'a') >> 31) & ((s1[i] - 'z') >> 31));
-                    // speed is my game
-                numberOfLettersInS1 += condition & 1;
+                numberOfLetters += ((~((array[i] - 'A') >> 31) & ((array[i] - 'Z') >> 31)) |
+                  (~((array[i] - 'a') >> 31) & ((array[i] - 'z') >> 31))) & 1;
             }
+            return numberOfLetters;
 
-            for (int i = 0; i < s2.Length; ++i)
-            {
-                condition = ((~((s2[i] - 65) >> 31) & ((s2[i] - 90) >> 31)) |
-                            (~((s2[i] - 97) >> 31) & ((s2[i] - 122) >> 31)));
-                numberOfLettersInS2 += condition & 1;
-            }
-
-            return numberOfLettersInS1 == numberOfLettersInS2;
         }
 
     }
