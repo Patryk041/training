429ee4af3d906b58a3bc535e71632456f9fa8615
Ghost Rider <warriorr@poczta.fm>
2015-07-02 11:50:06 +0200

    demo 1 07 code.

---
+++ demos/demo2/DemoApplication/DemoApplication/DbAccess/Application.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoApplication.DbAccess
+{
+    public class Application
+    {
+        public void DoAllThatAppDoes()
+        {
+            DbAccess acc = new DbAccess();
+
+            acc.SaveData("dfsafgdsgsd", new MySqlDbClient());
+        }
+    }
+}
---
+++ demos/demo2/DemoApplication/DemoApplication/Demo/AccessModifiers/Woman.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoApplication.Demo.AccessModifiers
+{
+    public class Woman : Human
+    {
+        protected override int Recognize(Func<Human, int> recDelegate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
