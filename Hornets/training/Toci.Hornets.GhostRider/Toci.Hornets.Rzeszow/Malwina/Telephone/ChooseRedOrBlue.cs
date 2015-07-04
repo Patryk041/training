c6d0c172565e58caf45e01d0b0f73076d165c645
Malwi <malwina.kleszno@gmail.com>
2015-07-04 13:33:06 +0200

    phone

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Malwina/Telephone/ChooseRedOrBlue.cs
@@ -0,0 +1,27 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Rzeszow.Malwina.Telephone
+{
+    public class ChooseRedOrBlue : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+          if (chosenOption == 1)
+          { return new PhotoCommunication();}
+
+          if (chosenOption == 2)
+          { return new SmsCommunication();}
+
+            if (chosenOption == 3)
+            { return new VoiceCommunication();}
+        }
+
+        protected override string MyNick()
+        {
+            string m = "Malwi";
+            return m;
+        }
+    }
+}
