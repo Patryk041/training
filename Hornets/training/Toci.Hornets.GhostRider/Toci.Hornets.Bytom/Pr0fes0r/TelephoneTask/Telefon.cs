01da23e0a6588b91b0454766a2120ac3abd9593d
Pr0fes0r <mmatixak@gmail.com>
2015-07-04 13:30:30 +0200

    profesor

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Pr0fes0r/TelephoneTask/Telefon.cs
@@ -0,0 +1,29 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Bytom.Pr0fes0r.TelephoneTask
+{
+    public class Telefon : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+            switch (chosenOption)
+            {
+                case 1: { return new PhotoCommunication();}
+                case 2: { return new SmsCommunication();}
+                case 3:
+                {
+                    return new VoiceCommunication();
+                }
+                default:break;
+                }
+            }
+        }
+
+        protected override string MyNick()
+        {
+            return "Pr0fes0r";
+        }
+    }
+}
