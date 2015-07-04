368f36142e0968bb7992d3ff853b62a2cb725093
Adrianowsky94 <adrian281094@gmail.com>
2015-07-04 13:38:14 +0200

    HW 1 by Adrianowsky94

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Kalisz/Adrianowsky94/TheChosenOne.cs
@@ -0,0 +1,30 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Kalisz.Adrianowsky94
+{
+    public class TheChosenOne : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+            switch (chosenOption)
+            {
+                case 1:
+                    PhotoCommunication photoCommunication = new PhotoCommunication();
+                    break;
+                case 2:
+                    SmsCommunication smsCommunication = new SmsCommunication();
+                    break;
+                case 3:
+                    VoiceCommunication voiceCommunication = new VoiceCommunication();
+                    break;
+            }
+        }
+
+        protected override string MyNick()
+        {
+            return "Adrianowsky94";
+        }
+    }
+}
