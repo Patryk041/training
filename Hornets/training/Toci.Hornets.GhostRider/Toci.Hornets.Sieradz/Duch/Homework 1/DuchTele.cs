4c661ced076314478b6963fcba2fc7b8e409b8b9
Krzysztof Niedziela <kniedziela00@gmail.com>
2015-07-05 13:52:55 +0200

    Duch HW 1

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/Duch/Homework 1/DuchTele.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Sieradz.Duch.Homework_1
+{
+    public class DuchTele : TelephoneInterpreter
+    {
+
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+            var aaa = new DuchTelephoneFactory();
+            
+            return aaa.GetInstance(chosenOption)();
+        }
+
+        protected override string MyNick()
+        {
+            return "Duch";
+        }
+
+        
+    }
+}
