7ca60ae577b1b5eebec35d8b848a1ead99f647e8
Krystian Kałużny <k.kaluzny141@gmail.com>
2015-07-05 19:50:25 +0200

    Praca domowa 1 - PeselValidation

--- Eagles/TrainingTwo/ObywatelGCC/ObywatelGCCPeselValidation.php
+++ Eagles/TrainingTwo/ObywatelGCC/ObywatelGCCPeselValidation.php
@@ -1,12 +1,11 @@
 <?php
-	require_once '../GhostRider/Homework/PeselValidation.php';
+	//require_once '../GhostRider/Homework/PeselValidation.php'; //do w³asnych testów
 	
 	class OGCCDate
 	{
-		public $year;
-		public $month;
-		public $day;
-		
+		private $year;
+		private $month;
+		private $day;
 		private $daysCount = array(1 => 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
 		
 		function __construct($year, $month, $day) 
@@ -14,6 +13,21 @@
 			$this->year = $year;
 			$this->month = $month;
 			$this->day = $day;
+		}
+		
+		public function GetYear() 
+		{
+			return $this->year;
+		}
+		
+		public function GetMonth()
+		{
+			return $this->month;
+		}
+		
+		public function GetDay()
+		{
+			return $this->day;
 		}
 		
 		public function IsLeapYear()
@@ -45,12 +59,10 @@
 		}
 	}
 	
-	
 	class ObywatelGCCPeselValidation extends PeselValidation
 	{	
-		protected function DateValidation($year, $month, $day)
+		protected function DateValidationFromOCCDate(OGCCDate $date)
 		{
-			$date = new ObywatelGCCDate($year, $month, $day);
 			$result = false;
 			$result = $result | !$date->IsValidYear();
 			$result = $result | !$date->IsValidMonth();
@@ -58,11 +70,17 @@
 			return !$result;
 		}
 		
+		protected function DateValidation($year, $month, $day)
+		{
+			$date = new ObywatelGCCDate($year, $month, $day);
+			return $this->DateValidationFromOCCDate($date);
+		}
+		
 		protected function CutOffDate($pesel)
 		{
-			$year;
-			$month;
-			$day;
+			$year = 0;
+			$month = 0;
+			$day = 0;
 			
 			if($pesel[2] == 8 || $pesel[2] == 9) { $year = 1800; }
 			else if($pesel[2] == 0 || $pesel[2] == 1) { $year = 1900; }
@@ -82,9 +100,9 @@
 		
 		protected function CheckSum($pesel)
 		{
-			/*1*a + 3*b + 7*c + 9*d + 1*e + 3*f + 7*g + 9*h + 1*i + 3*j
-Nastêpnie nale¿y odj¹æ ostatni¹ cyfrê otrzymanego wyniku od 10. Jeœli otrzymany wynik nie jest równy cyfrze kontrolnej, to znaczy, ¿e numer zawiera b³¹d[22].*/
-			$tempValue = 1 * $pesel[0] + $pesel[1] $pesel[2] + $pesel[3] + $pesel[4] + $pesel[5] + $pesel[6] + $pesel[7] + $pesel[8] + $pesel[9] + $pesel[10]
+			/*1*a + 3*b + 7*c + 9*d + 1*e + 3*f + 7*g + 9*h + 1*i + 3*j*/
+			$tempValue = $pesel[0] + 3 * $pesel[1] + 7 * $pesel[2] + 9 * $pesel[3] + $pesel[4] + 3 * $pesel[5] + 7 * $pesel[6] + 9 * $pesel[7] + $pesel[8] + 3 * $pesel[9];
+			return (10 - $tempValue % 10) % 10 == $pesel[10];
 		}
 		
 		public function ValidatePesel($pesel)
@@ -93,25 +111,10 @@ Nastêpnie nale¿y odj¹æ ostatni¹ cyfrê otrzymanego wyniku od 10. Jeœli otrzymany 
 			$result = false;
 			
 			$date = $this->CutOffDate($pesel);
-			var_dump($date);
 			$result = $result | !$this->DateValidationFromOCCDate($date);
-			//$result = $result | !$this->DateValidation($date->year, $date->month, $date->day)
-			//$result = $result | !$this->CheckSum($pesel);
+			//$result = $result | !$this->DateValidation($date->GetYear(), $date->GetMonth(), $date->GetDay()) //dla 
+			$result = $result | !$this->CheckSum($pesel);
 			return !$result;
 		}
 		
-		protected function DateValidationFromOCCDate(OGCCDate $date)
-		{
-			$result = false;
-			$result = $result | !$date->IsValidYear();
-			$result = $result | !$date->IsValidMonth();
-			$result = $result | !$date->IsValidDay();
-			return !$result;
-		}
 	}
-	
-	$validator = new ObywatelGCCPeselValidation();
-	if($validator->ValidatePesel('12345678901') == true)
-		echo "Valid";
-	else
-		echo "Invalid";
