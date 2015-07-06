62afbf5a0beb0786ca42a96f190710d1b1bfd8e9
TypowyAdam <adam0309@gmail.com>
2015-07-06 15:18:07 +0200

    StringManipulator in progress checkpoint

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/TypowyAdam/TrainingTwoTasks/StringManipulator/TypowyAdamStringManipulator.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/TypowyAdam/TrainingTwoTasks/StringManipulator/TypowyAdamStringManipulator.cs
@@ -1,32 +1,77 @@
-﻿using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
 
 namespace Toci.Hornets.Sieradz.TypowyAdam.TrainingTwoTasks.StringManipulator
 {
     public class TypowyAdamStringManipulator : GhostRiderStringManipulationsBase
     {
-        protected override bool IsStringInString(string subject, string seek)
+        protected override bool IsStringInString(string subject, string seek)//fastest
         {
-            throw new System.NotImplementedException();
+            if(subject.Length > seek.Length)
+                return Regex.IsMatch(subject, seek); 
+            return false;
         }
 
         protected override bool IsStringElementsInString(string subject, string seek)
         {
-            throw new System.NotImplementedException();
+            foreach (var character in seek)//middle one
+            {
+                if (subject.Contains(character))
+                    continue;
+                return false;
+            }
+            return true;
         }
 
-        protected override bool IsStringAnagramOfString(string subject, string seek)
+        protected override bool IsStringAnagramOfString(string subject, string seek)//slowest
         {
-            throw new System.NotImplementedException();
+
+            if (subject.Length != seek.Length) return false;
+            char[] subjectArray = subject.ToCharArray().AsParallel().WithDegreeOfParallelism(4).OrderBy(c => c).ToArray();
+            char[] seekArray = seek.ToCharArray().AsParallel().WithDegreeOfParallelism(4).OrderBy(c => c).ToArray();
+            return subjectArray.SequenceEqual(seekArray);
         }
 
         protected override string GetNick()
         {
-            throw new System.NotImplementedException();
+            return "TypowyAdam";
         }
 
-        public override StringManipulationsResults RunStringOperations(string subject, string seek)
-        {
-            throw new System.NotImplementedException();
+        public override StringManipulationsResults RunStringOperations(string subject, string seek) //method for running Run and so on
+        { 
+            return Run(subject, seek);
         }
+
+        protected override StringManipulationsResults Run(string subject, string seek)
+        {
+            var result = new StringManipulationsResults();
+
+            result.Nick = GetNick();
+            result.Subject = subject;
+            result.Seek = seek;
+
+            result.Type = this.GetType();
+
+           /* WORK IN PROGRESS
+            result.IsStringInString = IsStringInString(subject, seek);
+            if (result.IsStringInString)
+            {
+                result.IsStringElementsInString = true;
+                result.IsAnagram = IsStringAnagramOfString(subject, seek);
+                return result;
+            }
+            */
+
+            result.IsAnagram = IsStringAnagramOfString(subject, seek);
+            result.IsStringElementsInString = IsStringElementsInString(subject, seek);
+            result.IsStringInString = IsStringInString(subject, seek);
+
+            return result;
+        }
+
+
     }
 }
