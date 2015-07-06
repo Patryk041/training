6ee5e0e84ee9c4d832e80fe06fe4bd7fedd35ee5
Pr0fes0r <mmatixak@gmail.com>
2015-07-07 00:36:43 +0200

    Pr0fes0r Homework 2

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Pr0fes0r/TrainingTwo/String/SortrString.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Pr0fes0r/TrainingTwo/String/SortrString.cs
@@ -5,31 +5,13 @@ namespace Toci.Hornets.Bytom.Pr0fes0r.TrainingTwo.String
 {
     public class SortrString
     {
-        public Stringi Sort(string subject, string seek)
+        public char[] Sort(string word)
         {
-           var subjectChar = subject.ToCharArray();
-           var seekChar    = seek.ToCharArray();
+           var wordChar = word.ToCharArray();
+         
+           Array.Sort(wordChar);
 
-           Array.Sort(subjectChar);
-           Array.Sort(seekChar);
-
-            return new Stringi(subjectChar, seekChar);
-        }
-    }
-
-    public class Stringi
-    {
-        public Stringi(char[] subjectChar, char[] seekChar)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class String
-    {
-        public Stringi(char[] subject, char[] seek);
-        {
-            
+            return wordChar;
         }
     }
 }
--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Pr0fes0r/TrainingTwo/String/StringManipulator.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Pr0fes0r/TrainingTwo/String/StringManipulator.cs
@@ -11,21 +11,56 @@ namespace Toci.Hornets.Bytom.Pr0fes0r.TrainingTwo.String
     {
         protected override bool IsStringInString(string subject, string seek)
         {
-            throw new NotImplementedException();
+         
+           var subjectChar = subject.ToCharArray();
+           var seekChar = seek.ToCharArray();
+
+            Stringi information = new Stringi();
+
+            return information.CharInWord(subjectChar, seekChar);
         }
 
         protected override bool IsStringElementsInString(string subject, string seek)
         {
-            throw new NotImplementedException();
+            
+            SortrString word1= new SortrString();
+            SortrString word2 = new SortrString();
+            char[] subjectchar = word1.Sort(subject);
+            char[] seekchar = word2.Sort(seek);
+            Stringi information = new Stringi();
+
+            int lenghtsubject = subject.Length;
+            int lenghtseek = seek.Length;
+            int sumcontrol = lenghtseek;
+
+            for (int i = 0; i < lenghtsubject; i++)
+            {
+                for (int j = 0; j < lenghtseek; j++)
+                {
+                    if (subjectchar[i] == seekchar[j])
+                    {
+                        sumcontrol = sumcontrol - 1;
+                        break;
+                    }
+                }
+                if (sumcontrol == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+            
+
         }
 
         protected override bool IsStringAnagramOfString(string subject, string seek)
         {
-            string word;
-            SortrString anagram = new SortrString();
-            anagram.Sort(subject,seek);
-
-            
+            int lenghtsubject = subject.Length;
+            int lenghtseek = seek.Length;
+            SortrString subjectChar = new SortrString();
+            SortrString seekChar = new SortrString();
+            return (subjectChar.Sort(subject) == seekChar.Sort(seek))&&(lenghtsubject == lenghtseek);
         }
 
         protected override string GetNick()
