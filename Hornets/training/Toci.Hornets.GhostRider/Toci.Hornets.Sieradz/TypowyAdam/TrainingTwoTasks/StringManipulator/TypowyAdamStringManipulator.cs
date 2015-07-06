5af27f71581f68f4fb0f2ba856880b98f1571a22
TypowyAdam <adam0309@gmail.com>
2015-07-06 13:47:33 +0200

    StringManipulator initial commit

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/TypowyAdam/TrainingTwoTasks/StringManipulator/TypowyAdamStringManipulator.cs
@@ -0,0 +1,32 @@
+﻿using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Sieradz.TypowyAdam.TrainingTwoTasks.StringManipulator
+{
+    public class TypowyAdamStringManipulator : GhostRiderStringManipulationsBase
+    {
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override string GetNick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
