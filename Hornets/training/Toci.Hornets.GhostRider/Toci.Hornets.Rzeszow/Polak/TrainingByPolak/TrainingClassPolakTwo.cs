1a62744a83f40e43ee9aa200ce5ec12f06260d57
Polak <polak1111@o2.pl>
2015-07-06 17:59:54 +0200

    Training by Polak

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Polak/TrainingByPolak/TrainingClassPolakTwo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toci.Hornets.Rzeszow.Polak.TrainingByPolak
+{
+    public class TrainingClassPolakTwo :TrainingClassPolakOne
+    {
+        public TrainingClassPolakTwo()
+        {
+        }
+      
+
+
+      //aaaaa
+    }
+}
