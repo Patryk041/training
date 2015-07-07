30013d6cb78a82e86bd8db5b2a53c4c8cce681a2
Ksebal <konradtatar@interia.pl>
2015-07-07 14:39:26 +0200

    traning2 Homework stringOperation Zalegle traning 2.

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Rzeszow/Ksebal/Testy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+using Toci.Hornets.Rzeszow.KSebal.traning2Homework;
+
+namespace Toci.Hornets.UnitTests.Rzeszow.Ksebal
+{
+    [TestClass]
+   public  class Testy
+    {
+
+          [TestMethod]
+
+           public void AnagramTest()
+           {
+               KsebalStringManipulationClass Ksebal= new KsebalStringManipulationClass();
+              StringManipulationsResults results = Ksebal.RunStringOperations("EMIN", "mN");
+              //StringManipulationsResults result;
+
+             ///Assert.IsTrue(Ksebal.test("Karolina", "Karo"));
+             Assert.IsFalse(results.IsStringInString);
+             //Assert.IsTrue(Ksebal.test("Karolina", "Kla"));
+             Assert.IsFalse(results.IsAnagram);
+             Assert.IsTrue(results.IsStringElementsInString);
+
+             results=Ksebal.RunStringOperations("ZaqWsx", "XsWaZq");
+             Assert.IsFalse(results.IsStringInString);
+             //Assert.IsTrue(Ksebal.test("Karolina", "Kla"));
+             Assert.IsTrue(results.IsAnagram);
+             Assert.IsTrue(results.IsStringElementsInString);
+
+
+
+           }
+       
+    }
+}
