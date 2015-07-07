8b3553afdf9a117feea0ee7834de33a4ff9c3b90
kwadrat633 <kwadrat633@gmail.com>
2015-07-07 19:59:15 +0200

    testy+

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Rzeszow/Chojnecki/PeselTest/PESELTest.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Rzeszow/Chojnecki/PeselTest/PESELTest.cs
@@ -12,12 +12,18 @@ namespace Toci.Hornets.UnitTests.Rzeszow.Chojnecki.PeselTest
             MyPeselValidator newPesel = new MyPeselValidator();
 
             //Cyfra kontrolna i sprawdzanie poprawności numeru
-            Assert.IsTrue(newPesel.IsPeselValid("94120807274"));
+            Assert.IsTrue(newPesel.IsPeselValid("9120807274"));
+            Assert.IsTrue(newPesel.IsPeselValid("93122305155"));
             Assert.IsFalse(newPesel.IsPeselValid("94120807275"));
             Calendar c = new Calendar("94120807274");
             Assert.AreEqual("08",c.GetDay());
             Assert.AreEqual("1994",c.GetYear());
             Assert.AreEqual("12",c.GetMonth());
+
+            Calendar c2 = new Calendar("93122305155");
+            Assert.AreEqual("23", c2.GetDay());
+            Assert.AreEqual("1993", c2.GetYear());
+            Assert.AreEqual("12", c2.GetMonth());
         }
     }
 }
