a8355762df253dc1f7c1fe870b6b8b252c696d52
wilddance <1010000i100@gmail.com>
2015-07-08 21:22:27 +0200

    Zalegla praca domowa

---
+++ Eagles/TrainingTwo/Rudi/RudiStringOperability.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "../GhostRider/Homework/StringOperationsObj.php";
+require_once "../../TrainingOne/Rudi/funkcje.php";
+
+class RudiStringOperability extends GhostRiderStringOperability
+{
+	public function StringLength($string){
+		return Rudi_strlen($string);
+	}
+
+	public function SubString($string, $length){
+		return Rudi_substr($string, $length);
+		}
+
+	public function GetNick()
+	{
+	return "Rudi";
+	}
+	
+}
+
+$test=new RudiStringOperability();
+$test->CheckImplementation();
+
