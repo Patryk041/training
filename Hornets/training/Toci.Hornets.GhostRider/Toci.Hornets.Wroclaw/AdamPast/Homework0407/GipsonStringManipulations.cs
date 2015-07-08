41cf8b6d68208d9953c8c73db0145fc447bc6f24
Adam Pasternak <adaskogpsn@gmail.com>
2015-07-08 23:50:52 +0200

    gipson pesel commit

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/AdamPast/Homework0407/GipsonStringManipulations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Wroclaw.AdamPast.Homework0407
+{
+    public class GipsonStringManipulations : GhostRiderStringManipulationsBase
+    {
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            if (seek.Length != subject.Length)
+                return false;
+
+
+            throw new NotImplementedException();
+        }
+
+        protected override string GetNick()
+        {
+            return "gipson";
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
