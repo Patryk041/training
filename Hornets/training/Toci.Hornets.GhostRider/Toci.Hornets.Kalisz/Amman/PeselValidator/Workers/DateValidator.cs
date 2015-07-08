959a1a1c16f043258044d8f5c84a1a959f3447c4
Amman <amadeusz.opac@gmail.com>
2015-07-08 10:49:44 +0200

    2nd task probably done (finished if no further fixes exist)

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Kalisz/Amman/PeselValidator/Workers/DateValidator.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Kalisz/Amman/PeselValidator/Workers/DateValidator.cs
@@ -1,7 +1,13 @@
-﻿namespace Toci.Hornets.Kalisz.Amman.PeselValidator.Workers
+﻿using Toci.Hornets.Kalisz.Amman.PeselValidator.Workers.DateValidation;
+
+namespace Toci.Hornets.Kalisz.Amman.PeselValidator.Workers
 {
     public class DateValidator
     {
-         
+        public static bool IsValid(int day, int month, int year)
+        {
+            return DayValidator.IsValid(day,month,year)&& MonthValidator.IsValid(month)&&
+                   YearValidator.IsValid(year);
+        }
     }
 }
