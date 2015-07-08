d1956e671cd611161a6e9b4dc58b6eeccdc1e22b
Kollwing <Kollwing@gmail.com>
2015-07-08 19:36:14 +0200

    Kollwing Task

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/Kollwing/ChooseOperation.cs
@@ -0,0 +1,34 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Wroclaw.Kollwing
+{
+    public class ChooseOperation : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+            if (chosenOption == 1)
+            {
+                return new PhotoCommunication();
+            }
+
+            if (chosenOption == 2)
+            {
+                return new SmsCommunication();
+            }
+
+            if (chosenOption == 3)
+            {
+                return new VoiceCommunication();
+            }
+
+           
+        }
+
+        protected override string MyNick()
+        {
+            return "kollwing";
+        }
+    }
+}
