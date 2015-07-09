10818b528f17180281bb2ce74c46ac15b29ea67f
Michał Zembik <mikzepro@gmail.com>
2015-07-09 13:35:59 +0200

    homewrok

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Legnica/Michał Zembik/ZadaniePeselValidator.cs
@@ -0,0 +1,32 @@
+﻿using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Legnica.Michał_Zembik
+{
+    public class ZadaniePeselValidator : PeselValidator
+    {
+        protected override string CutOffDate(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool Checksum(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool ValidateDate(int year, int month, int day)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetNick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsPeselValid(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Legnica/Michał Zembik/ZadanieStringManipulations.cs
@@ -0,0 +1,38 @@
+﻿using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Legnica.Michał_Zembik
+{
+    public class ZadanieStringManipulations : GhostRiderStringManipulationsBase
+    {
+        // true: beata; at, ata, ta, ea
+        // false: beata; ae, eb, aa
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // true: beata; aat, taae, aea
+        // false: beata; aaa, taaa, tta, aee
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // true: beata; ataeb, aateb, tbaea
+        // false: beata; aaateb, fdwgdw
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override string GetNick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+              return Run(subject, seek);
+        }
+    }
+}
