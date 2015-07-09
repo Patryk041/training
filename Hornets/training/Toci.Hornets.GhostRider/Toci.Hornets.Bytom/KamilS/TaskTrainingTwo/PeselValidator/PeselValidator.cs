bafab7858c3e2103a83ec95fdf8b6dd251f188ff
Kamil Soltysik <kamilsoltysik@gmail.com>
2015-07-09 22:35:12 +0200

    1/2 Task

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/KamilS/TaskTrainingTwo/PeselValidator/PeselValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace Toci.Hornets.Bytom.KamilS.TaskTrainingTwo.PeselValidator
+{
+    public class PeselValidator : GhostRider.YourWork.TasksTrainingTwo.PeselValidator
+    {
+        protected override string CutOffDate(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool Checksum(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool ValidateDate(int year, int month, int day)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetNick()
+        {
+            return "KamilS";
+        }
+
+        public override bool IsPeselValid(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
