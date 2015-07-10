54db16c2e0d980907092e8de84b765b6ed0c96bf
TypowyAdam <adam0309@gmail.com>
2015-07-10 23:06:07 +0200

    ProEloMegaSuperTest v1 już jestem zmęczony :)

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/TypowyAdam/PeselValidatorTest.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/TypowyAdam/PeselValidatorTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
 using Toci.Hornets.Sieradz.Duch.Homework_2.PeselValidator;
 using Toci.Hornets.Sieradz.Quicksilver.TasksTrainingTwoQs;
 using Toci.Hornets.Sieradz.TypowyAdam.UndergroundTasks;
@@ -16,40 +18,19 @@ namespace Toci.Hornets.UnitTests.Sieradz.TypowyAdam
     public class PeselValidatorTest
     {
         private static string testDirectory = @"..\..\Sieradz\TypowyAdam\";
+        private static string assemblyName = "Toci.Hornets.Sieradz";
+
+        private static List<object> PeselValidatorsList = new List<object>();
+        private static List<List<string>> peselListsList = new List<List<string>>();
+        private static Dictionary<string, Func<string, bool>> ValidatorFactory = new Dictionary<string, Func<string, bool>>();
 
         [TestMethod]
         public void TestMethod1()
         {
-            /*List<string> validPeselList = new List<string>();
-            List<string> invalidPeselLIst = new List<string>();
-            List<string> uncommonCasesList = new List<string>
-            {
-                "93022912345",
-                "asdasdasdasd",
-                "asdgnbbn3434123",
-                "12345678901A"
-                //dodawajcie coś bo mi się nie chce 
-            };
-
-            using (StreamReader sr = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Sieradz\TypowyAdam\validPeselList.txt")))
-            {
-                while (sr.Peek() >= 0)
-                {
-                    validPeselList.Add(sr.ReadLine());
-                }
-            }
-            using (StreamReader sr = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Sieradz\TypowyAdam\invalidPeselList.txt")))
-            {
-                while (sr.Peek() >= 0)
-                {
-                    invalidPeselLIst.Add(sr.ReadLine());
-                }
-            }
-         
+            /*
             foreach (var item in ValidatorFactory)
             {
-                Stopwatch benchmark = new Stopwatch();
-                benchmark.Start();
+
                 for (int i = 0; i < 100; i++) //bez tego wykonuje się w 10ms co jest trochę za krótkie
                 {
                     foreach (string testPesel in validPeselList)
@@ -66,14 +47,41 @@ namespace Toci.Hornets.UnitTests.Sieradz.TypowyAdam
 
                     }
                 }
-                benchmark.Stop();
-                Debug.Print("{0}: {1}ms", item.Key, Convert.ToString(benchmark.ElapsedMilliseconds));
+
             }
             */
+            Stopwatch benchmark = new Stopwatch();
+            benchmark.Start();
             GenerateListOfPeselList(testDirectory);
+            GenerateObjectList(assemblyName);
+            GenerateMethodFactory();
+            benchmark.Stop();
+            Debug.Print("Generating structures takes:{0}ms", benchmark.ElapsedMilliseconds);
+            foreach (var isPeselValid in ValidatorFactory)
+            {
+                
+                benchmark.Start();
+                foreach (var peselLists in peselListsList)
+                {
+                    for(int i = 0; i<peselLists.Count-1;i++)//foreach (var pesel in peselLists)
+                    {
+                        Assert.AreEqual(isPeselValid.Value(peselLists[i]), (peselLists.Last() == "true"));
+                    }
+
+
+                }
+                benchmark.Stop();
+                Debug.Print("{0}: {1}ms", isPeselValid.Key, Convert.ToString(benchmark.ElapsedMilliseconds));
+            }
         }
 
-
+        private static void GenerateMethodFactory()
+        {
+            foreach (PeselValidator o in PeselValidatorsList)
+            {
+                ValidatorFactory.Add(o.GetNick(), o.IsPeselValid);
+            }
+        }
         private static void GenerateListOfPeselList(string initialDirectory)
         {
             List<string> fileNames = new List<string>(Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @initialDirectory)));
@@ -97,22 +105,15 @@ namespace Toci.Hornets.UnitTests.Sieradz.TypowyAdam
             return peselList;
         }
 
-        private static List<List<string>> peselListsList = new List<List<string>>();
-        private static Dictionary<string, Func<string, bool>> ValidatorFactory = new Dictionary<string, Func<string, bool>>()
-         {
-             //sorki Adam że zjechałem :P
+        private static void GenerateObjectList(string assemblyName)
+        {
 
-
-             // "Nie bądź jak leń co śmierdzący jest!"
-             //                                 UCTT
-
-             { "TypowyAdam", new TypowyAdamPeselValidator().IsPeselValid },
-             { "TypowyAdam4GhostWarrior", new TypowyAdamPeselValidator().IsPeselValid },
-             { "UCantTouchThisAutism", new UCantTouchThisAutismPeselValisator().IsPeselValid },
-             { "UCantTouchThis", new UCantTouchThisPeselValidator().IsPeselValid },
-             { "Quicksilver", new QuicksilverPeselValidator().IsPeselValid },
-             { "DuchGottaGoFast", new DuchPeselValidator().IsPeselValid }
-         };
+            Assembly myAssembly = AppDomain.CurrentDomain.Load(assemblyName);
+            foreach (var type in myAssembly.GetTypes().Where(type => type.IsClass && type.IsSubclassOf(typeof(PeselValidator))))
+            {
+                PeselValidatorsList.Add((PeselValidator) Activator.CreateInstance(type));
+            }   
+        }
     }
 
 
