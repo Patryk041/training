73168263dd05232a25b5a9f0a65f632f8538bbbf
Tomasz Szulczewski <szulczewski.tomasz@gmail.com>
2015-07-10 12:36:41 +0200

    telephone homework

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/Tszulczewski/TelephoneTask/HomeWorkClass.cs
@@ -0,0 +1,30 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Wroclaw.Tszulczewski.TelephoneTask
+{
+    public class HomeWorkClass : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+
+            switch (chosenOption)
+            {
+                case 1:
+                    return new PhotoCommunication();
+                case 2:
+                    return new SmsCommunication();
+                case 3:
+                    return new VoiceCommunication();
+                default:
+                    return new SmsCommunication();
+            }
+        }
+
+        protected override string MyNick()
+        {
+            return "Tszulczewski";
+        }
+    }
+}
