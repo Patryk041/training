3c38c788ffbcbc383b0407f4301f320fd3cc20d7
SemiRafista <rafista14@wp.pl>
2015-07-10 17:43:55 +0200

    My first homework task

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Rzeszow/SemiRafista/SeRaManipulatingStringsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Rzeszow.SemiRafista.TrainingTwoStringManipulations;
+
+namespace Toci.Hornets.UnitTests.Rzeszow.SemiRafista
+{
+    [TestClass]
+    public class SeRaManipulatingStringsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var seRaTest = new SeRaManipulatingStrings();
+
+            //IsStringInString
+            Assert.IsTrue(seRaTest.RunStringOperations("beata","eat").IsStringInString);
+            Assert.IsFalse(seRaTest.RunStringOperations("beata", "et").IsStringInString);
+
+            //IsStringElementsInString
+            Assert.IsTrue(seRaTest.RunStringOperations("beata", "taa").IsStringElementsInString);
+            Assert.IsFalse(seRaTest.RunStringOperations("beata", "ataa").IsStringElementsInString);
+
+            //IsStringAnagram
+            Assert.IsTrue(seRaTest.RunStringOperations("beata", "eatba").IsAnagram);
+            Assert.IsFalse(seRaTest.RunStringOperations("beata", "ttbae").IsAnagram);
+        }
+    }
+}
