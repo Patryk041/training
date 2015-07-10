7c34790139fbb7270307861362037295da913798
AP01Billy <adam.poloniewicz@gmail.com>
2015-07-10 18:35:16 +0200

    taskTrainingTwo v 0.1

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/AdamP/TaskTrainingTwo/PeselValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace Toci.Hornets.Bytom.AdamP.TaskTrainingTwo
+{
+    public class PeselValidator : GhostRider.YourWork.TasksTrainingTwo.PeselValidator
+    {
+        protected override string CutOffDate(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool Checksum(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool ValidateDate(int year, int month, int day)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetNick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsPeselValid(string pesel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
