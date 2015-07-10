106f2e7e29080e44e7302280a010506f8cd58152
Coffee13 <mateuszw19911@wp.pl>
2015-07-10 20:21:01 +0200

    Stringi

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTwo/StringManipulator/CoffeeStringManipulations.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Bytom.Coffee13.TaskTrainingTwo.StringManipulator
+{
+    public class CoffeeStringManipulations : GhostRiderStringManipulationsBase
+    {
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            return subject.Contains(seek);
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            subject = subject.ToLower();
+            seek = seek.ToLower();
+
+            
+
+            char[] subjectLetters = subject.ToCharArray();
+            char[] seekLetters = seek.ToCharArray();
+
+            var LetterCount = seekLetters.Length;
+
+            for (var i = 0; i < subjectLetters.Length; i++)
+            {
+                for (var j = 0; j < seekLetters.Length; j++)
+                {
+                    if (subjectLetters[i] == seekLetters[i])
+                    {
+                        LetterCount --;
+                        break;
+                    }
+                    if (LetterCount == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            if (subject.Length == seek.Length)
+            {
+                subject = subject.ToLower();
+                seek = seek.ToLower();
+
+                char[] subjectLetters = subject.ToCharArray();
+                char[] seekLetters = seek.ToCharArray();
+
+                Array.Sort(subjectLetters);
+                Array.Sort(seekLetters);
+
+                if (seekLetters == subjectLetters)
+                {
+                    return true;
+                }
+               
+            }
+            return false;
+        }
+
+        protected override string GetNick()
+        {
+            return "Coffee13";
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            return Run(subject,seek);
+        }
+    }
+}
