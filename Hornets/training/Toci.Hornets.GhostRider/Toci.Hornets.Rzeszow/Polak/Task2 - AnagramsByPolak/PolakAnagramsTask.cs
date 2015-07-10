6f45e70db850db997c56aca434176857365b60b5
Polak <polak1111@o2.pl>
2015-07-10 23:15:49 +0200

    commit 1

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Polak/Task2 - AnagramsByPolak/PolakAnagramsTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Rzeszow.Polak.Task2___AnagramsByPolak
+{
+    public   class PolakAnagramsTask : GhostRider.YourWork.TasksTrainingTwo.GhostRiderStringManipulationsBase
+    {
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetNick()
+        {
+            return "Polak";
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
