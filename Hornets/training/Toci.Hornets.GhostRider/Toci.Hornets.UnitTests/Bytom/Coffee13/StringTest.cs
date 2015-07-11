3b2d7b1f0f09cb4a7ce23abbbdf9f01af34a10b6
Vernathic <natalia.legarska@gmail.com>
2015-07-11 13:21:53 +0200

    fix merge

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Bytom/Coffee13/StringTest.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Bytom/Coffee13/StringTest.cs
@@ -4,20 +4,21 @@ using Toci.Hornets.Bytom.Coffee13.TaskTrainingTwo.StringManipulator;
 
 namespace Toci.Hornets.UnitTests.Bytom.Coffee13
 {
-    [TestClass]
-    public class StringTest
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            CoffeeStringManipulations test1 = new CoffeeStringManipulations();
+	[TestClass]
+	public class StringTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			CoffeeStringManipulations test1 = new CoffeeStringManipulations();
 
 
 
-           Assert.IsTrue(test1.RunStringOperations("beata","ataeb").IsAnagram);
-           Assert.IsTrue(test1.RunStringOperations("beata", "aea").IsStringElementsInString);
-           Assert.IsTrue(test1.RunStringOperations("beata", "ata").IsStringInString);
+			Assert.IsTrue(test1.RunStringOperations("beata", "ataeb").IsAnagram);
+			Assert.IsTrue(test1.RunStringOperations("beata", "aea").IsStringElementsInString);
+			Assert.IsTrue(test1.RunStringOperations("beata", "ata").IsStringInString);
 
-        }
-    }
+		}
+	}
+
 }
