571b419f33ab212e0a1383acb717dfe78856bbc8
Vernathic <natalia.legarska@gmail.com>
2015-07-11 12:46:25 +0200

    fix tests in testproject

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Bytom/Vnth/VnthFirstUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Hornets.UnitTests.Bytom.Vnth
+{
+	[TestClass]
+	public class VnthFirstUnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			Assert.IsTrue(true);
+			Assert.IsTrue(false);
+
+		}
+	}
+}
