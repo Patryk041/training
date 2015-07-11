1b85920186244b1c1046b10380b213a96d708fde
Vernathic <natalia.legarska@gmail.com>
2015-07-11 21:14:26 +0200

    restore Vnth's unit test and partial solusion to homework 2 pesel validator

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Bytom/Vnth/VnthPeselValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Bytom.Vernathic.TrainingTwo;
+
+namespace Toci.Hornets.UnitTests.Bytom.Vnth
+{
+	[TestClass]
+	public class VnthPeselValidatorTest
+	{
+		[TestMethod]
+		public void VPeselValidatorTestMethod()
+		{
+			var validator = new VnthPeselValidator();
+
+			Assert.IsTrue(validator.IsPeselValid("92051414608"));
+			Assert.IsTrue(validator.IsPeselValid("92090607249"));
+			Assert.IsTrue(validator.IsPeselValid("39030719172"));
+			Assert.IsTrue(validator.IsPeselValid("75110913971"));
+		}
+	}
+}
