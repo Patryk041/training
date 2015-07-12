14858ef55e16284547e4d043659254ca838968d5
cCreature <cCreature@users.noreply.github.com>
2015-07-12 08:20:24 +0200

    Homework - jakeComplexStrings and improving mistakes in pesel validation

--- Eagles/TrainingTwo/jake/jakePeselValidation.php
+++ Eagles/TrainingTwo/jake/jakePeselValidation.php
@@ -104,7 +104,7 @@ class jakePeselValidation extends PeselValidation
         }
     }
 
-    function isLeapYear ($year) {
+    function isLeapYear($year) {
         if ($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0) {
             return true;
         }
