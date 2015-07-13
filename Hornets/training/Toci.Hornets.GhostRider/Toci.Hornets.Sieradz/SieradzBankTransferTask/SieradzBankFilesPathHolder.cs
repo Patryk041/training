0df4d9bbd434a7921a83e17408dc31c4931242c9
UCantTouchThis <adam.stawarz11@gmail.com>
2015-07-13 20:53:37 +0200

    BankTransfersTask things, FileOperations test

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/SieradzBankFilesPathHolder.cs
@@ -0,0 +1,8 @@
+﻿namespace Toci.Hornets.Sieradz.SieradzBankTransferTask
+{
+    public static class SieradzBankFilesPathHolder
+    {
+            //  relative patch from unit tests .exe to Sieradz Bank Transfer files
+        public const string path = @"..\..\..\Toci.Hornets.Sieradz\SieradzBankTransferTask\BankTransfersFiles\";
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/BankTransferTests/FileOperationsTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Sieradz.SieradzBankTransferTask;
+
+namespace Toci.Hornets.UnitTests.Sieradz.BankTransferTests
+{
+    [TestClass]
+    public class FileOperationsTest
+    {
+        [TestMethod]
+        public void SieradzFileOperationsTest()
+        {
+            var fileList = Directory.GetFiles(SieradzBankFilesPathHolder.path);
+
+            foreach (var file in fileList)
+            {
+                Debug.WriteLine(file);
+            }
+        }
+    }
+}
