86bd92129ee32fa1b608eef4bdcb3fe7dfb7cad5
kwadrat633 <kwadrat633@gmail.com>
2015-07-13 18:40:13 +0200

    no message

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Chojnecki/Kir/AndrzejBankTransfersParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Rzeszow.Chojnecki.Kir
+{
+    public class AndrzejBankTransfersParser : BankTransfersParser
+    {
+        public override List<BankTransfer> GetBankTransfers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
