470cf4dc710e2feaccb33098e36c8a44a594ffdc
Tomasz Szulczewski <szulczewski.tomasz@gmail.com>
2015-07-14 01:54:54 +0200

    StringManipulations

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Wroclaw/Tszulczewski/StringManipulationsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+using Toci.Hornets.Wroclaw.Tszulczewski.Training2HomeWork;
+
+namespace Toci.Hornets.UnitTests.Wroclaw.Tszulczewski
+{
+    [TestClass]
+    public class StringManipulationsTest
+    {
+        [TestMethod]
+        public void StringManipulationsTestMethod()
+        {
+            TszulczewskiStringManipulations obj1 = new TszulczewskiStringManipulations();
+            StringManipulationsResults stringmresults = obj1.RunStringOperations("Alunia", "Alunia");
+            Assert.IsFalse(stringmresults.IsStringInString);
+            Assert.IsFalse(stringmresults.IsStringElementsInString);
+            Assert.IsTrue(stringmresults.IsAnagram);
+        }
+         
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/Tszulczewski/Training2HomeWork/TszulczewskiStringManipulations.cs
@@ -0,0 +1,79 @@
+﻿using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Wroclaw.Tszulczewski.Training2HomeWork
+{
+    public class TszulczewskiStringManipulations : GhostRiderStringManipulationsBase
+    {
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            if (seek.Length > subject.Length)
+                return false;
+            if (seek.Equals(""))
+                return true;
+            
+            for (int i = 0; i < subject.Length-seek.Length; i++)
+            {
+                if (subject.Substring(i, seek.Length).Equals(seek))
+                    return true;
+            }
+            return false;
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            if (seek.Length > subject.Length)
+                return false;
+            if (seek.Equals(""))
+                return true;
+
+            int sublength = seek.Length;
+            char[] subjectChar = subject.ToCharArray();
+
+            for (int k = 0; k < seek.Length; k++)
+            {
+                for (int i = 0; i < subject.Length - 1; i++)
+                {
+                    if (seek[k] == subjectChar[i])
+                    {
+                        subjectChar[i] = ' ';
+                        sublength--;
+                    }
+                    else if (sublength == 0)
+                        return true;
+                }
+            }
+                return false;
+            //throw new System.NotImplementedException();
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            if (seek.Length != subject.Length)
+                return false;
+            return IsStringElementsInString(subject, seek);
+        }
+
+        protected override string GetNick()
+        {
+            return "Tszulczewski";
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            var result = new StringManipulationsResults();
+
+            result.Nick = GetNick();
+            result.Seek = seek;
+            result.Subject = subject;
+
+            result.Type = GetType();
+
+            result.IsStringInString = IsStringInString(subject, seek);
+            result.IsAnagram = IsStringAnagramOfString(subject, seek);
+            result.IsStringElementsInString = IsStringElementsInString(subject, seek);
+
+            return result;
+
+        }
+    }
+}
