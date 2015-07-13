118efe2551e67dbf026d1e926a051aa52e285b03
Foonesh <foonesh@gmail.com>
2015-07-13 23:32:32 +0200

    Bank task part one

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Foonesh/TrainingThree/BankTransfersParserFoonesh.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Rzeszow.Foonesh.TrainingThree
+{
+    class BankTransfersParserFoonesh:BankTransfersParser
+    {
+        public override List<BankTransfer> GetBankTransfers()
+        {
+            List<BankTransfer> bankTransferList = new List<BankTransfer>();
+            string bankTransfers = BankFileOperation.GetFileContent("C:\test");
+            foreach (String s in bankTransfers.Split())
+            {
+                bankTransferList.Add(GetTransferEntry(s));
+            }
+            return bankTransferList;
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+            BankTransfer bankTransfer = new BankTransfer();
+            bankTransfer.SourceBank = entry.Split(';')[0];
+            bankTransfer.DestinationBank = entry.Split(';')[1];
+            return bankTransfer;
+        }
+    }
+}
