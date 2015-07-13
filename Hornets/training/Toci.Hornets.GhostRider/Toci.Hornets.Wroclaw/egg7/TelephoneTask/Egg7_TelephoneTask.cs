3bbfad8e9871a4f62b96d928fbb357cf96c29be1
egg7 <michalbialek1995@interia.pl>
2015-07-13 12:26:15 +0200

    Egg7FirstHomework

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/egg7/TelephoneTask/Egg7_TelephoneTask.cs
@@ -0,0 +1,28 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Wroclaw.egg7.TelephoneTask
+{
+    class Egg7_TelephoneTask : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChoseMedium(int chosenOption)
+        {
+            switch (chosenOption)
+            {
+                case 1 : 
+                    return new PhotoCommunication();
+                case 2 :
+                    return new SmsCommunication();
+                case 3 :
+                    return new VoiceCommunication();
+                default :
+                    return null;
+            }
+        }
+        protected override string MyNick()
+        {
+            return "kollwing";
+        }
+    }
+}
