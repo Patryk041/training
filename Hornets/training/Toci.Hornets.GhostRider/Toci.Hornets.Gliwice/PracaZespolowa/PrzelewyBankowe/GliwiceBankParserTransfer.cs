0248d8df73b0e75c9cbe9a79c440d40c961e4299
Didi <kropka9313@gmail.com>
2015-07-14 01:29:09 +0200

    architektura dla zadania przelewy bankowe

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/GliwiceBankParserTransfer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe
+{
+    class GliwiceBankParserTransfer : BankTransfersParser 
+    {
+        public override List<BankTransfer> GetBankTransfers()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
