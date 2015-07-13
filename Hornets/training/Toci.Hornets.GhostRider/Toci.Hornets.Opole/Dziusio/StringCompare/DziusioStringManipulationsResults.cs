264816899d23ea51feab45011135832c284c062b
Andrzej <andrzej@elnetsystem.com>
2015-07-13 13:21:01 +0200

    hmmmmm

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Opole/Dziusio/StringCompare/DziusioStringManipulationsResults.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Opole.Dziusio.StringCompare
+{
+    public class DziusioStringManipulationsResults : GhostRiderStringManipulationsBase
+    {
+        public StringManipulationsResults Dziusio = new StringManipulationsResults();
+        
+
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            if (subject.Length != seek.Length)
+            {
+                return false;
+            }
+
+    
+            if (Equals(subject, seek))
+            {
+                return true;
+            }
+        }
+
+        protected override string GetNick()
+        {
+            return "Dziusio";
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
