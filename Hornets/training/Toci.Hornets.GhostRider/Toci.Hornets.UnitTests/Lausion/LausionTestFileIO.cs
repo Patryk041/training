060e2cdf991f758e9e742bf7390fc183bc7a8efe
Lausion <szymon67456@o2.pl>
2015-07-14 21:38:55 +0200

    File Operation

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Lausion/LausionTestFileIO.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Opole.OpoleKir;
+
+namespace Toci.Hornets.UnitTests.Lausion
+{
+    [TestClass]
+    public class LausionTestFileIO
+    {
+        [TestMethod]
+        public void Test()
+        {
+            OpoleFileOperation op=new OpoleFileOperation();
+            string s =
+                op.GetFileContent(
+                    @"D:\Szkolenie C#\SoutceTree\Hornets\training\Toci.Hornets.GhostRider\Toci.Hornets.UnitTests\Lausion\Check.txt");
+            Debug.Write(s);
+            
+        }
+    }
+}
