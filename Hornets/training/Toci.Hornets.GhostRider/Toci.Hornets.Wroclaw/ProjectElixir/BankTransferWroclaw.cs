2d46e0bc82cd4f70d1a951808cfe87cd45b364fa
Wojciech Grzesiak <wgrzesiak147@gmail.com>
2015-07-15 23:06:59 +0200

    some update

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/ProjectElixir/BankTransferWroclaw.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/ProjectElixir/BankTransferWroclaw.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.Kir;
 
 namespace Toci.Hornets.Wroclaw.ProjectElixir
 {
-    public class BankTransferWroclaw
+    public class BankTransferWroclaw : BankTransfer
     {
        
-            public string DestinationBank { get; set; }
-
+  
             public string SourceBank { get; set; }
+
+            public string DestinyBank { get; set; }
 
             public bool IsTransferSuccessful { get; set; }
 
