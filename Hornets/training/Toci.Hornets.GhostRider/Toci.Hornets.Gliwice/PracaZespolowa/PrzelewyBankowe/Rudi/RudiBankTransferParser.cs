343a213b896934720224cef8ea40ac59c6740ce3
Rudi <1010000i100@gmail.com>
2015-07-15 13:13:27 -0700

    parser i kilak rzeczy

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Rudi/RudiBankTransferParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Rudi/RudiBankTransferParser.cs
@@ -7,24 +7,28 @@ using Toci.Hornets.GhostRider.Kir;
 
 namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.Rudi
 {
-
-   
     public class RudiBankTransferParser : BankTransfersParser
     {
         public List<String> listaPrzelewow{get;set;}
 
-        public RudiBankTransferParser()
+        public RudiBankTransferParser(string path = @"..\\Didi\\DidiPrzelewy.txt")
         {
             BankFileOperation = new GliwiceFileOperation();
-            string input = BankFileOperation.GetFileContent("..\\Didi\\DidiPrzelewy.txt");
-
+            string input = BankFileOperation.GetFileContent(path);
             listaPrzelewow = new List<string>(input.Split('\n'));
-
         }
 
-        public override List<BankTransfer> GetBankTransfers()
+
+        public override List<RudiBankTransfer> GetBankTransfers()
         {
-            throw new NotImplementedException();
+            List<RudiBankTransfer> bankTransfers = new List<RudiBankTransfer>();
+
+            foreach(String przelew in listaPrzelewow)
+            {
+                bankTransfers.Add(new RudiBankTransfer(przelew.Split(';')));
+            }
+
+            return bankTransfers;
         }
 
         protected override BankTransfer GetTransferEntry(string entry)
