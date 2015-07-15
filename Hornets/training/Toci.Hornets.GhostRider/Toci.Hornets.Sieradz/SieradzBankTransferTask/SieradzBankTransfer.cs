fbb6e9222488faa2f131876c210f94cbd3db9844
TypowyAdam <adam0309@gmail.com>
2015-07-15 11:30:25 +0200

    Let's rock!

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/SieradzBankTransfer.cs
@@ -0,0 +1,10 @@
+﻿using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Sieradz.SieradzBankTransferTask
+{
+    public class SieradzBankTransfer : BankTransfer
+    {
+         //Tutaj dodamy property odnośnie danych przelewu, potem będziemy sobie je rzutować 
+        //Podzielił bym tutaj to na 3 grupy - nadawca, odbiorca i dane przelewu czyli kwota i jego tytuł
+    }
+}
