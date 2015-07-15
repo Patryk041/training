6cadb8e43196c842aa52bfd9756fe8d22e0f49dc
Rudi <1010000i100@gmail.com>
2015-07-15 10:34:22 -0700

    male czyszczenie

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Rudi/RudiBankTransferParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.Rudi
+{
+
+   
+    public class RudiBankTransferParser : BankTransfersParser
+    {
+        public List<String> listaPrzelewow{get;set;}
+
+        public RudiBankTransferParser()
+        {
+            BankFileOperation = new GliwiceFileOperation();
+            string input = BankFileOperation.GetFileContent("..\\Didi\\DidiPrzelewy.txt");
+
+            listaPrzelewow = new List<string>(input.Split('\n'));
+
+        }
+
+        public override List<BankTransfer> GetBankTransfers()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+            //zmienia ze strinka na banktransfer
+            throw new NotImplementedException();
+     
+        }
+    }
+}
