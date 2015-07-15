ce2d9717932522265b6f364413d177f86e7529aa
TypowyAdam <adam0309@gmail.com>
2015-07-15 18:54:55 +0200

    Adding pdf reference to the Sieradz project, some TransferHandle tests

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/TypowyAdam/PeselValidatorTest/TypowyAdamSampleTest.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Sieradz/TypowyAdam/PeselValidatorTest/TypowyAdamSampleTest.cs
@@ -2,7 +2,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices.ComTypes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.GhostRider.Kir;
 using Toci.Hornets.Legnica.Filip_Sokolowski.homeWork.peselValidator;
+using Toci.Hornets.Sieradz.SieradzBankTransferTask.mBank;
 using Toci.Hornets.Sieradz.TypowyAdam.UndergroundTasks;
 
 namespace Toci.Hornets.UnitTests.Sieradz.TypowyAdam.PeselValidatorTest
@@ -15,16 +17,9 @@ namespace Toci.Hornets.UnitTests.Sieradz.TypowyAdam.PeselValidatorTest
         [TestMethod]
         public void TestMethod1()
         {
-            TypowyAdamPeselValidator peselValidator = new TypowyAdamPeselValidator();
-             MethodInfo test = peselValidator.GetType().GetMethod("IsPeselValid");
-            var test1 = peselValidator.GetType().GetMethod("IsPeselValid");
-            
-               var test2 = test1.GetParameters();
-            var test3 = test1.ReturnType;
-            //var type = Type.ReflectionOnlyGetType(test2[0].ParameterType.FullName,true,true);
-            var type = test2[0].GetType();
 
-
+            var test = new TypowyAdamTransferHandle();
+            test.SendTransfer(new BankTransfer());
         }
 
     }
