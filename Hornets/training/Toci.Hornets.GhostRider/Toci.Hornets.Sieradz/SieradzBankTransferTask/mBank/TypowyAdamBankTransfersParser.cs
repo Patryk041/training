7980acb2b7b45be53355ecf226676d1b808c0f33
TypowyAdam <adam0309@gmail.com>
2015-07-15 20:49:37 +0200

    Spire.PDF fix, dlls added to the repo

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/mBank/TypowyAdamBankTransfersParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/mBank/TypowyAdamBankTransfersParser.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 using Toci.Hornets.GhostRider.Kir;
 using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
 
@@ -6,14 +8,39 @@ namespace Toci.Hornets.Sieradz.SieradzBankTransferTask.mBank
 {
     public class TypowyAdamBankTransfersParser : BankTransfersParser
     {
+#pragma warning disable 618
+        protected XmlDocument myString= new XmlDataDocument();
+#pragma warning restore 618
         public override List<BankTransfer> GetBankTransfers()
         {
-            throw new System.NotImplementedException();
+            BankFileOperation = new SieradzFileOperation();
+         
+            List<BankTransfer> mBankTransferList = new List<BankTransfer>();
+            myString.LoadXml(BankFileOperation.GetFileContent(SieradzBankFilesPathHolder.TransferFilesPath + @"mBankTransfers.xml"));
+            foreach (var childNode in myString.DocumentElement.ChildNodes)
+            {
+               // GetTransferEntry(myString.GetElementsByTagName(childNode.).Item(0).InnerXml);
+            }
+                
+            return mBankTransferList;
+
         }
 
         protected override BankTransfer GetTransferEntry(string entry)
         {
-            throw new System.NotImplementedException();
+            var mBankTransfer = new SieradzBankTransfer();
+            
+            //TODO it's possible to reduce that sphagetti?
+            mBankTransfer.SourceBank = "";
+            mBankTransfer.DestinationBank = "";
+            mBankTransfer.TransferInfo.Amount = "";
+            mBankTransfer.TransferInfo.DestinationAccountNumber = "";
+            mBankTransfer.TransferInfo.DestinationPersonName = "";
+            mBankTransfer.TransferInfo.SourceAccountNumber = "";
+            mBankTransfer.TransferInfo.TransferDate = "";
+            mBankTransfer.TransferInfo.TransferTitle = "";
+            return mBankTransfer;
+
         }
     }
 }
