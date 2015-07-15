cd09971b27a2ccf35bbd8b43ce512bf236ca1fa8
Vernathic <natalia.legarska@gmail.com>
2015-07-15 21:54:52 +0200

    Some changes in Banking System - add VnthTransfer model.

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Vernathic/Kir/VnthAccountInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Hornets.Bytom.Vernathic.Kir
+{
+	class VnthAccountInfo
+	{
+		public string AccountNumber { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string BankName { get; set; }
+	}
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Vernathic/Kir/VnthJsonSeliarizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace Toci.Hornets.Bytom.Vernathic.Kir
+{
+	public class VnthJsonSeliarizer
+	{
+		public DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(VnthTransfer));
+
+		//string jsonStream 
+		//serializer.
+
+	}
+}
