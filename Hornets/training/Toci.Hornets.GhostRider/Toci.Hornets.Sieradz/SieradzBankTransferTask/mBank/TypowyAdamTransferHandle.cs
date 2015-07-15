9057e5d9fe30d003f0f366f359d5122c5c78eb0f
TypowyAdam <adam0309@gmail.com>
2015-07-15 17:46:18 +0200

    Add handle and sample.xml transfer file

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/mBank/TypowyAdamTransferHandle.cs
@@ -0,0 +1,12 @@
+﻿using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Sieradz.SieradzBankTransferTask.mBank
+{
+    public class TypowyAdamTransferHandle :TransferHandle
+    {
+        protected override bool Send(BankTransfer transfer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
