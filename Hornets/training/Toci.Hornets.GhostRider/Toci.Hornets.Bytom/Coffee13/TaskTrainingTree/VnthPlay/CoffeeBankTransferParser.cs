045b29dcb475f1b0bde73ead640c6787e43e6915
Vernathic <natalia.legarska@gmail.com>
2015-07-17 01:00:41 +0200

    Add necessary files (Vnth Plays with Coffee XML serialization)

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/VnthPlay/CoffeeBankTransferParser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Bytom.Coffee13.TaskTrainingTree.VnthPlay
+{
+    public class Coffee2BankTransferParser : BankTransfersParser
+    {
+
+        // wczytac plik do stringu
+        // rozbic string na linie
+        // linie przetworzyc GetTransferEntry na typ BankTransfer
+        public override List<BankTransfer> GetBankTransfers()
+        {
+			var transfer = new Coffee2BankTransfer();
+
+            var file = new CoffeeFileOperation();
+            string transfers = file.GetFileContent(@"..\..\Coffee13\TaskTrainingTree\newTransfers.xml"); //path?\
+
+            return null;
+
+			//foreach iterujący po tablicy CoffeeBankTransferów
+
+	        GetTransferEntry(Coffee2XmlSerialization.SerializeObjectToStringEntry(transfer));
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry) //przyjmuje stringa transter (1 rekord), zwraca typ wypełniony danymi ze stringa
+        {
+			var transfer = new Coffee2BankTransfer();
+
+	        return transfer as BankTransfer;
+
+
+
+
+	        //todo: zwrócić CoffeeBankTransfer i scastować do BankTransfer (
+	        //return transfers as BankTransfer;
+        }
+
+    }
+}
