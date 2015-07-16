5226603e5c41922e3cda6014e17063e8d5aa80f2
TypowyAdam <adam0309@gmail.com>
2015-07-16 16:24:56 +0200

    Parser

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/mBank/TypowyAdamBankTransfersParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/mBank/TypowyAdamBankTransfersParser.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using Toci.Hornets.GhostRider.Kir;
 using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+using Toci.Hornets.Sieradz.UCantTouchThis.ExtensionMethods;
 
 namespace Toci.Hornets.Sieradz.SieradzBankTransferTask.mBank
 {
     public class TypowyAdamBankTransfersParser : BankTransfersParser
     {
-#pragma warning disable 618
-        protected XmlDocument myString= new XmlDataDocument();
-#pragma warning restore 618
+
         public override List<BankTransfer> GetBankTransfers()
         {
             BankFileOperation = new SieradzFileOperation();
-         
+            XmlDocument xmlFile= new XmlDataDocument();
             List<BankTransfer> mBankTransferList = new List<BankTransfer>();
-            myString.LoadXml(BankFileOperation.GetFileContent(SieradzBankFilesPathHolder.TransferFilesPath + @"mBankTransfers.xml"));
-            foreach (var childNode in myString.DocumentElement.ChildNodes)
+            xmlFile.LoadXml(BankFileOperation.GetFileContent(SieradzBankFilesPathHolder.TransferFilesPath + @"mBankTransfers.xml"));
+            foreach (var childNode in xmlFile.DocumentElement.ChildNodes)
             {
-               // GetTransferEntry(myString.GetElementsByTagName(childNode.).Item(0).InnerXml);
+                GetTransferEntry(xmlFile.GetElementsByTagName((string) childNode.GetType().GetProperty("Name").GetValue(childNode)).Item(0).InnerXml); 
             }
                 
             return mBankTransferList;
@@ -29,16 +29,20 @@ namespace Toci.Hornets.Sieradz.SieradzBankTransferTask.mBank
         protected override BankTransfer GetTransferEntry(string entry)
         {
             var mBankTransfer = new SieradzBankTransfer();
+            XmlDocument xmlNode = new XmlDataDocument();
+            string datePattern = "d.m.yyyy";
+            DateTime currentDate = DateTime.Now;
             
+            xmlNode.LoadXml(entry);
             //TODO it's possible to reduce that sphagetti?
-            mBankTransfer.SourceBank = "";
-            mBankTransfer.DestinationBank = "";
-            mBankTransfer.TransferInfo.Amount = "";
-            mBankTransfer.TransferInfo.DestinationAccountNumber = "";
-            mBankTransfer.TransferInfo.DestinationPersonName = "";
-            mBankTransfer.TransferInfo.SourceAccountNumber = "";
-            mBankTransfer.TransferInfo.TransferDate = "";
-            mBankTransfer.TransferInfo.TransferTitle = "";
+            mBankTransfer.SourceBank = xmlNode.GetElementsByTagName("SourceBank").Item(0).InnerText;
+            mBankTransfer.DestinationBank = xmlNode.GetElementsByTagName("DestinationBank").Item(0).InnerText;
+            mBankTransfer.TransferInfo.Amount = xmlNode.GetElementsByTagName("Amount").Item(0).InnerText;
+            mBankTransfer.TransferInfo.DestinationAccountNumber = xmlNode.GetElementsByTagName("DestinationAccountNumber").Item(0).InnerText;
+            mBankTransfer.TransferInfo.DestinationPersonName = xmlNode.GetElementsByTagName("DestinationPersonName").Item(0).InnerText;
+            mBankTransfer.TransferInfo.SourceAccountNumber = xmlNode.GetElementsByTagName("SourceAccountNumber").Item(0).InnerText;
+            mBankTransfer.TransferInfo.TransferDate = currentDate.ToString(datePattern);
+            mBankTransfer.TransferInfo.TransferTitle = xmlNode.GetElementsByTagName("TransferTitle").Item(0).InnerText;
             return mBankTransfer;
 
         }
