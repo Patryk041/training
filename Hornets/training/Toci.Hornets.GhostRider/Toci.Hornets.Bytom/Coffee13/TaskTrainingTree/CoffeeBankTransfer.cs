a5966d4bc9260c6e78f1ec533bf3a30832bac1fa
Coffee13 <mateuszw19911@wp.pl>
2015-07-16 22:32:53 +0200

    XmlSerializer method

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/CoffeeBankTransfer.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/CoffeeBankTransfer.cs
@@ -1,18 +1,59 @@
 ﻿using System;
 using System.Data;
+using System.Xml.Serialization;
 using Toci.Hornets.GhostRider.Kir;
 
 namespace Toci.Hornets.Bytom.Coffee13.TaskTrainingTree
 {
-    internal class CoffeeBankTransfer : BankTransfer
+    [Serializable()]
+    [XmlRoot("AllTransfers")]
+    public class CoffeeBankTransfer : BankTransfer
     {
-        public CoffeeAccountInfo SourceInfo { get; set; }
-        public CoffeeAccountInfo DestinationInfo { get; set; }
-        public decimal TransferAmount { get; set; }
-        public string TitleOfTransfer { get; set; }
-        public DateTime DateOfTransfer { get; set; }
-        public DateTime DateOfRecord { get; set; }
+        [XmlArray("Transfers")]
+        [XmlArrayItem("Transfer",typeof(Transfer))]
+        public Transfer[] Transfer { get; set; }
+    }
 
-        //type of transfer
+    [Serializable()]
+    public class Transfer
+    {
+        [XmlElement("SourceAccountNumber")]
+        public string SourceAccountNumber { get; set; }
+
+        [XmlElement("SourceName")]
+        public string SourceName { get; set; }
+
+        [XmlElement("SourceAddress")]
+        public string SourceAddress { get; set; }
+
+        [XmlElement("SourceBank")]
+        public string SourceBank { get; set; }
+
+        [XmlElement("DestinationAccountNumber")]
+        public string DestinationAccountNumber { get; set; }
+
+        [XmlElement("DestinationName")]
+        public string DestinationName { get; set; }
+
+        [XmlElement("DestinationAddress")]
+        public string DestinationAddress { get; set; }
+
+        [XmlElement("DestinationBank")]
+        public string DestinationBank { get; set; }
+
+        [XmlElement("TransferType")]
+        public string TransferType { get; set; }
+
+        [XmlElement("Amount")]
+        public decimal Amount { get; set; }
+
+        [XmlElement("TitleOfTransfer")]
+        public string TitleOfTransfer { get; set; }
+
+        [XmlElement("DateOfTransfer")]
+        public string DateOfTransfer { get; set; }
+
+        [XmlElement("DateOfRecord")]
+        public string DateOfRecord { get; set; }
     }
 }
--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/CoffeeBankTransferParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/CoffeeBankTransferParser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using Toci.Hornets.GhostRider.Kir;
 
 namespace Toci.Hornets.Bytom.Coffee13.TaskTrainingTree
@@ -12,46 +14,61 @@ namespace Toci.Hornets.Bytom.Coffee13.TaskTrainingTree
         // linie przetworzyc GetTransferEntry na typ BankTransfer
         public override List<BankTransfer> GetBankTransfers()
         {
-            List<BankTransfer> listOfTransfers = new List<BankTransfer>();
-            List<string> separatedFromMainString = new List<string>();
+            //List<BankTransfer> listOfTransfers = new List<BankTransfer>();
+            //List<string> separatedFromMainString = new List<string>();
 
             var file = new CoffeeFileOperation();
-            string transfers = file.GetFileContent(@"..\..\Coffee13\TaskTrainingTree\Transfers.xml");   //path?\
+            string transfers = file.GetFileContent(@"..\..\Coffee13\TaskTrainingTree\newTransfers.xml");   //path?\
 
-            separatedFromMainString = SeparateTransfers(transfers);
+            //separatedFromMainString = SeparateTransfers(transfers);
 
-            foreach (var entry in separatedFromMainString)
-            {
-                 listOfTransfers.Add(GetTransferEntry(entry));
-            }
-            return listOfTransfers; 
+            //foreach (var entry in separatedFromMainString)
+            //{
+            //     listOfTransfers.Add(GetTransferEntry(entry));
+            //}
+            //return listOfTransfers; 
+
+
+
+            return null;
+
+
         }
 
         protected override BankTransfer GetTransferEntry(string entry)  //tworzy ze stringa zawierającego pojedynczy transfer obiekt typu BankTransfer
         {
-            return null; //tymczasowo
-        }
+            CoffeeBankTransfer transfers = null;
 
-        protected List<string> SeparateTransfers(string transfers)    //dzieli string zawierajacy caly plik na transfery
-        {
-            List<string> sepTrans = new List<string>();
+            XmlSerializer serializer = new XmlSerializer(typeof(CoffeeBankTransfer));
 
-            string[] transfersDelimiters = { "<transfer>", "</transfer>" };
-            string[] separatedStrings = transfers.Split(transfersDelimiters, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var element in separatedStrings)
+            using (TextReader reader = new StringReader(entry))
             {
-                if (String.IsNullOrWhiteSpace(element))  //białe znaki pomiedzy transferami
-                {
-                    continue;
-                }
-                sepTrans.Add(element);
+                transfers = (CoffeeBankTransfer)serializer.Deserialize(reader);
             }
 
-            sepTrans.RemoveAt(0);                              //nagłówek
-            sepTrans.RemoveAt(sepTrans.Count - 1);            //koncowy node
-
-            return sepTrans;
+            return null;
         }
+
+        //protected List<string> SeparateTransfers(string transfers)    //dzieli string zawierajacy caly plik na transfery
+        //{
+        //    List<string> sepTrans = new List<string>();
+
+        //    string[] transfersDelimiters = { "<transfer>", "</transfer>" };
+        //    string[] separatedStrings = transfers.Split(transfersDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        //    foreach (var element in separatedStrings)
+        //    {
+        //        if (String.IsNullOrWhiteSpace(element))  //białe znaki pomiedzy transferami
+        //        {
+        //            continue;
+        //        }
+        //        sepTrans.Add(element);
+        //    }
+
+        //    sepTrans.RemoveAt(0);                              //nagłówek
+        //    sepTrans.RemoveAt(sepTrans.Count - 1);            //koncowy node
+
+        //    return sepTrans;
+        //}
     }
 }
