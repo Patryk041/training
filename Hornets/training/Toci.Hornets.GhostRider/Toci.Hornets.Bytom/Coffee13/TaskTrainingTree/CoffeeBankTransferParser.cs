02a41d1e6d37ab29be9d272ca76e17d11eb8322f
Coffee13 <mateuszw19911@wp.pl>
2015-07-16 22:54:53 +0200

    czystka

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/CoffeeBankTransferParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Coffee13/TaskTrainingTree/CoffeeBankTransferParser.cs
@@ -8,67 +8,34 @@ namespace Toci.Hornets.Bytom.Coffee13.TaskTrainingTree
 {
     public class CoffeeBankTransferParser : BankTransfersParser
     {
-        
+
         // wczytac plik do stringu
         // rozbic string na linie
         // linie przetworzyc GetTransferEntry na typ BankTransfer
         public override List<BankTransfer> GetBankTransfers()
         {
-            //List<BankTransfer> listOfTransfers = new List<BankTransfer>();
-            //List<string> separatedFromMainString = new List<string>();
 
             var file = new CoffeeFileOperation();
-            string transfers = file.GetFileContent(@"..\..\Coffee13\TaskTrainingTree\newTransfers.xml");   //path?\
-
-            //separatedFromMainString = SeparateTransfers(transfers);
-
-            //foreach (var entry in separatedFromMainString)
-            //{
-            //     listOfTransfers.Add(GetTransferEntry(entry));
-            //}
-            //return listOfTransfers; 
-
-
+            string transfers = file.GetFileContent(@"..\..\Coffee13\TaskTrainingTree\newTransfers.xml"); //path?\
 
             return null;
 
 
         }
 
-        protected override BankTransfer GetTransferEntry(string entry)  //tworzy ze stringa zawierającego pojedynczy transfer obiekt typu BankTransfer
+        protected override BankTransfer GetTransferEntry(string entry)
         {
             CoffeeBankTransfer transfers = null;
 
-            XmlSerializer serializer = new XmlSerializer(typeof(CoffeeBankTransfer));
+            XmlSerializer serializer = new XmlSerializer(typeof (CoffeeBankTransfer));
 
             using (TextReader reader = new StringReader(entry))
             {
-                transfers = (CoffeeBankTransfer)serializer.Deserialize(reader);
+                transfers = (CoffeeBankTransfer) serializer.Deserialize(reader);
             }
 
             return null;
         }
 
-        //protected List<string> SeparateTransfers(string transfers)    //dzieli string zawierajacy caly plik na transfery
-        //{
-        //    List<string> sepTrans = new List<string>();
-
-        //    string[] transfersDelimiters = { "<transfer>", "</transfer>" };
-        //    string[] separatedStrings = transfers.Split(transfersDelimiters, StringSplitOptions.RemoveEmptyEntries);
-
-        //    foreach (var element in separatedStrings)
-        //    {
-        //        if (String.IsNullOrWhiteSpace(element))  //białe znaki pomiedzy transferami
-        //        {
-        //            continue;
-        //        }
-        //        sepTrans.Add(element);
-        //    }
-
-        //    sepTrans.RemoveAt(0);                              //nagłówek
-        //    sepTrans.RemoveAt(sepTrans.Count - 1);            //koncowy node
-
-        //    return sepTrans;
-        //}
     }
 }
