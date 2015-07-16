29cc5e4246df0657ef33e4f1e7a72b068f64568d
Didi <kropka9313@gmail.com>
2015-07-16 16:47:35 +0200

    Poprawa błędów na TOCI Gliwice

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Rudi/RudiBankTransferParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Rudi/RudiBankTransferParser.cs
@@ -10,8 +10,8 @@ namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.Rudi
     public class RudiBankTransferParser : BankTransfersParser
     {
         public List<String> listaPrzelewow{get;set;}
-
-        public RudiBankTransferParser(string path = @"..\\Didi\\DidiPrzelewy.txt")
+#warning RUDI Poprawa funkcji 
+        public RudiBankTransferParser(string path = @"..\\Didi\\DidiPrzelewy.txt") //co to ma być?! :P czemu mój plik jest jako parametr domyślny? 
         {
             BankFileOperation = new GliwiceFileOperation();
             string input = BankFileOperation.GetFileContent(path);
@@ -19,23 +19,26 @@ namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.Rudi
         }
 
 
-        public override List<RudiBankTransfer> GetBankTransfers()
+        public override List<BankTransfer> GetBankTransfers()    
         {
+            // To jest funkcja napisana przez Bartka, Bartek dostraczył architekturę, którą my dziedziczym NIE MOŻEMY jej zmieniać 
+            //bo nam typ nie odpowiada, która ona zwraca. 
             List<RudiBankTransfer> bankTransfers = new List<RudiBankTransfer>();
 
             foreach(String przelew in listaPrzelewow)
             {
                 bankTransfers.Add(new RudiBankTransfer(przelew.Split(';')));
             }
-
-            return bankTransfers;
+            // tutaj należy robić cast na Bartka typ. 
+            return null;
         }
 
         protected override BankTransfer GetTransferEntry(string entry)
         {
             //zmienia ze strinka na banktransfer
             throw new NotImplementedException();
-     
+            //do soboty pola które łapią wyjątki mają być zaimplementowane albo chociaż napisany byle jaki returnn 
+            //tak aby na testach nie sypało Bartkowi 
         }
     }
 }
