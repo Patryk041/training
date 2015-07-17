9205148ba107038282d104e76987b7c4d0e732ae
Roman Suska <r.suska91@gmail.com>
2015-07-17 13:44:10 +0200

    Soltys zadanie Bank Transfer

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Soltys/Banks/SoltysPkoBankTransferHandle.cs
@@ -0,0 +1,19 @@
+﻿using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.Soltys.Banks
+{
+    public class SoltysPkoBankTransferHandle : TransferHandle
+    {
+        public SoltysPkoBankTransferHandle()
+        {
+            BankName = "PKO";
+        }
+
+        protected override bool Send(BankTransfer transfer)
+        {
+            transfer.IsTransferSuccessful = true;
+
+            return transfer.IsTransferSuccessful;
+        }
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/Soltys/SoltysBankTransfer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.Soltys
+{
+    public class SoltysBankTransfer : BankTransfer
+    {
+        public decimal Amount { get; set; }
+        public string ReceiverName { get; set; }
+        public string SenderName { get; set; }
+        public DateTime TransferDate { get; set; }
+        public string Title { get; set; }
+    }
+}
