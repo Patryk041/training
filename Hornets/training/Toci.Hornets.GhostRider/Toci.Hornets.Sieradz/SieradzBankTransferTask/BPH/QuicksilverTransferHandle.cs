5a9013ef29e8498b0971f06c7ef44582ace4c964
Natalia Radomska <natalia.a.radomska@gmail.com>
2015-07-17 13:55:17 +0200

    Bank task is done

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/BPH/QuicksilverTransferHandle.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/BPH/QuicksilverTransferHandle.cs
@@ -4,9 +4,33 @@ namespace Toci.Hornets.Sieradz.SieradzBankTransferTask.BPH
 {
     public class QuicksilverTransferHandle: TransferHandle
     {
+
+        public QuicksilverTransferHandle()
+        {
+            BankName = "BPH";
+        }
+
         protected override bool Send(BankTransfer transfer)
         {
-            throw new System.NotImplementedException();
+            var outTransfer = (SieradzBankTransfer) transfer;
+            string performedTransfers =
+
+                "Historia wykonanych przelewów: \n Numer konta nadawcy przelewu: " +
+                outTransfer.TransferInfo.SourceAccountNumber +
+                " \n Nazwisko nadawcy: " + outTransfer.TransferInfo.SourcePersonName +
+                " \n Numer konta odbiorcy przelewu: " + outTransfer.TransferInfo.DestinationAccountNumber +
+                " \n Nazwisko odbiorcy: " + outTransfer.TransferInfo.DestinationPersonName +
+                " \n Tytułem: " + outTransfer.TransferInfo.TransferTitle +
+                " \n Kwota: " + outTransfer.TransferInfo.Amount +
+                " \n Data wykonania: " + outTransfer.TransferInfo.TransferDate;
+            
+            
+            System.IO.StreamWriter file = new System.IO.StreamWriter(SieradzBankFilesPathHolder.TransferFilesPath + @"..\BPH\QuicksilverPerformed.txt",true);
+            file.WriteLine(performedTransfers);
+
+            file.Close();
+            transfer.IsTransferSuccessful = true;
+            return transfer.IsTransferSuccessful;
         }
     }
 }
