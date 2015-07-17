b8b636a7170edd39cd20545388c3202e46c11db5
MoWerr <matthew2601@gmail.com>
2015-07-18 01:30:14 +0200

    MoWerr Banks Homework

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/MoWerr/MoWerrBankTransferParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.MoWerr
+{
+    public class MoWerrBankTransferParser : BankTransfersParser
+    {
+        private const string FilePath = @"../../../Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/MoWerr/BankTransfers.txt";
+
+        public override List<BankTransfer> GetBankTransfers()
+        {
+            var fileOperation = new GliwiceFileOperation();
+            var fileContent = fileOperation.GetFileContent(FilePath);
+
+            var separators = new char[] {'\n', '\r'};
+
+            var transferEntries = fileContent.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            //reSharper faktycznie uczy programowania :D
+            return transferEntries.Select(GetTransferEntry).ToList();
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+            var entryArray = entry.Split(';');
+
+            var bankTransfer = new MoWerrBankTransfer
+            {
+                SourceBank = entryArray[0],
+                DestinationBank = entryArray[1],
+                SenderName = entryArray[2],
+                ReciverName = entryArray[3],
+                Amount = float.Parse(entryArray[4]),
+                TransferDate = DateTime.Parse(entryArray[5]),
+                Title = entryArray[6]
+            };
+
+            var transferHandler = MoWerrTransferHandlerFactory.GetTransferHandler(bankTransfer.DestinationBank);
+
+            if (transferHandler != null)
+            {
+                transferHandler.SendTransfer(bankTransfer);
+            }
+            else
+            {
+                bankTransfer.IsTransferSuccessful = false;
+            }
+
+            return bankTransfer;
+        }
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Gliwice/MoWerr/BankTransfers/MoWerrBankTransferTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe.MoWerr;
+
+namespace Toci.Hornets.UnitTests.Gliwice.MoWerr.BankTransfers
+{
+    [TestClass]
+    public class MoWerrBankTransferTest
+    {
+        [TestMethod]
+        public void MoWerrBankTransferParserTest()
+        {
+            var bankTransferParser = new MoWerrBankTransferParser();
+
+            var bankTransfers = bankTransferParser.GetBankTransfers();
+
+            Assert.IsTrue(bankTransfers[0].IsTransferSuccessful);
+            Assert.IsTrue(bankTransfers[1].IsTransferSuccessful);
+
+            Assert.AreEqual(bankTransfers[0].DestinationBank, "mBank");
+            Assert.AreEqual(bankTransfers[1].DestinationBank, "PKO");
+        }
+    }
+}
