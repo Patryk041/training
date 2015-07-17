7b6273d72548add9727504d552b26298eba89534
Foonesh <foonesh@gmail.com>
2015-07-17 14:19:59 +0200

    training three updates

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Foonesh/TrainingThree/BankTransfersParserFoonesh.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Foonesh/TrainingThree/BankTransfersParserFoonesh.cs
@@ -7,13 +7,14 @@ using Toci.Hornets.GhostRider.Kir;
 
 namespace Toci.Hornets.Rzeszow.Foonesh.TrainingThree
 {
-    class BankTransfersParserFoonesh:BankTransfersParser
+    public class BankTransfersParserFoonesh:BankTransfersParser
     {
         public override List<BankTransfer> GetBankTransfers()
         {
+            BankFileOperation = new FileOperationFoonesh();
             List<BankTransfer> bankTransferList = new List<BankTransfer>();
-            string bankTransfers = BankFileOperation.GetFileContent("C:\test");
-            foreach (String s in bankTransfers.Split())
+            string bankTransfers = BankFileOperation.GetFileContent(@"D:\Szkolenie C#\Code\Hornets\training\Toci.Hornets.GhostRider\Toci.Hornets.Rzeszow\Foonesh\TrainingThree\TransferData\TransferOne.txt");
+            foreach (String s in bankTransfers.Split('\n'))
             {
                 bankTransferList.Add(GetTransferEntry(s));
             }
@@ -22,9 +23,11 @@ namespace Toci.Hornets.Rzeszow.Foonesh.TrainingThree
 
         protected override BankTransfer GetTransferEntry(string entry)
         {
-            BankTransfer bankTransfer = new BankTransfer();
-            bankTransfer.SourceBank = entry.Split(';')[0];
-            bankTransfer.DestinationBank = entry.Split(';')[1];
+            BankTransfer bankTransfer = new BankTransfer
+            {
+                SourceBank = entry.Split(';')[0],
+                DestinationBank = entry.Split(';')[1]
+            };
             return bankTransfer;
         }
     }
