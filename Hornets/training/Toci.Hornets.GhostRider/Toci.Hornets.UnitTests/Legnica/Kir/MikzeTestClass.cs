513dfd8b98a68935c5222bbd8ba74acccd7f90b8
Michał Zembik <mikzepro@gmail.com>
2015-07-17 19:15:18 +0200

    TEST do banktransferu

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Legnica/Kir/MikzeTestClass.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Legnica/Kir/MikzeTestClass.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.GhostRider.Kir;
 using Toci.Hornets.Legnica.zadania_grupowe.Legnica_Kir.BankTransfersParsers;
 
 namespace Toci.Hornets.UnitTests.Legnica.Kir
@@ -11,6 +13,13 @@ namespace Toci.Hornets.UnitTests.Legnica.Kir
         {
             BzwbkBankTransfrsParser transfer = new BzwbkBankTransfrsParser();
             var bankTransfers = transfer.GetBankTransfers();
+
+            foreach (BankTransfer bankTransfer in bankTransfers)
+            {
+                Debug.WriteLine(bankTransfer.SourceBank);
+                Debug.WriteLine(bankTransfer.DestinationBank);
+                Debug.WriteLine(bankTransfer.IsTransferSuccessful);
+            }
 
         }
     }
