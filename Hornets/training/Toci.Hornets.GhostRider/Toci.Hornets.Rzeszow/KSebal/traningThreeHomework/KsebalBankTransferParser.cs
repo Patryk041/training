a78b6ab6c8373b1617f44fbc56b3ef978f92cdf0
Ksebal <konradtatar@interia.pl>
2015-07-17 23:20:40 +0200

    HausenArbaiten v. 1.0 (niegotowe)

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/traningThreeHomework/KsebalBankTransferParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.Kir;
+using Toci.Hornets.GhostRider.TrainingThree.Chor;
+
+namespace Toci.Hornets.Rzeszow.KSebal.traningThreeHomework
+{
+    public class KsebalBankTransferParser : BankTransfersParser
+    {
+        public override List<BankTransfer> GetBankTransfers()
+        {   //BankFileOperation = new FyFileOperation1();
+            var lines = BankFileOperation.GetFileContent(UserPath.find()+"\\SuperBank.txt").Split(new[] {"\n"}, StringSplitOptions.None);
+            return lines.Select(GetTransferEntry).ToList();
+           
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+           
+            var words = entry.Split(new []{" "}, StringSplitOptions.None);
+
+            return new KsebalBankTransfer(words);
+        }
+
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/traningThreeHomework/KsebalTransferHandle.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Rzeszow.KSebal.traningThreeHomework
+{
+    public class KsebalTransferHandle:TransferHandle
+    {
+
+
+       public KsebalTransferHandle()
+       {
+           BankName = "SuperBank";
+       }
+        
+        //private string path =
+          
+        protected override bool Send(BankTransfer transfer)
+        {   
+           // FindFile();
+            StreamWriter sw;
+            KsebalBankTransfer tr =(KsebalBankTransfer) transfer;
+            
+                
+            if (File.Exists(UserPath.find()+"raport.txt"))
+                using ( sw = File.AppendText(UserPath.find()))
+                {
+                    sw.WriteLine(tr.ConsumerTransferName + " to " + transfer.SourceBank);
+                    sw.Close();
+                    tr.IsTransferSuccessful = true;
+
+                }
+
+            else
+            {   sw = new StreamWriter(UserPath.find()+"raport.txt");
+                sw.WriteLine(tr.SenderTransferSurname+ " to " + tr.ConsumerTransferSurname);
+                sw.Close();
+                tr.IsTransferSuccessful = true;
+            }
+            return true;
+
+        }
+
+    }
+}
