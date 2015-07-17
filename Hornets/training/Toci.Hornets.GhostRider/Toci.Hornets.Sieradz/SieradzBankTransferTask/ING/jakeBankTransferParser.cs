60844462b8aa449e8192e278b5d54f2769f85f45
cCreature <cCreature@users.noreply.github.com>
2015-07-18 00:31:59 +0200

    Jake PeselValidator

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/SieradzBankTransferTask/ING/jakeBankTransferParser.cs
@@ -0,0 +1,13 @@
+﻿using Toci.Hornets.GhostRider.Kir;
+using Toci.Hornets.Sieradz.SieradzBankTransferTask.SieradzBankUtils;
+
+
+namespace Toci.Hornets.Sieradz.SieradzBankTransferTask.ING
+{
+    public class jakeBankTransferParser
+    {
+
+        protected const string BankName = "ING";
+
+    }
+}
