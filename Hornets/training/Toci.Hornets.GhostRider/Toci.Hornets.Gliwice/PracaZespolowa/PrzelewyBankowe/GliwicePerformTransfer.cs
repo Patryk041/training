a9ae15c30dce97ae2a6df9cb0ce28faa05d20e5b
Didi <kropka9313@gmail.com>
2015-07-17 19:05:54 +0200

    Czyszczenie

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/GliwicePerformTransfer.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/PracaZespolowa/PrzelewyBankowe/GliwicePerformTransfer.cs
@@ -13,39 +13,29 @@ namespace Toci.Hornets.Gliwice.PracaZespolowa.PrzelewyBankowe
    public class GliwicePerformTransfer : PerformTransfers
    {
        private List<BankTransfersParser> _bankTransfersList;
-<<<<<<< HEAD
        private DidiBankTransferParser _DidTransfers;
        private RudiBankTransferParser _RudiTransfers;
        private SoltysBankTransferParser _SoltysTransfers;
-=======
 
        //private DidiBankTransferParser _DidTransfers;
        //private RudiBankTransferParser _RudiTransfers;
->>>>>>> master
 
 
        public GliwicePerformTransfer()
        {
            _bankTransfersList = new List<BankTransfersParser>();
-<<<<<<< HEAD
+
            _DidTransfers = new DidiBankTransferParser();
            _RudiTransfers = new RudiBankTransferParser();
            _SoltysTransfers = new SoltysBankTransferParser();
        }
 
-       protected override List<BankTransfersParser> GetAllParsers()
-       {
-=======
-           //_DidTransfers = new DidiBankTransferParser();
-           //_RudiTransfers = new RudiBankTransferParser();
-       }
 
         protected override List<BankTransfersParser> GetAllParsers()
         {
             ParsersGenerator generator = new ParsersGenerator();
 
             _bankTransfersList = generator.GetList("Gliwice");
->>>>>>> master
 
            _bankTransfersList.Add(_DidTransfers);
            _bankTransfersList.Add(_RudiTransfers);
