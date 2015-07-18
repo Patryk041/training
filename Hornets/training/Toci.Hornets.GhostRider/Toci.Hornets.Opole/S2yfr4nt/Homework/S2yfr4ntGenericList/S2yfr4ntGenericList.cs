3285fe1e73bf05631e2e452131b4cd58737df75b
szyfr4nt <Nirvana007@wp.pl>
2015-07-18 14:48:38 +0200

    18 minutes after deadline :D

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Opole/S2yfr4nt/Homework/S2yfr4ntGenericList/S2yfr4ntGenericList.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Opole/S2yfr4nt/Homework/S2yfr4ntGenericList/S2yfr4ntGenericList.cs
@@ -1,4 +1,5 @@
-﻿using Toci.Hornets.GhostRider.TrainingFour.Generics;
+﻿using System.Linq;
+using Toci.Hornets.GhostRider.TrainingFour.Generics;
 
 namespace Toci.Hornets.Opole.S2yfr4nt.Homework.S2yfr4ntGenericList
 {
@@ -6,12 +7,50 @@ namespace Toci.Hornets.Opole.S2yfr4nt.Homework.S2yfr4ntGenericList
     {
         public override bool Add(TItems item)
         {
-            throw new System.NotImplementedException();
+            if(items.Length == 0)
+                items = new[]{item};
+            else
+            {
+                TItems[] array = new TItems[items.Length];
+                for (int i = 0; i < items.Length-1; i++)
+                {
+                    array[i] = items[i];
+                }
+                array[array.Length] = item;
+                items = array;
+            }
+            
+            return true;
         }
 
         public override bool Remove(TItems item)
         {
-            throw new System.NotImplementedException();
+            if (items.Length == 0)
+                return false;
+           // TItems[] array = new TItems[items.Length-1];
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!items[i].Equals(item))
+                {
+                    RemoveItem(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void RemoveItem(int indexOfItem)
+        {
+            TItems[] array = new TItems[items.Length - 1];
+            for (int i = 0, j=0; i < items.Length; i++, j++)
+            {
+                if (i != indexOfItem)
+                {
+                    array[j] = items[i];
+                }
+                else j--;
+            }
+            items = array;
+
         }
     }
 }
