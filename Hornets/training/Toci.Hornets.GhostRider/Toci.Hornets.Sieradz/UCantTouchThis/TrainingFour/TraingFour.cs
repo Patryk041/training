82dcfcfa70e97aa5ee55ef7379b6a6d77723cb3c
UCantTouchThis <adam.stawarz11@gmail.com>
2015-07-18 14:01:12 +0200

    Majonez

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Sieradz/UCantTouchThis/TrainingFour/TraingFour.cs
@@ -0,0 +1,20 @@
+﻿using Toci.Hornets.GhostRider.TrainingFour.Generics;
+using Toci.Hornets.Sieradz.Duch.Traning;
+
+namespace Toci.Hornets.Sieradz.UCantTouchThis.TrainingFour
+{
+    public class TraingFour<TItems> : GhostRiderGenericList<TItems>
+    {
+        public override bool Add(TItems item)
+        {
+            items = DuchGenListUtil<TItems>.ListMagic(items, ListEnum.Add, item);
+            return true;
+        }
+
+        public override bool Remove(TItems item)
+        {
+            items = DuchGenListUtil<TItems>.ListMagic(items, ListEnum.Remove, item);
+            return true;
+        }
+    }
+}
