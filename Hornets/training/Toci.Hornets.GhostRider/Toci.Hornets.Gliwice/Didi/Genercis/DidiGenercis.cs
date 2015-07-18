88bb06fbb8f2f44f09c308b9da383269c2b07480
Didi <kropka9313@gmail.com>
2015-07-18 14:26:44 +0200

    genercis

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/Didi/Genercis/DidiGenercis.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/Didi/Genercis/DidiGenercis.cs
@@ -3,23 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Toci.Hornets.GhostRider.TrainingFour.Generics;
 
 namespace Toci.Hornets.Gliwice.Didi.Genercis
 {
-    public class DidiGenercis<Titem> : GhostRiderGenericList<Titem>
+    public class DidiGenercis<T> : GhostRiderGenericList<T>
     {
-       
-        public override bool Add(Titem item)
+        private int _top;
+        private const int Capacity = 20;
+
+        public DidiGenercis()
         {
-            return Add(item); 
+            _top = 0;
+            items = new T[Capacity];
         }
 
-        public override bool Remove(Titem item)
+        public override bool Add(T item)
         {
-            return Remove(item); 
+            if (_top == Capacity)
+                return false;
+           items[_top++] = item;
+           return true;
+        }
+
+        public override bool Remove(T item)
+        {
+            var tab = items.Where(val => !val.Equals(item)).ToArray();            
+            if (tab.Length != items.Length)
+            {
+                _top -= items.Length - tab.Length;
+                items = tab;
+                return true;
+            }
+            return false;
+
         }
     }
 }
