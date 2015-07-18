d8c727afd83f83c80753f87627eda6f45dac2fd5
Didi <kropka9313@gmail.com>
2015-07-18 14:15:07 +0200

    poprawka

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/Didi/Genercis/DidiGenercis.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/Didi/Genercis/DidiGenercis.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Toci.Hornets.GhostRider.TrainingFour.Generics;
 
 namespace Toci.Hornets.Gliwice.Didi.Genercis
 {
-    public class DidiGenercis : GhostRiderGenericList<int>
+    public class DidiGenercis<Titem> : GhostRiderGenericList<Titem>
     {
-        public override bool Add(int item)
+       
+        public override bool Add(Titem item)
         {
-          return Add(item);  
+            return Add(item); 
         }
 
-        public override bool Remove(int item)
+        public override bool Remove(Titem item)
         {
             return Remove(item); 
         }
