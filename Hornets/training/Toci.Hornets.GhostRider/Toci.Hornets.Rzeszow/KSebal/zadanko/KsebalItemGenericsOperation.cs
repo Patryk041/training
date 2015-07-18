acd7d4d545230fbb72a37bbd765781651f4c8594
Ksebal <konradtatar@interia.pl>
2015-07-18 14:26:42 +0200

    zadanko ;o

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/zadanko/KsebalItemGenericsOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.TrainingFour.Generics;
+
+namespace Toci.Hornets.Rzeszow.KSebal.zadanko
+{
+   public class KsebalItemGenericsOperation<TItem>:GhostRiderGenericList<TItem> where TItem:IEnumerable<TItem>,ICollection<TItem>
+   {
+       public override bool Add(TItem item)
+       {
+           items=new TItem[items.Length+1];
+           items.SetValue(item,items.Length);
+           return true;
+       }
+
+       public override bool Remove(TItem item)
+       {
+           return true;
+
+       }
+    }
+
+   
+}
