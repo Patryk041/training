ad9ad1019fd678f1eb22cac2767ed5b433cc8189
szyfr4nt <Nirvana007@wp.pl>
2015-07-18 14:20:00 +0200

    no message

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Opole/S2yfr4nt/Homework/S2yfr4ntGenericList/S2yfr4ntGenericList.cs
@@ -0,0 +1,17 @@
+﻿using Toci.Hornets.GhostRider.TrainingFour.Generics;
+
+namespace Toci.Hornets.Opole.S2yfr4nt.Homework.S2yfr4ntGenericList
+{
+    public class S2yfr4ntGenericList<TItems>: GhostRiderGenericList<TItems>
+    {
+        public override bool Add(TItems item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Remove(TItems item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
