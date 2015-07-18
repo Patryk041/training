576246ba127085e2d5a7be178fbedaa6856fc66c
Didi <kropka9313@gmail.com>
2015-07-18 14:09:37 +0200

    Genercis

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Gliwice/Didi/Genercis/DidiGenercis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.TrainingFour.Generics;
+
+namespace Toci.Hornets.Gliwice.Didi.Genercis
+{
+    public class DidiGenercis : GhostRiderGenericList<int>
+    {
+        public override bool Add(int item)
+        {
+          return Add(item);  
+        }
+
+        public override bool Remove(int item)
+        {
+            return Remove(item); 
+        }
+    }
+}
