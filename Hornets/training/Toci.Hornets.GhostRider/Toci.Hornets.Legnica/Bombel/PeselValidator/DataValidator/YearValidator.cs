02c999c3143cd068a444ed92fd83b2f5b3b7047c
Denis <dyniol_23@tlen.pl>
2015-07-18 04:35:46 +0200

    Bombel PeselValidator

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Legnica/Bombel/PeselValidator/DataValidator/YearValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Toci.Hornets.Legnica.Bombel.PeselValidator.DateValidator
+{
+    public class YearValidator
+    {
+        private const int Min = 1800;
+        private const int Max = 2299;
+
+        public static bool ValidYear(int year)
+        {
+            return (year >= Min && year <= Max);
+        }
+
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Legnica/Bombel/BombelUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Legnica.Bombel.PeselValidator;
+
+namespace Toci.Hornets.UnitTests.Legnica.Bombel
+{
+    [TestClass]
+    public class BombelUnitTest
+    {
+        [TestMethod]
+        public void TestPeselValidator()
+        {
+            var validator = new BombelPeselValidator();
+
+            Assert.IsTrue(validator.IsPeselValid("93101901138"));
+            Assert.IsTrue(validator.IsPeselValid("69910200924"));
+            Assert.IsTrue(validator.IsPeselValid("09062809286"));
+
+            Assert.IsFalse(validator.IsPeselValid("44051401358"));
+            Assert.IsFalse(validator.IsPeselValid("4405140158"));
+            Assert.IsFalse(validator.IsPeselValid("93131901138"));
+        }
+    }
+}
