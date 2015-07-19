5725e060141135ac4ed545e5defa1173ab6c4e80
kwadrat633 <kwadrat633@gmail.com>
2015-07-19 20:37:51 +0200

    zadanko z soboty

--- Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Chojnecki/Kir/AndrzejBankTransfersParser.cs
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Chojnecki/Kir/AndrzejBankTransfersParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Toci.Hornets.GhostRider.Kir;
 
 namespace Toci.Hornets.Rzeszow.Chojnecki.Kir
@@ -7,13 +8,23 @@ namespace Toci.Hornets.Rzeszow.Chojnecki.Kir
     {
         public override List<BankTransfer> GetBankTransfers()
         {
-            throw new System.NotImplementedException();
+           var transfersList = new List<BankTransfer>();
+           const string path = "C:\\Hornets\\Hornets\\training\\Toci.Hornets.GhostRider\\Toci.Hornets.Rzeszow\\Chojnecki\\Kir\\Przelewy.txt";
+           var bankTransfers = new AndrzejFileOperation().GetFileContent(path);
+           foreach (String s in bankTransfers.Split())
+           {
+               transfersList.Add(GetTransferEntry(s));
+           }
+           return transfersList;
         }
 
         protected override BankTransfer GetTransferEntry(string entry)
         {
 
-            throw new System.NotImplementedException();
+            BankTransfer bankTransfer = new BankTransfer();
+            bankTransfer.SourceBank = entry.Split(';')[0];
+            bankTransfer.DestinationBank = entry.Split(';')[1];
+            return bankTransfer;
         }
 
 
