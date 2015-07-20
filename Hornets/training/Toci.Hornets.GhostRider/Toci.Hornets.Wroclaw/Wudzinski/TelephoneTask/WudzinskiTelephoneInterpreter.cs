948c90f8562de69bc849c95a69954027b0cc6d43
Yarlaxle <adaswudzinski94@gmail.com>
2015-07-20 08:04:31 +0200

    telephone Interpreter wudzinski

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Wroclaw/Wudzinski/TelephoneTask/WudzinskiTelephoneInterpreter.cs
@@ -0,0 +1,34 @@
+﻿using Toci.Hornets.GhostRider.InterfacesExtending;
+using Toci.Hornets.GhostRider.TElephoneCommunication;
+using Toci.Hornets.GhostRider.YourWork.TelephoneTask;
+
+namespace Toci.Hornets.Wroclaw.Wudzinski.TelephoneTask
+{
+    public class WudzinskiTelephoneInterpreter : TelephoneInterpreter
+    {
+        protected override ITelephoneCommunication ChooseMedium(int chosenOption)
+        {
+            if (chosenOption == 1)
+            {
+                return new PhotoCommunication();
+            }
+            else if (chosenOption == 1)
+            {
+                return new SmsCommunication();
+            }
+            else if (chosenOption == 1)
+            {
+                return new VoiceCommunication();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected override string MyNick()
+        {
+            return "Wudziński";
+        }
+    }
+}
