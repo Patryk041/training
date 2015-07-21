d57bb97bc6803f6ba46fdd40129dac94bf3ab01c
Vernathic <natalia.legarska@gmail.com>
2015-07-21 19:27:55 +0200

    missing files?

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Common/DrHouse/Logic/DbHandle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Hornets.Bytom.Common.DrHouse.Logic
+{
+	public abstract class DbHandle
+	{
+		// todo: work on it
+		public abstract bool Insert();
+		public abstract bool Delete();
+		public abstract bool Fetch();
+
+		//http://www.codeproject.com/Articles/4416/Beginners-guide-to-accessing-SQL-Server-through-C
+	}
+}
