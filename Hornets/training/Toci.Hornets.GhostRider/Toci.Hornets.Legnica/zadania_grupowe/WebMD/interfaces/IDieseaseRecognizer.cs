3a787a569cbd446407c18d7228ba02c2f72a9cd6
Patryk Stulczewski <patrykj122@o2.pl>
2015-07-21 19:45:19 +0200

    architektura

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Legnica/zadania_grupowe/WebMD/interfaces/IDieseaseRecognizer.cs
@@ -0,0 +1,14 @@
+﻿using Toci.Hornets.Legnica.zadania_grupowe.WebMD.Data;
+
+namespace Toci.Hornets.Legnica.zadania_grupowe.WebMD.interfaces
+{
+    public interface IDieseaseRecognizer
+    {
+        /*nie wiem, czy klasa implementujaca powinna zwracac wykryta 
+         * chorobe, czy sama cos z tym robic. Zostawiam tak ze wzgledu 
+         * na to, ze po wykryciu choroby mozemy np sprawdzic, ktory najblizszy
+         * szpital ma wolne miejsce, lub ktora apteka ma wymagane lekarstwa
+         */
+        void Recognize(PatientInforamtion patientInforamtion); 
+    }
+}
