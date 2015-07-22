77d68d6e83480291ca9669b045f677bb388d4e63
Denis <dyniol_23@tlen.pl>
2015-07-22 19:02:11 +0200

    Bombel StringManipulator

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Legnica/Bombel/StringManipulator/BombelStringManipulator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Toci.Hornets.GhostRider.YourWork.TasksTrainingTwo;
+
+namespace Toci.Hornets.Legnica.Bombel.StringManipulator
+{
+    public class BombelStringManipulator : GhostRiderStringManipulationsBase
+    {
+        protected override bool IsStringInString(string subject, string seek)
+        {
+            return subject.Equals(seek) || subject.Contains(seek);
+        }
+
+        protected override bool IsStringElementsInString(string subject, string seek)
+        {
+            foreach (var element in seek)
+            {
+                if (!subject.Contains(element)) return false;
+                subject.Remove(element);
+            }
+            return true;
+        }
+
+        protected override bool IsStringAnagramOfString(string subject, string seek)
+        {
+            if (subject.Length != seek.Length) return false;
+            return StringSort(subject).Equals(StringSort(seek));
+        }
+
+        protected override string GetNick()
+        {
+            return "Bombel";
+        }
+
+        public override StringManipulationsResults RunStringOperations(string subject, string seek)
+        {
+            return Run(subject.ToLower(), seek.ToLower()); 
+        }
+
+        protected string StringSort(string str)
+        {
+            var tab = str.ToCharArray();
+            Array.Sort(tab);
+            return new string(tab);
+        }
+    }
+}
