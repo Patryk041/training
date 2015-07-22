db60726f3b78915c325bef98e4efdc5eda2341ba
Ksebal <konradtatar@interia.pl>
2015-07-22 14:18:35 +0200

    Homework v.1

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/TrainingFourHomework/Disease.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Toci.Hornets.Rzeszow.KSebal.TrainingFourHomework
+{
+    public abstract class Disease
+    {
+        public List<Research> researches;
+        public List<Sympthom> symptomy;
+
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/TrainingFourHomework/Sympthom.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Toci.Hornets.Rzeszow.KSebal.TrainingFourHomework
+{
+    public class Sympthom
+    {
+        private Dictionary<string, double> PainfulScale;
+        private string other;
+
+    }
+}
