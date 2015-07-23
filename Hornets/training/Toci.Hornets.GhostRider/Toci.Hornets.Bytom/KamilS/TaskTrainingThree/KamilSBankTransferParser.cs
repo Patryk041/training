2a97c3455107b8cc2bd0d17e38d5ce043837015c
Kamil Sołtysik <kamilsoltysik@gmail.com>
2015-07-24 00:44:49 +0200

    kir commit

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/KamilS/TaskTrainingThree/KamilSBankTransferParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Hornets.GhostRider.Kir;
+
+namespace Toci.Hornets.Bytom.KamilS.TaskTrainingThree
+{
+    class KamilSBankTransferParser : BankTransfersParser
+    {
+        public override List<BankTransfer> GetBankTransfers()
+        {
+            // wczytac plik do stringu
+            // rozbic string na linie
+            // linie przetworzyc GetTransferEntry na typ BankTransfer
+            BankFileOperation = new KamilSFileOperation();
+            List<BankTransfer> bankTransferList = new List<BankTransfer>();
+            string bankTransfers = BankFileOperation.GetFileContent(@"..\..\KamilS\TaskTrainingThree\TaskThree.txt");
+            foreach (String s in bankTransfers.Split('\n'))
+            {
+                bankTransferList.Add(GetTransferEntry(s));
+            }
+            return bankTransferList;
+        }
+
+        protected override BankTransfer GetTransferEntry(string entry)
+        {
+            BankTransfer bankTransfer = new BankTransfer
+            {
+                SourceBank = entry.Split(';')[0],
+                DestinationBank = entry.Split(';')[1]
+            };
+            return bankTransfer;
+        }
+    }
+}
