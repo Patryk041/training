52db5654e3c9487b2b1f267c72dd9fc2bd9de36b
Polak <polak1111@o2.pl>
2015-07-23 22:36:35 +0200

    commity

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/TrainingFourHomework/DownloadData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Hornets.Rzeszow.KSebal.TrainingFourHomework
+{
+   public abstract  class DownloadData
+   {
+       public abstract Dictionary<string, double> patientResults();
+
+   }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Rzeszow/Chojnecki/tFour/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Rzeszow.Chojnecki.trainingFour;
+
+namespace Toci.Hornets.UnitTests.Rzeszow.Chojnecki.tFour
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var gen = new AndrzejGen<string>();
+            var gen2 = new AndrzejGen<int>();
+            gen.Add("1");
+            gen.Add("2");
+            gen.Add("3");
+
+            gen2.Add(2);
+            gen2.Add(6);
+            gen2.Add(7);
+
+            gen2.Remove(2);
+            gen2.Add(4);
+        }
+    }
+}
