6d937e032c3696affb85d7286d53e051ce6abae2
Foonesh <foonesh@gmail.com>
2015-07-24 19:56:19 +0200

    initial Disease arch

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/Foonesh/TrainingFour/Symptom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Hornets.Rzeszow.Foonesh.TrainingFour
+{
+    public enum Symptom
+    {
+        Coughing,
+        Sneezing,
+        Temperature,
+        Pain
+    }
+}
