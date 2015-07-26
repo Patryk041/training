f6c91dfac976874b32b2598394bd3a1f8e129f0d
Amman <amadeusz.opac@gmail.com>
2015-07-26 19:46:34 +0200

    Training four catchup

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.UnitTests/Legnica/Amman/AmmanUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Hornets.Legnica.Amman.DelegatesSandbox;
+
+namespace Toci.Hornets.UnitTests.Legnica.Amman
+{
+    [TestClass]
+    public class AmmanUnitTests
+    {
+        [TestMethod]
+        public void DelegatesSandboxTest()
+        {
+            var delegates = new DelegatesSandbox();
+            delegates.Test();
+        }
+    }
+}
