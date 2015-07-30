eb8bc7a21f8b3f13efe47b0663b8cf0484e7499f
Ksebal <konradtatar@interia.pl>
2015-07-30 11:57:37 +0200

    Morfologia

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Rzeszow/KSebal/NFZ/Morphology.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Toci.Hornets.GhostRider.NFZ.Przychodnia.Base.Interface;
+using Toci.Hornets.GhostRider.NFZ.Przychodnia.Base.Types;
+using Toci.Hornets.GhostRider.NFZ.Przychodnia.ExaminationManagement.Interfaces;
+using Toci.Hornets.Rzeszow.KSebal.TrainingFourHomework;
+
+namespace Toci.Hornets.Rzeszow.KSebal.NFZ
+{   
+    
+    public class Morphology: IExamination<double>
+    {
+        private Examination _data;
+        private Dictionary<string, double> _resultDictionary;
+
+
+        public Morphology()
+        {
+            _data = new Examination();
+            _resultDictionary=new Dictionary<string, double>();   
+        }
+
+        public Morphology(Examination data)
+        {   
+            _resultDictionary=new Dictionary<string, double>();
+            _data = data;
+        }
+
+        public Morphology(Examination data, Dictionary<string,double> morphology )
+        {
+            _data = data;
+            _resultDictionary = morphology;
+        }
+
+        public Examination GetExaminationData()
+        {
+            return _data;
+        }
+
+        public Dictionary<string, double> GetResultDictionary()
+        {
+            return _resultDictionary;
+        }
+
+        public void AddBloodExam(string name,double value)
+        {
+            _resultDictionary.Add(name,value);
+        }
+
+        public bool ChangeValue(string name, double value)
+        {
+            if (!_resultDictionary.ContainsKey(name)) return false;
+            _resultDictionary[name] = value;
+            return true;
+        }
+
+    }
+
+
+ }
