276bee8bbb0de34de6d52747626c5efd41553553
Tomasz Lach <tomaszlach86@gmail.com>
2015-08-01 19:17:09 +0200

    funkcje stringowe z lekcji pierwszej zrobione

--- Eagles/TrainingOne/tomasz_lach/stringi.php
+++ Eagles/TrainingOne/tomasz_lach/stringi.php
@@ -18,7 +18,30 @@ function tomekL_reverse($text)
 echo tomekL_reverse("natalia");
 
 
-function tomekL_substring($text, $miejsce, $dlugosc)
+function tomekL_substring($text, $count)
 {
+	$output = "";
+
+	for($i = 0; $i < $count; $i++)
+	{
+		$output .= $text[$i];
+	}
+	return $output."\n";
+}
+
+echo tomekL_substring("Edward", 4);
+
+function tomekL_strlen($text)//isset
+{
+	$output = 0;
+	for($i=0; isset($text[$i]); $i++)
+	{
+		$output = $i+1;//bo elementy w tablicy indeksowane są od zera
+	}
+
+	return $output."\n";
 
 }
+
+echo tomekL_strlen("abcdefghij");
+echo tomekL_strlen("");
