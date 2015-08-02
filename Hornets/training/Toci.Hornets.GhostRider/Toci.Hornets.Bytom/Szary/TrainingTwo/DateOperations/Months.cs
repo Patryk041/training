1a0af8371ebf14e0b8f62001d648977baf1d6a4d
Szary <opentany.1972@gmail.com>
2015-08-02 15:09:56 +0200

    zaległa praca domowa 1/3

---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Szary/TrainingTwo/DateOperations/Months.cs
@@ -0,0 +1,10 @@
+﻿namespace Toci.Hornets.Bytom.Szary.TrainingTwo.DateOperations
+{
+    public class Months
+    {
+        public static bool Month(int month)
+        {
+            return month % 20 < 13 && month > 0;
+        }
+    }
+}
---
+++ Hornets/training/Toci.Hornets.GhostRider/Toci.Hornets.Bytom/Szary/TrainingTwo/StringTask/SortString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Toci.Hornets.Bytom.Szary.TrainingTwo.StringTask
+{
+    public class SortString
+    {
+        public char[] Sort(string word)
+        {
+            var wordChar = word.ToCharArray();
+
+            Array.Sort(wordChar);
+
+            return wordChar;
+        } 
+    }
+}
