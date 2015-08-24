11f15e23d1720b260880642d6682559b26e4a6a4
kwadrat633 <kwadrat633@gmail.com>
2015-08-24 09:29:26 +0200

    Banki

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/AndzejC/Banki/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace Startup.TrainingOneHomeworks.AndzejC.Banki
+{
+    public class Transaction
+    {
+        public string SenderAccNumber{ get; set; }
+        public string ReceiverAccNumber { get; set; }
+        
+        
+
+
+
+    }
+}
---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/AndzejC/Banki/TransactionMenager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Startup.TrainingOneHomeworks.AndzejC.Banki
+{
+    public class TransactionMenager
+    {
+        protected LinkedList<Transaction> TransferList;      
+    }
+
+
+}
