4e972e2b4c6ef29983a92c3fbff2930496f815ac
iwonam <iwonaw87@gmail.com>
2015-08-26 19:52:09 +0200

    Banki Iwona

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/IwonaM/Banks/BankTransaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Startup.TrainingOneHomeworks.IwonaM.Banks
+{
+    public abstract class BankTransaction
+    {
+     
+        //saldo metoda
+        public virtual void InputTransaction()
+        {
+
+        }
+
+        public virtual void OutputTransaction()
+        {
+
+        }
+
+    }
+}
