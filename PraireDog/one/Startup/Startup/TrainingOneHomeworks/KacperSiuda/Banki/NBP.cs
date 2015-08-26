27486f6420ba3d2eb34775f04320390b439a9b9c
Kacper Siuda <kacper.siuda@hotmail.com>
2015-08-26 21:36:03 +0200

    [Zadanie domowe][1] Banki

--- PraireDog/one/Startup/Startup/TrainingOneHomeworks/KacperSiuda/Banki/NBP.cs
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/KacperSiuda/Banki/NBP.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZadanieDomowe.Banki
+namespace Toci.Startup.Test
 {
     public class Nbp : Bank
     {
