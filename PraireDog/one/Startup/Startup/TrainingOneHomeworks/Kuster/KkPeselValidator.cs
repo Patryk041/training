2a0384cd091288a8ec8778b5058a707b5d74315f
Kuster <kacper.kustra@gmail.com>
2015-08-28 01:15:12 +0200

    add peselValidator

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/Kuster/KkPeselValidator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace KusterPeselValidator
+{
+    public class KkPeselValidator
+    {
+        public static readonly int[] coefficients = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+        public DateTime time;
+
+        public string GetNick()
+        {
+            return "Kuster";
+        }
+        
+        public bool PeselCorrectnessLegnth(string pesel)
+        {
+            return pesel.Length == 11;
+        }
+
+        public bool PeselCorrectnessNumbers(string pesel)
+        {
+            int checkingSum = 0;
+
+            int[] coefficients = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            for (int i = 0; i < 10; i++)
+            {
+                checkingSum += (coefficients[i] * int.Parse(pesel[i].ToString()));
+            }
+            if (checkingSum % 10 == 0 && int.Parse(pesel[11].ToString()) == 0)
+                return true;
+            else if (10 - checkingSum == int.Parse(pesel[11].ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool PeselCorrectnessBirthDate(string pesel)
+        {
+            int birthDateYearInt = int.Parse(pesel.Substring(0, 2));
+            int birthDateMonthInt = int.Parse(pesel.Substring(2, 2));
+            int birthDateDayInt = int.Parse(pesel.Substring(4, 2));
+
+            if (birthDateMonthInt / 20 == 0) birthDateYearInt += 1900;
+            if (birthDateMonthInt / 20 == 1) birthDateYearInt += 2000;
+            if (birthDateMonthInt / 20 == 2) birthDateYearInt += 2100;
+            if (birthDateMonthInt / 20 == 3) birthDateYearInt += 2200;
+            if (birthDateMonthInt / 20 == 4) birthDateYearInt += 1800;
+            birthDateMonthInt = birthDateMonthInt % 20;
+
+            try
+            {
+                time = new DateTime(birthDateYearInt, birthDateMonthInt, birthDateDayInt);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
