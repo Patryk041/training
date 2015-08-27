c6b9cd319488ac8485099f712b19048ee8adaab6
gruchaxd <gruszkaxd@gmail.com>
2015-08-27 21:46:32 +0200

    Update konto.php

--- Eagles/Eagles Reloaded/TrainingOneHomework/Tomasz_Trzebiński/Bank/konto.php
+++ Eagles/Eagles Reloaded/TrainingOneHomework/Tomasz_Trzebiński/Bank/konto.php
@@ -8,9 +8,7 @@ abstract class konto
     protected $nrKonta;
     
     protected abstract function zrobPrzelew();
-    //funkcja do przedefiniopwania w ka¿dej klasie banku wdg. w³asnego pomys³u
+    //funkcja do przedefiniopwania w kaÅ¼dej klasie banku wdg. wÅ‚asnego pomysÅ‚u
 }
-$ACDC = new AccountTester();
-echo $ACDC->identify("9810309875098f978766768976");
 
 
