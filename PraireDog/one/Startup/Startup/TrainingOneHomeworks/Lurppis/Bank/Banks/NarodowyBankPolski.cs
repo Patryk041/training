2fb70b5880b36438ec2038512cd1b684ec331368
Marcin Pilarczyk <marcinpilarczyk@me.com>
2015-08-29 00:58:00 +0200

    Koncepcja

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/Lurppis/Bank/Banks/NarodowyBankPolski.cs
@@ -0,0 +1,9 @@
+﻿namespace Startup.TrainingOneHomeworks.Lurppis.Bank.Banks
+{
+    public class NarodowyBankPolski : Abstract.Bank
+    {
+        public NarodowyBankPolski(string bankName, int id, string name, int amount, string lastName) : base(bankName: "Narodowy Bank Polski", id: 1010, name: name, amount: amount, lastName: lastName)
+        {
+        }
+    }
+}
