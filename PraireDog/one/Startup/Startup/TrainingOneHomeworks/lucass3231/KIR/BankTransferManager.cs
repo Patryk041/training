3c22f8161fe209bb5c6392147a9f32a72c3f3b65
lucass3231 <lucass3231@gmail.com>
2015-08-30 00:34:03 +0200

    commit

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/lucass3231/KIR/BankTransferManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Startup.TrainingOneHomeworks.lucass3231.KIR.Interfaces;
+
+
+namespace Startup.TrainingOneHomeworks.lucass3231.KIR
+{
+    public class BankTransferManager : IBankTransferManager
+    {
+        public List<Interfaces.IClientTransaction> ClientTransactions { get; }
+        public List<List<BankTransaction>> VerifiedClientTransactions { get; }
+        public void VerifyTransfer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BankTransaction SearchAccount(string number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
