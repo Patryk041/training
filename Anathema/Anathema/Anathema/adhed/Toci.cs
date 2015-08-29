d0b44c1ac78acca872d1f6fa93f77cf20ca27908
Adam <adamroweb@gmail.com>
2015-08-29 14:50:31 +0200

    test

---
+++ Anathema/Anathema/Anathema/adhed/Toci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema.adhed
+{
+    class Toci
+    {
+        public string showName()
+        {
+            return "name";
+        }
+    }
+}
