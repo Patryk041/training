2b731ed471edb1b898b84cb940287665421a5d84
Adam Rak <raqsasg@gmail.com>
2015-08-30 16:39:14 +0200

    .

--- Eagles/Rezidue/UmiemyInterfejsy/Raqs/ja.php
+++ Eagles/Rezidue/UmiemyInterfejsy/Raqs/ja.php
@@ -1,9 +1,17 @@
 <?php
-	 class Raqs implements IHuman {
+
+	require_once '../Interfaces.php';
+	
+	
+	 class Raqs implements IHuman,IDeveloper {
 	 	function getName(){
 	 		return "Janusz Cebula";
 	 	}
 	 	function getSex(){
 	 		return "Nieznana";
+	 	}
+	 	
+	 	function developSomeCode($codeToCode){
+	 		return "Kod Raqsa ".$string;
 	 	}
 	 }
