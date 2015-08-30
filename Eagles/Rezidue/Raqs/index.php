6c3926beded534c18f9c594ee7647606da03b441
Adam Rak <raqsasg@gmail.com>
2015-08-30 15:07:21 +0200

    .

--- Eagles/Rezidue/Raqs/index.php
+++ Eagles/Rezidue/Raqs/index.php
@@ -2,21 +2,24 @@
 require '../polimorfizm.php';
 	
 	class IBeliveICanFly implements IWeAreEvolvingFast{
+		private $filteringResult;
+		
 		public function LetsHaveFun($string, $int, $double, $array){
 			$chunk = '';
 			for ($i=$int; $i < strlen($string); $i++){
 				$chunk .= $string[$i];
 				
-				$this->ThereIsEvenMoreFun($chunk, $array);
+				$result = $this->ThereIsEvenMoreFun($chunk, $array);
+				$this->filteringResult = array_combine(array($result), array($result, $result, $result));
 			}
 		}
 		
 		public function ThereIsEvenMoreFun($chunk, $array){
 			
 			if(isset($array[$chunk])){
-				
+				return $array[$chunk];
 			}
-			
+			return "Gra¿yna";
 		}
 	}
 	
