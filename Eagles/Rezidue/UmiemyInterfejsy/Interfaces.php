2a288de912d31f461201e8fa3cd6844ddc0c5d3b
Adam Rak <raqsasg@gmail.com>
2015-08-30 16:31:46 +0200

    Dodawajcie swoje foldery i klasy.

---
+++ Eagles/Rezidue/UmiemyInterfejsy/Interfaces.php
@@ -0,0 +1,17 @@
+<?php
+ 	interface IDeveloper{
+ 		
+ 	}
+ 	
+ 	interface ISenses{
+ 			public function getSenseName();
+ 			public function useSense();
+ 			
+ 	}
+ 	
+ 	interface IHuman{
+ 			public function getName();
+ 			public function getSex();
+ 			
+ 			
+ 	}
