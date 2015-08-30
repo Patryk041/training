7b979fdf0d20558dc4c2251a79a5ab776a6db2a7
Adam Rak <raqsasg@gmail.com>
2015-08-30 14:56:19 +0200

    .

---
+++ Eagles/Rezidue/Raqs/index.php
@@ -0,0 +1,12 @@
+<?php
+require '../polimorfizm.php';
+	
+	class IBeliveICanFly implements IWeAreEvolvingFast{
+		public function LetsHaveFun($string, $int, $double, $array){
+			$chunk = '';
+			for ($i=$int; $i < strlen($string); $i++){
+				
+			}
+		}
+	}
+	
