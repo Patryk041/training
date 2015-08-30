8a395eb8bd0805d411deac40dca17ea949c75d3c
Damian Dąbrowski <dabek1812@gmail.com>
2015-08-30 15:26:21 +0200

    Coś prostego dla przykładu

---
+++ Eagles/Rezidue/estler/qwewqe.php
@@ -0,0 +1,30 @@
+<?php
+
+	interface IWeAreEnvolvingFast { 
+		function LetsHaveFun($string, $int, $double, array $array);
+		
+		function ThereIsEvenMoreFun($chunk, $array);
+	}
+
+	class IDontBeliveIt implements IWeAreEnvolvingFast {		
+		function LetsHaveFun($string, $int, $double, array $array) { 
+			for ($i = $int; $i < $double; $i++) { 
+				$array[$i] = $string."$i"; 
+			}
+			
+			$this -> ThereIsEvenMoreFun($double, $array);
+		}
+			
+		function ThereIsEvenMoreFun($chunk, $array) { 
+			for ($i = 0; $i < $chunk; $i++) { 
+				echo $array[$i]."\n";
+			}
+		}
+	}
+	
+	$IDontBeliveIt = new IDontBeliveIt();
+	
+	$IDontBeliveIt -> LetsHaveFun('User', 0, 20, array());
+	
+	
+?>
