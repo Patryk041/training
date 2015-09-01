1af92d36bd82a1e6646fb92aeb97c1d91a9aded2
gruchaxd <gruszkaxd@gmail.com>
2015-09-01 20:57:09 +0200

    Saros TEst

---
+++ Eagles/SarosTest/Piekny.php
@@ -0,0 +1,22 @@
+<?php
+
+class Pieknie
+{   
+
+    function nakurwiajSalto ()
+    {
+        echo "nakurwiam salto";
+    }
+}
+for ($i = 0; $i <= 10; $i ++)
+    for ($j = 0; $j <= 10; $j ++)
+        echo "*";
+echo "\n";
+
+if ($jedenie == 19)
+{
+    echo "";
+} else
+{
+    echo 'mylem plecy starej babie';
+}
