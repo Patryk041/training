9aaf295a33c06322e572bf4f345130023a386d62
Puchacz <kabanos997@o2.pl>
2015-09-04 00:45:42 +0200

    Banki - lepiej późno niż wcale;)

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/Puchacz/Banki/Banks/TransactionAlior.cs
@@ -0,0 +1,23 @@
+﻿using Startup.TrainingOneHomeworks.Puchacz.Banki.Abstract;
+
+namespace Startup.TrainingOneHomeworks.Puchacz.Banki.Banks
+{
+    public class TransactionAlior : BankTransaction
+    {
+        private const string BankId = "2490";
+        public override string GetBankId()
+        {
+            return BankId;
+        }
+
+        public override bool NotifyIncome()
+        {
+            return true;
+        }
+
+        public override bool NotifyOutcome()
+        {
+            return true;
+        }
+    }
+}
