5355bd30b803bc8bb259c6fdba42bc202e470ed8
lyczkul <lyczkul@interia.pl>
2015-09-03 19:09:42 +0200

    first class with body for methods

--- Eagles/Rezidue/lyczkul/index.php
+++ Eagles/Rezidue/lyczkul/index.php
@@ -1,11 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP to C++ application</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
 <?php
+require_once 'classes/PHPtoCPP.php';
 
-class Main
-{
-	public function __construct()
-	{
+$app = new PHPtoCPP();
+?>
 
-	}
-}
-
-$app = new Main();
+</body>
+</html>
