38b07ba30bd26c4a97c6818086c2288b9222a4ae
Widmosso <gilas.mateusz@gmail.com>
2015-09-04 17:22:41 +0200

    no message

--- Anathema/Anathema/Anathema/GhostRider/HelloEverybodyByGhostRider.cs
+++ Anathema/Anathema/Anathema/GhostRider/HelloEverybodyByGhostRider.cs
@@ -1,9 +1,13 @@
-﻿namespace Anathema.GhostRider
+﻿using System;
+
+namespace Anathema.GhostRider
 {
     public class HelloEverybodyByGhostRider
     {
         public string HelloEverybody()
         {
+            //test
+            //visual studio anywhere
             return "You are now on Your right way to glory on toci :)";
         }
     }
