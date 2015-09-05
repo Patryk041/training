839991ad0d45fb209fae190e0bb3739c59b89e42
Terry <wysocki31@gmail.com>
2015-09-05 21:07:47 +0200

    Szkolenie 14:30 LINQ i ex methods

---
+++ Anathema/Anathema/Anathema/GhostRider/TEST.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anathema.GhostRider
+{
+    public abstract class TEST<T,T2>
+    {
+        private Dictionary<T, Func<T2>> dict;
+    }
+}
