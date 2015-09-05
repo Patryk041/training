8719cb9dbb2647a58518b7f981266f7e067f0d09
skilloo <waskokrzysztof1@gmail.com>
2015-09-06 00:18:54 +0200

    no message

---
+++ Anathema/Anathema/Anathema/Skilloo/Fabryka/Passat.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace Anathema.Skilloo.Fabryka
+{
+    public class Passat:ICar
+    {
+        public Color color { get; set; }
+
+        public bool GetSomething()
+        {
+            return true;
+        }
+    }
+}
--- Anathema/Anathema/UnitTests/skilloo/UnitTest1.cs
+++ Anathema/Anathema/UnitTests/skilloo/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anathema.Skilloo.Fabryka;
@@ -17,6 +18,9 @@ namespace UnitTests.skilloo
 
             car.color = Color.Red;
             var car2 = fact.GetCar("Golf");
+            if(car2 ==typeof( Passat))
+                ((Passat)car2).GetSomething();
+           
         }
     }
 }
