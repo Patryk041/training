b23e8956783a518a9575d004d1cdf55fd2341518
marcin022 <marcinraniecki@gmail.com>
2015-09-08 20:35:33 +0200

    Demakaronizacja czyli wydzielanie bardziej elementarnych klas z juz istniejacych w celu pozniejszej latwiejszej implementacji w innych projektach

---
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/marcin022/bank_v_1_1/AccValidation.cs
@@ -0,0 +1,40 @@
+﻿namespace Startup.TrainingOneHomeworks.marcin022.bank_1_2
+{
+    public class AccValidation
+    {
+        private string AccNumber;
+        private string BankId;
+
+        public  AccValidation(string accNumber)
+        {
+            BankId = CutToId(CheckNumberFormat(accNumber));
+        }
+
+        public string CheckNumberFormat(string accNumber)
+        {
+            accNumber.Replace(" ", "");
+            if (accNumber.Length == 28)
+            {
+                string temp = accNumber.Substring(2);
+                accNumber = temp;
+                return accNumber;
+            }
+            else
+            {
+                return accNumber;
+            }
+        }
+
+        public string CutToId(string fullnumner)
+        {
+
+            BankId = fullnumner.Substring(2, 4);
+            return BankId;
+        }
+
+        public string GetBankId()
+        {
+            return BankId;
+        }
+    }
+}
--- PraireDog/one/Startup/Startup/TrainingOneHomeworks/marcin022/bank_v_1_1/BankValidate.cs
+++ PraireDog/one/Startup/Startup/TrainingOneHomeworks/marcin022/bank_v_1_1/BankValidate.cs
@@ -5,35 +5,16 @@ namespace Startup.TrainingOneHomeworks.marcin022.bank_1_2
 {
     public class BankValidate
     {
+        private AccValidation accVal;
         private string AccNumber;
         private string BankId;
 
-        public string CheckNumberFormat(string accNumber)
-        {
-            accNumber.Replace(" ", "");
-            if (accNumber.Length == 28)
-            {
-                string temp = accNumber.Substring(2);
-                accNumber = temp;
-                return accNumber;
-            }
-            else
-            {
-                return accNumber;
-            }
-        }
-
-        public string CutToId(string fullnumner)
-        {
-            
-            BankId = fullnumner.Substring(2, 4);
-            return BankId;
-        }
+       
 
         public BankValidate(string accNumber)
         {
-            string id = CutToId(CheckNumberFormat(accNumber));
-            CreateBank(id);
+            accVal=new AccValidation(accNumber);
+            CreateBank(accVal.GetBankId());
         }
 
         private static Dictionary<string, Func<IBank>> bankDictionary = new Dictionary<string, Func<IBank>>
