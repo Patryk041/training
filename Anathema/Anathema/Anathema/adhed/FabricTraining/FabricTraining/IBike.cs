0d71fe4b5f11481c7c411010143314c1e0173619
Adam <adamroweb@gmail.com>
2015-09-14 21:40:54 +0200

    fabric training

---
+++ Anathema/Anathema/Anathema/adhed/FabricTraining/FabricTraining/IBike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricTraining
+{
+    public interface IBike
+    {
+        bool Ride();
+
+        int WheelSize { get; set; }
+        int FrameSize { get; set; }
+        string Description { get; set; }
+
+        
+
+    }
+}
