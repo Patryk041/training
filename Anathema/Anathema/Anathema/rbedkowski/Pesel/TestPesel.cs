533ef27cb5671c3b9abbbe083f99acf94f3e8fce
rafalbedkowski <rafal.bedkowski@gmail.com>
2015-09-15 12:03:35 +0200

    test

---
+++ Anathema/Anathema/Anathema/rbedkowski/Pesel/TestPesel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anathema.rbedkowski.Pesel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.rbedkowski
+{
+   
+    public class TestPesel
+    {
+        
+        public void MainTest()
+        {
+            PeselValidator pesel = new PeselValidator();
+
+            try
+            {
+                Console.WriteLine(pesel.SetPesel("740621a9896"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nieprawidłowy pesel : " + e.Message);
+            }
+            //       pesel.GetNumberFromFile();
+            Console.Read();
+        }
+    }
+}
