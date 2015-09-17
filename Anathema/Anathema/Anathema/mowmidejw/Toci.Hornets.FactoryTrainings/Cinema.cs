e729d5928b6b6e0c8f7f684004adfb605d95c5ce
mowmidejw <dawooo95@gmail.com>
2015-09-17 20:41:05 +0200

    Factory test2

---
+++ Anathema/Anathema/Anathema/mowmidejw/Toci.Hornets.FactoryTrainings/Cinema.cs
@@ -0,0 +1,16 @@
+﻿namespace Toci.Hornets.FactoryTrainings
+{
+    public class Cinema : Building
+    {
+        public Cinema()
+        {
+            BuildName = "cinema";
+            Area = 33871;
+        }
+
+        public override string Description()
+        {
+            return "Do You want watch interesting film? We invite You to our cinema!";
+        }
+    }
+}
