bb8abf8058a8873e3c8c7073995f954c88db0fd6
Adam <adamroweb@gmail.com>
2015-09-20 14:30:03 +0200

    cities homework

---
+++ Anathema/Anathema/Anathema/adhed/CityTraining/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema.adhed.CityTraining
+{
+    public class City
+    {
+        private string _cityName;
+
+        public City(string name)
+        {
+            _cityName = name;
+        }
+    }
+}
