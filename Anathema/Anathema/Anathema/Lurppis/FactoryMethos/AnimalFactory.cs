4238c1c00963f1049bac46e67e6908e7370db5b9
Marcin Pilarczyk <marcinpilarczyk@me.com>
2015-09-21 23:26:10 +0200

    Factory Method Test z zwierzetami

---
+++ Anathema/Anathema/Anathema/Lurppis/FactoryMethos/AnimalFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Anathema.Lurppis.FactoryMethos.Abstract;
+using Anathema.Lurppis.FactoryMethos.Abstract.AbstractFactory;
+using Anathema.Lurppis.FactoryMethos.Interface;
+using Anathema.Lurppis.FactoryMethos.Zwierzeta;
+
+namespace Anathema.Lurppis.FactoryMethos
+{
+    public class AnimalFactory : AbstractAnimkalFactory<string,IAnimal>
+    {
+        public AnimalFactory()
+        {
+            Dictionary = new Dictionary<string, Func<IAnimal>>
+            {
+                {"Cat", () => new Cat()},
+                {"Wolf", () => new Wolf() }
+                //Można tak w nieskonczonosc
+            };
+        }
+    }
+}
