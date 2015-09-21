dd44d585c6cd9174b5b6d9eae1049448f870bbeb
Marcin Pilarczyk <marcinpilarczyk@me.com>
2015-09-21 22:26:46 +0200

    Fasada wzorzec przyklad

---
+++ Anathema/Anathema/UnitTests/Lurppis/Fasada/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Anathema.Lurppis.Fasada;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Lurppis.Fasada
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Fasada test
+            // Niestety na razie nie działa mi using....
+        }
+    }
+}
