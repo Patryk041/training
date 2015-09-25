f79b8ebdef8f0b1b3ef572919ee43dc9578506db
rafalbedkowski <rafal.bedkowski@gmail.com>
2015-09-25 09:35:45 +0200

    Strategy

---
+++ Anathema/Anathema/Anathema/rbedkowski/Strategy/Sailor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema.rbedkowski.Strategy
+{
+    class Sailor : IAction
+    {
+        public string GetAction()
+        {
+            return "Sailor";
+        }
+    }
+}
