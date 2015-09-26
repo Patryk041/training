8ee93e2a7dd485d61cd72d07cb617e2b74aa134e
brzydalek <marcin.mazurowski@o2.pl>
2015-09-26 12:02:57 +0200

    Pierwsze szkolenie zadanie1

---
+++ Anathema/Anathema/Anathema/Brzydal/Zad1GenerycznaLista/BrzydkaGenerycznaLista.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Anathema.Generics;
+
+namespace Anathema.Brzydal.Zad1GenerycznaLista
+{
+    public class BrzydkaGenerycznaLista<T> : GRList<T>
+    {
+
+        private T[] _items = new T[10];
+        //
+        private int _index = 0;
+
+
+        public override bool AddElement(T item, bool unique = true)
+        {
+            if (!_items.Contains(item))
+            {
+                if (_items.Length >= _index + 1)
+                {
+                    _items[_index] = item;
+                    _index++;
+                }
+                else
+                {
+                    Array.Resize(ref _items, _items.Length+10);
+                    //Array.Copy(_items, _items, _items.Length + 10);
+                    _items[_index] = item;
+                    _index++;
+                }
+           
+
+                return true;
+            }
+            return false;
+        }
+
+  
+    }
+}
---
+++ Anathema/Anathema/UnitTests/Brzydal/BrzydalTestListaGeneryczna.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Anathema.Brzydal.Zad1GenerycznaLista;
+using Anathema.Lurppis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Brzydal
+{
+
+    [TestClass]
+    public class BrzydalTestListaGeneryczna
+    {
+        [TestMethod]
+        public void TestListaGeneryczna()
+
+        {
+            BrzydkaGenerycznaLista<MyClass> mojaLista = new BrzydkaGenerycznaLista<MyClass>();
+
+
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+            mojaLista.AddElement(new MyClass());
+     
+
+         
+         
+           Debug.WriteLine(mojaLista);
+        } 
+
+    }
+}
