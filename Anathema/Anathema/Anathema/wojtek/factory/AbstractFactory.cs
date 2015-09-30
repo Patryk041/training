56b204c7c739f610aef1c1f4d14318985162774c
Wojtek Sabat <wojsabat@gmail.com>
2015-09-30 14:34:32 +0200

    AbstractFactory. U Lurpisa wyremowałem linijkę "public double PierwiastekStopnia2(double a) => a < 0 ? 0 : Math.Sqrt(a);", która powodowała błędy kompilacji.

---
+++ Anathema/Anathema/Anathema/wojtek/factory/AbstractFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema.wojtek.factory
+{
+    public abstract class AbstractFactory<Tkey,Titem>
+    {
+        Dictionary<Tkey,Titem> dictionary = new Dictionary<Tkey,Titem>();
+
+        public Titem Create(Tkey key)
+        {
+           return  dictionary.ContainsKey(key) ? dictionary[key] : default(Titem);
+        }
+
+
+
+        
+    }
+}
