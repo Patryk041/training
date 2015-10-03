3a7ceab85014a36bbf78fcdca0c71da81a301138
szycikm <szycikm@gmail.com>
2015-10-03 15:38:47 +0200

    no message

---
+++ Anathema/Anathema/Anathema/Sancho/Linq/SuperLinq.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Anathema.stpetersburgstakeout.linq;
+
+namespace Anathema.Sancho.Linq
+{
+	public class SuperLinq : LinqOperations
+	{
+		public List<string> GetUsernamesByValue(string value)
+		{
+			return ListaSlownikowa.Where(item => item.Value == value).Select(item => item.Key).ToList();
+		}
+	}
+}
