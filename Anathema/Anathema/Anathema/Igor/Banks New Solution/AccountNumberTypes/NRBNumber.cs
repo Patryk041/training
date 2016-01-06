c8b1e453cf9be449176d9757a532dd4f2f9219b3
Igor Santarek <jegor377@gmail.com>
2016-01-06 05:38:46 +0100

    new solution

---
+++ Anathema/Anathema/Anathema/Igor/Banks New Solution/AccountNumberTypes/NRBNumber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema.Igor.Banks_New_Solution.AccountNumberTypes
+{
+    class NRBNumber
+    {
+        public ushort institutionID { get; protected set; }
+        public byte branchID { get; protected set; }
+        public byte controlDigit { get; protected set; }
+        public int clientAccountNumber { get; protected set; }
+        public byte controlAmount { get; protected set; }
+        public int bankRoutingNumber { get; protected set; }
+        public string originalNumber { get; protected set; }
+
+        public NRBNumber(string originalNumber)
+        {
+            this.originalNumber = originalNumber;
+        }
+
+        protected void designateValuesIfNumberIsCorrect()
+        {
+
+        }
+
+        protected void designateControlAmount()
+        {
+            controlAmount = byte.Parse(originalNumber.Substring(0, 2));
+        }
+
+        protected void designateInstitutionID()
+        {
+            institutionID = ushort.Parse(originalNumber.Substring(2, 4));
+        }
+
+        protected void designateBranchID()
+        {
+            branchID = byte.Parse(originalNumber.Substring(6, 3));
+        }
+        
+        protected void designateControlDigit()
+        {
+            controlDigit = byte.Parse(originalNumber.Substring(9, 1));
+        }
+
+        protected void designateClientAccountNumber()
+        {
+            clientAccountNumber = int.Parse(originalNumber.Substring(10, 16));
+        }
+
+        protected void designateBankRoutingNumber()
+        {
+            bankRoutingNumber = int.Parse(originalNumber.Substring(2, 8));
+        }
+    }
+}
---
+++ Anathema/Anathema/Anathema/Igor/Banks New Solution/AccountNumberValidator/AccountNumberValidator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema.Igor.Banks_New_Solution.AccountNumberValidator
+{
+    class AccountNumberValidator
+    {
+        private string accountNumber;
+
+        public AccountNumberValidator(string accountNumber)
+        {
+            this.accountNumber = accountNumber;
+        }
+
+        public bool isAccountNumberCorrect()
+        {
+            if(isAccountNumberCorrect())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool isIBANAccountNumberCorrect()
+        {
+            if(isIBANAccountNumber())
+            {
+                if(FirstTwoSignsAreString())
+                {
+                    if(RestOfSignsAreDigits())
+                    {
+                        
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool isIBANAccountNumber()
+        {
+            return accountNumber.Length == 28;
+        }
+
+        public bool isNRBAccountNumber()
+        {
+            return accountNumber.Length == 26;
+        }
+
+        protected bool FirstTwoSignsAreString()
+        {
+            return !accountNumber.Substring(0, 2).All(char.IsDigit);
+        }
+
+        protected bool RestOfSignsAreDigits()
+        {
+            return accountNumber.All(char.IsDigit);
+        }
+
+        protected bool isIBANControlAmountCorrect()
+        {
+            var tmpAccountNumberForStringOperations = accountNumber;
+
+            tmpAccountNumberForStringOperations = tmpAccountNumberForStringOperations.Substring(2,
+                tmpAccountNumberForStringOperations.Length - 2) +
+                tmpAccountNumberForStringOperations.Substring(0, 2);
+
+
+
+            return true;
+        }
+
+        protected string changeStringLettersToThemAlphabetNumbers(string str)
+        {
+            var result = "";
+
+            foreach(var sign in )
+        }
+
+        protected bool isAccountNumberLengthCorrect()
+        {
+            return accountNumber.Length == 26 || accountNumber.Length == 28;
+        }
+    }
+}
